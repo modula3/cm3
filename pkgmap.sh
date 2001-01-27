12bcd5d67b9be43f93c19367bce319dabdbc9853
Olaf Wagner <wagner@elegosoft.com>
2001-01-27 18:00:17 +0000

    changes:  o added m3-tools/m3bundle to core system  o added do-cm3-comm.sh to build the communication packages  o fixed global building in pkgmap.sh  o added alternative command options -ship and -install to pkgmap.sh

--- pkgmap.sh
+++ pkgmap.sh
@@ -4,19 +4,38 @@ ROOT=${ROOT:-$(pwd)}
 
 BUILDLOCAL="cm3 -build -override -DROOT=${ROOT}"
 CLEANLOCAL="cm3 -clean -override -DROOT=${ROOT}"
-SHIP="cm3 -ship -DROOT=${ROOT}"
-BUILDGLOBAL="cm3 -build  -DROOT=${ROOT} && ${SHIP} -DROOT=${ROOT}"
+BUILDGLOBAL="cm3 -build  -DROOT=${ROOT}"
 CLEANGLOBAL="cm3 -clean -DROOT=${ROOT}"
+SHIP="cm3 -ship -DROOT=${ROOT}"
 
-CMD="${BUILDLOCAL}"
 ACTION="build"
+AREA=""
+
+exec_cmd() {
+  if [ "${ACTION}" = build ] ; then
+    if [ "${AREA}" = global ] ; then
+      ${BUILDGLOBAL} && ${SHIP}
+    else
+      ${BUILDLOCAL}
+    fi
+  elif [ "${ACTION}" = clean ] ; then
+    if [ "${AREA}" = global ] ; then
+      ${CLEANGLOBAL}
+    else
+      ${CLEANLOCAL}
+    fi
+  else
+    echo "unknown action" 1>&2
+    exit 1
+  fi
+}
 
 usage() {
   ME=$(basename $0)
   echo "usage:"
   echo "  $ME -buildlocal|-build"
   echo "      build and import from workspace (default action)"
-  echo "  $ME -buildglobal|-buildship"
+  echo "  $ME -buildglobal|-buildship|-ship|-install"
   echo "      build and ship; import from system pool"
   echo "  $ME -cleanlocal|-clean"
   echo "      clean packages with imports from workspace"
@@ -26,12 +45,18 @@ usage() {
 
 if [ -n "$1" ] ; then
   case "$1" in
-    -build|-buildlocal) 	CMD="${BUILDLOCAL}"; ACTION="build"; shift;;
-    -buildglobal|-buildship)	CMD="${BUILDGLOBAL}"; ACTION="build"; shift;;
-    -clean|-cleanlocal)		CMD="${CLEANLOCAL}"; ACTION="clean"; shift;;
-    -cleanglobal)		CMD="${CLEANGLOBAL}"; ACTION="clean"; shift;;
-    -help|-h) 			usage; exit 0;;
-    -*)				usage; exit 1;;
+    -build|-buildlocal)
+      ACTION="build"; AREA="local";  shift;;
+    -buildglobal|-buildship|-ship|-install)
+      ACTION="build"; AREA="global"; shift;;
+    -clean|-cleanlocal)
+      ACTION="clean"; AREA="local";  shift;;
+    -cleanglobal)
+      ACTION="clean"; AREA="global"; shift;;
+    -help|-h)
+      usage; exit 0;;
+    -*)
+      usage; exit 1;;
   esac
 fi
 
@@ -56,7 +81,7 @@ fi
 for PKG in ${PKGS} ; do
   echo "=== package ${PKG} ==="
   if cd ${ROOT}/${PKG} ; then
-    if ${CMD} ; then
+    if exec_cmd ; then
       echo " => ${PKG} done"
     else
       echo " *** ${ACTION}ing of ${PKG} failed ***" 
