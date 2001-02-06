6bd5ce216cd17cec29f9aa0d358e8b8f02df4027
wagner <>
2001-02-06 20:29:03 +0000

    accidentally left out at last commit

---
+++ do-cm3-ui.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+WIN32=${WIN32:-yes}
+SERIAL=${SERIAL:-yes}
+
+UNAME=$(uname)
+
+if [ "${UNAME}" = FreeBSD ] ; then
+  WIN32=no
+  SERIAL=no
+  ROOT=${ROOT:-$(pwd)}
+fi
+
+export WIN32 ROOT
+
+# base libraries and compiler
+PKGS=""
+[ "${WIN32}" != yes ] && PKGS="${PKGS} m3-ui/X11R4"
+PKGS="${PKGS} m3-ui/ui"
+PKGS="${PKGS} m3-ui/vbtkit"
+PKGS="${PKGS} m3-comm/tcp"
+PKGS="${PKGS} m3-ui/cmvbt"
+PKGS="${PKGS} m3-ui/jvideo"
+PKGS="${PKGS} m3-ui/videovbt"
+PKGS="${PKGS} m3-www/web"
+PKGS="${PKGS} m3-ui/formsvbtpixmaps"
+PKGS="${PKGS} m3-ui/formsvbt"
+PKGS="${PKGS} m3-ui/formsview"
+PKGS="${PKGS} m3-ui/formsedit"
+
+#[ "${WIN32}" = yes ] && PKGS="${PKGS} m3-comm/tapi"
+
+echo ./pkgmap.sh $@ ${PKGS}
+exec ./pkgmap.sh $@ ${PKGS}
