73869dcd8517c0b8c4d8e95560bdff543dfce8b7
wagner <>
2001-03-01 22:57:06 +0000

    build and ship m3bundle if not already installed
    modified: scripts/do-cm3-std.sh

--- scripts/do-cm3-std.sh
+++ scripts/do-cm3-std.sh
@@ -37,6 +37,9 @@ ${GEN_CMDS}"
 
 show_usage $@
 
+type m3bundle >/dev/null 2>/dev/null || \
+  "$ROOT/scripts/do-pkg.sh" buildship m3bundle
+
 OPTIONS=`extract_options $@`
 ACTION=`map_action $@`
 ADDARGS=`add_action_opts $@`
