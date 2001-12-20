5147951f02bd99bb23fe580b64c14f29f12a4ab5
Olaf Wagner <wagner@elegosoft.com>
2001-12-20 11:59:36 +0000

    add missing package m3bundle
    modified: scripts/make-bin-dist-min.sh

--- scripts/make-bin-dist-min.sh
+++ scripts/make-bin-dist-min.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: make-bin-dist-min.sh,v 1.14 2001-12-19 17:34:20 wagner Exp $
+# $Id: make-bin-dist-min.sh,v 1.15 2001-12-20 11:59:36 wagner Exp $
 
 if [ -n "$ROOT" -a -d "$ROOT" ] ; then
   sysinfo="$ROOT/scripts/sysinfo.sh"
@@ -38,6 +38,7 @@ if syscall_wrappers_exist ; then
 fi
 P="${P} m3core"
 P="${P} libm3"
+P="${P} m3bundle"
 P="${P} m3middle"
 [ "${M3OSTYPE}" = "WIN32" ] && P="${P} m3objfile"
 P="${P} m3linker"
