ad30bdb27aa10649490fac44b3ee7567be0a277b
wagner <>
2003-02-07 15:37:36 +0000

    upgrade version default

--- scripts/sysinfo.sh
+++ scripts/sysinfo.sh
@@ -11,7 +11,7 @@ PRJ_ROOT=${PRJ_ROOT:-${HOME}/work}
 
 #-----------------------------------------------------------------------------
 # set some defaults
-CM3VERSION=${CM3VERSION:-"5.1.8"}
+CM3VERSION=${CM3VERSION:-"5.2.0"}
 CM3_GCC_BACKEND=yes
 CM3_GDB=no
 CM3_INSTALL=/usr/local/cm3
