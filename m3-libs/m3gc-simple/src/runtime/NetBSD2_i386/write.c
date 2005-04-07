ca84f7f5848ad77d0ee40a3f433fdf12e2062124
Stefan Sperling <stsp@elego.de>
2005-04-07 22:24:02 +0000

    NetBSD 2.0 / i386 support

---
+++ m3-libs/m3gc-simple/src/runtime/NetBSD2_i386/write.c
@@ -0,0 +1,14 @@
+#include "wrap.h"
+#include <sys/types.h>
+#include <unistd.h>
+
+size_t
+m3_write(int fd, const void *buf, int nbytes)
+{
+  int result;
+
+  ENTER_CRITICAL;
+  result = write(fd, buf, nbytes);
+  EXIT_CRITICAL;
+  return result;
+}
