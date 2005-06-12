3b830bf131e99c0ac28b987d605f4b376e3a851d
Olaf Wagner <wagner@elegosoft.com>
2005-06-12 21:25:30 +0000

    add scripts to upgrade to 5.3 (new garbage collection support and new thread implementations) set current version to d5.3.0-2005-06-12 (until 5.3.0 release)
    added: scripts/backup-pkgs.sh
    added: scripts/upgrade-5.3.sh
    modified: scripts/sysinfo.sh

---
+++ scripts/backup-pkgs.sh
@@ -0,0 +1,101 @@
+#!/bin/sh
+# $Id: backup-pkgs.sh,v 1.1 2005-06-12 21:25:30 wagner Exp $
+
+if [ -n "$ROOT" -a -d "$ROOT" ] ; then
+  sysinfo="$ROOT/scripts/sysinfo.sh"
+else
+  root=`pwd`
+  while [ -n "$root" -a ! -f "$root/scripts/sysinfo.sh" ] ; do
+    root=`dirname $root`
+  done
+  sysinfo="$root/scripts/sysinfo.sh"
+  if [ ! -f "$sysinfo" ] ; then
+    echo "scripts/sysinfo.sh not found" 1>&2
+    exit 1
+  fi
+  export root
+fi
+
+. "$sysinfo"
+
+if [ "x$1" = "x-f" -o "x$1" = "x-force" ]; then
+  FORCE="yes"
+  shift
+else
+  FORCE=""
+fi
+if [ "x$1" = "x-r" -o "x$1" = "x-restore" ]; then
+  RESTORE="yes"
+  shift
+else
+  RESTORE=""
+fi
+if [ -n "$1" ]; then
+  BACKUPID="$1"
+  shift
+fi
+
+BACKUPID=${BACKUPID:-${CM3VERSION}}
+
+. "$ROOT/scripts/pkginfo.sh"
+. "$ROOT/scripts/pkgcmds.sh"
+
+P=""
+[ ${TARGET} != NT386 ] && P="${P} m3gc-simple"
+if syscall_wrappers_exist && [ -z "$M3GC_SIMPLE" ] ; then
+  [ ${TARGET} != NT386 ] && P="${P} m3gc-enhanced"
+fi
+P="${P} m3core"
+P="${P} libm3"
+P="${P} m3middle"
+[ "${M3OSTYPE}" = "WIN32" ] && P="${P} m3objfile"
+P="${P} m3linker"
+[ "${GCC_BACKEND}" != yes ] && P="${P} m3back"
+[ "${GCC_BACKEND}" != yes ] && P="${P} m3staloneback"
+P="${P} m3front"
+P="${P} m3quake"
+[ "${GCC_BACKEND}" = yes -a -z "$OMIT_GCC" ] && P="${P} m3cc"
+P="${P} cm3"
+P="${P} m3scanner"
+P="${P} m3tools"
+P="${P} m3cgcat"
+P="${P} m3cggen"
+P="${P} m3bundle"
+[ "${M3OSTYPE}" = "WIN32" ] && P="${P} mklib"
+[ "${M3OSTYPE}" = "WIN32" ] && P="${P} dll2lib"
+[ "${M3OSTYPE}" = "WIN32" ] && P="${P} fix_nl"
+[ "${M3OSTYPE}" = "WIN32" ] && P="${P} libdump"
+
+USAGE="
+  `basename $0` [ generic_options ] [ generic_cmd ]
+
+  will make copies of the following packages in ${CM3_INSTALL}:
+
+`print_list4 ${P}`
+"
+
+show_usage $@
+
+cd ${INSTALLROOT} || exit 1
+echo "${INSTALLROOT}:"
+
+if [ -z "${RESTORE}" ]; then
+  SRC="pkg"
+  DEST="pkg-${BACKUPID}"
+  if [ -d "${DEST}" ]; then
+    if [ -z "${FORCE}" ] ; then
+      echo "${DEST} already exists" 1>&2
+      exit 1
+    fi
+  else
+    mkdir ${DEST}
+  fi
+else
+  SRC="pkg-${BACKUPID}"
+  DEST="pkg"
+fi
+for p in ${P}; do
+  echo "  ${SRC}/${p} --> ${DEST}/${p}"
+  cp -pr ${SRC}/${p} ${DEST}
+done
+
--- scripts/sysinfo.sh
+++ scripts/sysinfo.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: sysinfo.sh,v 1.42 2005-05-15 10:29:55 wagner Exp $
+# $Id: sysinfo.sh,v 1.43 2005-06-12 21:25:30 wagner Exp $
 
 if [ "$SYSINFO_DONE" != "yes" ] ; then
 
@@ -12,7 +12,7 @@ PRJ_ROOT=${PRJ_ROOT:-${HOME}/work}
 
 #-----------------------------------------------------------------------------
 # set some defaults
-CM3VERSION=${CM3VERSION:-"d5.2.8-2005-05-12"}
+CM3VERSION=${CM3VERSION:-"d5.3.0-2005-06-12"}
 CM3_GCC_BACKEND=yes
 CM3_GDB=no
 CM3_INSTALL=/usr/local/cm3
