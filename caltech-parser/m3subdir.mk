a533425f1cf2b7c3a7090f0da86804865b2520f9
Dragiša Durić <dragisha@m3w.org>
2005-09-01 09:43:52 +0000

    This commit was manufactured by cvs2svn to create branch 'release_website_2005-09-02_branch_start'.

--- caltech-parser/m3subdir.mk
+++
@@ -1,8 +0,0 @@
-# $Id: m3subdir.mk,v 1.2 2001-09-19 15:39:38 wagner Exp $ 
-PRE_MAKE?=echo
-PRE_MAKE_CLEAN?=echo
-all: 
-	cd src; $(PRE_MAKE); m3build $(M3BUILDOPTS); cd ..
-
-clean:
-	cd src; $(PRE_MAKE_CLEAN); m3build $(M3BUILDOPTS) clean || true ; cd ..
