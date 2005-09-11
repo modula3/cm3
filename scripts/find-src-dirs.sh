12dd3e5cd5431f72864fb975c17a23ea590f79bc
wagner <>
2005-09-11 14:00:28 +0000

    add script to list all CM3 source dirs in the current workspace suitable to be fed into (m3)gdb
    added: scripts/find-src-dirs.sh

---
+++ scripts/find-src-dirs.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# $Id$
+
+if [ -n "$ROOT" -a -d "$ROOT" ] ; then
+  sysinfo="$ROOT/scripts/sysinfo.sh"
+else
+  root=`pwd`
+  while [ -n "$root" -a ! -f "$root/scripts/sysinfo.sh" ] ; do
+    root=`dirname $root`
+  done
+  sysinfo="$root/scripts/sysinfo.sh"
+  if [ ! -f "$sysinfo" ] ; then
+    echo "scripts/sysinfo.sh not found" 1>&2
+    exit 1
+  fi
+  export root
+fi
+
+. "$sysinfo"
+
+find "${root}" -name m3makefile -print | \
+  sed -e 's;/m3makefile$;;' -e 's;^;dir ;'
+
