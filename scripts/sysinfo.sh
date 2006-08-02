14269e0a570674c1ba36255fc371838b5233a0ed
Olaf Wagner <wagner@elegosoft.com>
2006-08-02 21:29:24 +0000

    increase version to d5.4.0
    modified: scripts/sysinfo.sh

--- scripts/sysinfo.sh
+++ scripts/sysinfo.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: sysinfo.sh,v 1.46 2006-04-20 18:46:56 micha Exp $
+# $Id: sysinfo.sh,v 1.47 2006-08-02 21:29:24 wagner Exp $
 
 if [ "$SYSINFO_DONE" != "yes" ] ; then
 
@@ -12,7 +12,7 @@ PRJ_ROOT=${PRJ_ROOT:-${HOME}/work}
 
 #-----------------------------------------------------------------------------
 # set some defaults
-CM3VERSION=${CM3VERSION:-"d5.3.2-2006-04-20"}
+CM3VERSION=${CM3VERSION:-"d5.4-0"}
 CM3_GCC_BACKEND=yes
 CM3_GDB=no
 CM3_INSTALL=/usr/local/cm3
