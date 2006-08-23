eba408d7a5638d185a8434c2b25b663603b63dc7
hosking <>
2006-08-23 04:04:04 +0000

    For I386_DARWIN.

--- m3-libs/m3core/src/Csupport/I386_DARWIN/dtoa.c
+++ m3-libs/m3core/src/Csupport/I386_DARWIN/dtoa.c
@@ -1,5 +1,9 @@
-/* Copyright according to COPYRIGHT-CMASS. */
-/* FIXME: copied from FreeBSD3 target. Probably needs to be changed. */
+/* Copyright (C) 1992, Digital Equipment Corporation          */
+/* All rights reserved.                                       */
+/* See the file COPYRIGHT for a full description.             */
+
+/* Last modified on Wed Apr  5 08:44:12 PDT 1995 by kalsow    */
+/*      modified on Mon Oct 12 13:50:54 PDT 1992 by muller    */
 
 #ifndef KR_headers
 #define KR_headers
@@ -10,17 +14,3 @@
 #endif
 
 #include "dtoa.h"
-
-/* Apparently libc defines both "__dtoa" and "dtoa".  ???  */
-#if 0
-char * __dtoa       
-#ifdef KR_headers
-        (d, mode, ndigits, decpt, sign, rve)
-        double d; int mode, ndigits, *decpt, *sign; char **rve;
-#else 
-        (double d, int mode, int ndigits, int *decpt, int *sign, char **rve)
-#endif
-{
-  return dtoa(d, mode, ndigits, decpt, sign, rve);
-}
-#endif
