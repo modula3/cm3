fd7b23f2b5ad02094bbeb417df085247c0def3c8
hosking <>
2006-09-20 22:30:27 +0000

    Somehow the setting of VOLATILE got lost in init_offset.

--- m3-sys/m3cc/gcc/gcc/m3cg/parse.c
+++ m3-sys/m3cc/gcc/gcc/m3cg/parse.c
@@ -2749,6 +2749,7 @@ m3cg_init_offset (void)
   tree f, v;
   TREE_USED (var) = 1;
   /* M3 hack to preserve TREE_ADDRESSABLE: see setup_pointers_and_addressables */
+  TREE_THIS_VOLATILE (var) = 1;
   one_field (o, t_int, &f, &v);
   TREE_VALUE (v) = var;		/* we will fix the offset later */
 }
