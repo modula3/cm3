af5b40df521364614ecb95ec85eed9b80e71a593
stsp <>
2006-10-11 15:44:11 +0000

    Drop d prefix from version number. Update modification datestamp.

--- scripts/sysinfo.sh
+++ scripts/sysinfo.sh
@@ -12,7 +12,7 @@ PRJ_ROOT=${PRJ_ROOT:-${HOME}/work}
 
 #-----------------------------------------------------------------------------
 # set some defaults
-CM3VERSION=${CM3VERSION:-"d5.4-0"}
+CM3VERSION=${CM3VERSION:-"5.4-0"}
 CM3_GCC_BACKEND=yes
 CM3_GDB=no
 CM3_INSTALL=/usr/local/cm3
