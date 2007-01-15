b6f7c1a41b442205cfcce4e3d6dac1574b21151c
jkrell <>
2007-01-15 01:10:12 +0000

    works to make cm3-min-WIN32-NT386-version.tgz

--- scripts/win/make-dist.cmd
+++ scripts/win/make-dist.cmd
@@ -23,8 +23,11 @@ set INSTALLROOT_STD=%STAGE%\std
 set INSTALLROOT_CORE=%STAGE%\core
 set INSTALLROOT_BASE=%STAGE%\base
 
+@echo on
+@goto :TarGzip
+
 @rem ------------------------------------------------------------------------------------------------------------------------
-call :header build new compiler with old compiler (%INSTALLROOT_PREVIOUS% to %INSTALLROOT_COMPILER_WITH_PREVIOUS%)
+call :Echo build new compiler with old compiler (%INSTALLROOT_PREVIOUS% to %INSTALLROOT_COMPILER_WITH_PREVIOUS%)
 @rem ------------------------------------------------------------------------------------------------------------------------
 
 set P=^
@@ -56,7 +59,7 @@ call :RealClean || exit /b 1
 endlocal
 
 @rem ----------------------------------------------------------------------------------------------------------------------------------
-call :header build compiler with itself (%INSTALLROOT_COMPILER_WITH_PREVIOUS% to %INSTALLROOT_COMPILER_WITH_SELF%)
+call :Echo build compiler with itself (%INSTALLROOT_COMPILER_WITH_PREVIOUS% to %INSTALLROOT_COMPILER_WITH_SELF%)
 @rem ----------------------------------------------------------------------------------------------------------------------------------
 
 setlocal
@@ -66,6 +69,10 @@ set PATH=%INSTALLROOT_COMPILER_WITH_PREVIOUS%\bin;%PATH%
 call :RealClean || exit /b 1
 call :BuildShip || exit /b 1
 call :ShipCompiler || exit /b 1
+@rem
+@rem save cminstall.exe away for later
+@rem
+call :CopyFile %ROOT%\m3-sys\cminstall\%TARGET%\cminstall.exe %STAGE%\cminstall.exe || exit /b 1
 call :RealClean || exit /b 1
 
 endlocal
@@ -73,7 +80,7 @@ endlocal
 set P=
 
 @rem ----------------------------------------------------------------------------------------------------------------------------------
-call :header build minimal packages with new compiler
+call :Echo build minimal packages with new compiler
 @rem ----------------------------------------------------------------------------------------------------------------------------------
 
 setlocal
@@ -89,7 +96,7 @@ call %~dp0do-cm3-min realclean || exit /b 1
 endlocal
 
 @rem ----------------------------------------------------------------------------------------------------------------------------------
-call :header build core packages with new compiler
+call :Echo build core packages with new compiler
 @rem ----------------------------------------------------------------------------------------------------------------------------------
 
 setlocal
@@ -98,14 +105,15 @@ set INSTALLROOT=%INSTALLROOT_CORE%
 call :CopyCompiler %INSTALLROOT_COMPILER_WITH_SELF% %INSTALLROOT% || exit /b 1
 set PATH=%INSTALLROOT%\bin;%PATH%
 
-call %~dp0do-cm3-core realclean || exit /b 1
-call %~dp0do-cm3-core buildship || exit /b 1
-call %~dp0do-cm3-core realclean || exit /b 1
+call :Echo skipping..
+rem call %~dp0do-cm3-core realclean || exit /b 1
+rem call %~dp0do-cm3-core buildship || exit /b 1
+rem call %~dp0do-cm3-core realclean || exit /b 1
 
 endlocal
 
 @rem ----------------------------------------------------------------------------------------------------------------------------------
-call :header build standard packages with new compiler
+call :Echo build standard packages with new compiler
 @rem ----------------------------------------------------------------------------------------------------------------------------------
 
 setlocal
@@ -114,9 +122,10 @@ set INSTALLROOT=%INSTALLROOT_STD%
 call :CopyCompiler %INSTALLROOT_COMPILER_WITH_SELF% %INSTALLROOT% || exit /b 1
 set PATH=%INSTALLROOT%\bin;%PATH%
 
-call %~dp0do-cm3-std realclean || exit /b 1
-call %~dp0do-cm3-std buildship || exit /b 1
-call %~dp0do-cm3-std realclean || exit /b 1
+call :Echo skipping..
+rem call %~dp0do-cm3-std realclean || exit /b 1
+rem call %~dp0do-cm3-std buildship || exit /b 1
+rem call %~dp0do-cm3-std realclean || exit /b 1
 
 endlocal
 
@@ -124,9 +133,40 @@ endlocal
 @rem ----------------------------------------------------------------------------------------------------------------------------------
 
 echo INSTALLROOT_MIN=%INSTALLROOT_MIN%
-echo INSTALLROOT_STD=%INSTALLROOT_STD%
-echo INSTALLROOT_CORE=%INSTALLROOT_CORE%
-echo INSTALLROOT_BASE=%INSTALLROOT_BASE%
+rem echo INSTALLROOT_STD=%INSTALLROOT_STD%
+rem echo INSTALLROOT_CORE=%INSTALLROOT_CORE%
+rem echo INSTALLROOT_BASE=%INSTALLROOT_BASE%
+
+
+echo now need to tar/gzip it up
+rem we want tar.exe, gzip.exe, cygwin.dll, cminstall.exe, copyright-cmass, and system.tgz, tar/gziped
+rem into cm3-min-win32-nt386-<version>.tar.gz
+rem bzip2 would be preferable for a smaller size
+
+:TarGzip
+
+pushd %INSTALLROOT_MIN%
+cacls . /t /e /g everyone:r
+mkdir symbols
+for /f %%a in ('dir /s/b/a-d *.pdb') do move %%a symbols
+if exist system.tgz del system.tgz
+tar -cvzf system.tgz bin lib pkg || exit /b 1
+@rem cheat
+call %~dp0do-pkg buildship cminstall
+call :CopyFile %ROOT%\m3-sys\cminstall\%TARGET%\cminstall.exe . || exit /b 1
+call :CopyFile %ROOT%\m3-sys\COPYRIGHT-CMASS . || exit /b 1
+call :CopyFile %ROOT%\tools\win32\tar.exe . || exit /b 1
+call :CopyFile %ROOT%\tools\win32\gzip.exe . || exit /b 1
+call :CopyFile %ROOT%\tools\win32\cygwin.dll . || exit /b 1
+if exist cm3-min-%M3OSTYPE%-%TARGET%-%CM3VERSION%.tgz del cm3-min-%M3OSTYPE%-%TARGET%-%CM3VERSION%.tgz
+tar -cvzf cm3-min-%M3OSTYPE%-%TARGET%-%CM3VERSION%.tgz cminstall.exe COPYRIGHT-CMASS system.tgz tar.exe gzip.exe cygwin.dll  || exit /b 1
+tar -cvzf cm3-min-%M3OSTYPE%-%TARGET%-%CM3VERSION%-symbols.tgz symbols  || exit /b 1
+popd
+
+echo DONE!
+echo Output is %INSTALLROOT_MIN%\cm3-min-%M3OSTYPE%-%TARGET%-%CM3VERSION%.tgz
+echo Output is %INSTALLROOT_MIN%\cm3-min-%M3OSTYPE%-%TARGET%-%CM3VERSION%-symbols.tgz
+echo Lots of intermediate state remains in %STAGE%.
 
 goto :eof
 
@@ -149,12 +189,12 @@ goto :eof
     @rem This is easily dealt with on NT by moving it away to a unique location, MoveFileEx to delete (admin-only).
     @rem Do it manually for now.
     @rem
-    call :my_mkdir %INSTALLROOT%
-    call :my_copy       %ROOT%\m3-sys\cm3\%TARGET%\cm3.exe %INSTALLROOT%\bin\cm3.exe || exit /b 1
-    call :my_copy       %ROOT%\m3-sys\cm3\%TARGET%\cm3.cfg %INSTALLROOT%\bin\cm3.cfg || exit /b 1
-    call :my_copy       %ROOT%\m3-sys\cm3\%TARGET%\cm3.pdb %INSTALLROOT%\bin\cm3.pdb || exit /b 1
-    call :copy_if_exist %ROOT%\m3-sys\cm3\%TARGET%\cm3.exe.manifest %INSTALLROOT%\bin\cm3.exe.manifest || exit /b 1
-    call :my_copy       %ROOT%\m3-sys\cminstall\src\config\%TARGET% %INSTALLROOT%\bin\cm3.cfg || exit /b 1
+    call :CreateDirectory %INSTALLROOT%
+    call :CopyFile       %ROOT%\m3-sys\cm3\%TARGET%\cm3.exe %INSTALLROOT%\bin\cm3.exe || exit /b 1
+    call :CopyFile       %ROOT%\m3-sys\cminstall\src\config\%TARGET% %INSTALLROOT%\bin\cm3.cfg || exit /b 1
+    call :CopyFile       %ROOT%\m3-sys\cm3\%TARGET%\cm3.pdb %INSTALLROOT%\bin\cm3.pdb || exit /b 1
+    call :CopyFileIfExist %ROOT%\m3-sys\cm3\%TARGET%\cm3.exe.manifest %INSTALLROOT%\bin\cm3.exe.manifest || exit /b 1
+    call :CopyFile       %ROOT%\m3-sys\cminstall\src\config\%TARGET% %INSTALLROOT%\bin\cm3.cfg || exit /b 1
     goto :eof
 
 :CopyCompiler
@@ -162,35 +202,39 @@ goto :eof
     @rem Copy the compiler from one INSTALLROOT to another, possibly having cleaned out the intermediate directories.
     @rem The config file always comes right out of the source tree.
     @rem
-    call :my_mkdir %2\bin
-    call :my_copy       %1\bin\cm3.exe %2\bin\cm3.exe || exit /b 1
-    call :my_copy       %1\bin\cm3.pdb %2\bin\cm3.pdb || exit /b 1
-    call :copy_if_exist %1\bin\cm3.exe.manifest %2\bin\cm3.exe.manifest || exit /b 1
-    call :my_copy       %ROOT%\m3-sys\cminstall\src\config\%TARGET% %2\bin\cm3.cfg || exit /b 1
+    call :CreateDirectory %2\bin
+    call :CopyFile       %1\bin\cm3.exe %2\bin\cm3.exe || exit /b 1
+    call :CopyFile       %1\bin\cm3.pdb %2\bin\cm3.pdb || exit /b 1
+    call :CopyFileIfExist %1\bin\cm3.exe.manifest %2\bin\cm3.exe.manifest || exit /b 1
+    call :CopyFile       %ROOT%\m3-sys\cminstall\src\config\%TARGET% %2\bin\cm3.cfg || exit /b 1
     goto :eof
 
 :CopyMklib
     @rem
     @rem Copy mklib from one INSTALLROOT to another, possibly having cleaned out the intermediate directories.
     @rem
-    call :my_mkdir %2\bin
-    call :my_copy       %1\bin\mklib.exe %2\bin\mklib.exe || exit /b 1
-    call :my_copy       %1\bin\mklib.pdb %2\bin\mklib.pdb || exit /b 1
-    call :copy_if_exist %1\bin\mklib.exe.manifest %2\bin\mklib.exe.manifest || exit /b 1
+    call :CreateDirectory %2\bin
+    call :CopyFile        %1\bin\mklib.exe %2\bin\mklib.exe || exit /b 1
+    call :CopyFile        %1\bin\mklib.pdb %2\bin\mklib.pdb || exit /b 1
+    call :CopyFileIfExist %1\bin\mklib.exe.manifest %2\bin\mklib.exe.manifest || exit /b 1
     goto :eof
 
-:my_copy
+:CopyFile
+    setlocal
+    set from=%1
+    set to=%2
+    if "%to%" == "." set to=.\%~nx1
+    if exist %to% del %to% || exit /b 1
+    copy %from% %to% || exit /b 1
+    endlocal
+    goto :eof
+
+:CopyFileIfExist
     if exist %2 del %2 || exit /b 1
     if exist %1 copy %1 %2 || exit /b 1
     goto :eof
 
-:copy_if_exist
-    if exist %2 del %2 || exit /b 1
-    if exist %1 copy %1 %2 || exit /b 1
-    goto :eof
-
-
-:header
+:Echo
     @echo.
 	@echo =============================================================================
 	@echo %*
@@ -209,9 +253,9 @@ goto :eof
     goto :eof
 
 :CreateSkel
-    call :my_mkdir %INSTALLROOT%\bin || exit /b 1
-    call :my_mkdir %INSTALLROOT%\lib || exit /b 1
-    call :my_mkdir %INSTALLROOT%\pkg || exit /b 1
+    call :CreateDirectory %INSTALLROOT%\bin || exit /b 1
+    call :CreateDirectory %INSTALLROOT%\lib || exit /b 1
+    call :CreateDirectory %INSTALLROOT%\pkg || exit /b 1
     goto :eof
 
 :Do
@@ -230,6 +274,6 @@ goto :eof
     call :Run call %~dp0pkgmap %OPTIONS% %ADDARGS% -c "%ACTION%" %P% || exit /b 1
     goto :eof
 
-:my_mkdir
+:CreateDirectory
     if not exist %1 mkdir %1
     goto :eof
