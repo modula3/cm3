b1b7579a23f5cfb7ce60bc41fc1e8f1eec2d19c4
hosking <>
2007-07-03 17:08:56 +0000

    Eliminate C-level definition of inCritical -- it is a thread-implementation specific variable (in ThreadPosix.m3).

--- m3-libs/m3core/src/runtime/DS3100/RTThreadC.c
+++ m3-libs/m3core/src/runtime/DS3100/RTThreadC.c
@@ -33,9 +33,6 @@ if (((*to)[JB_S8] <= (int)(&etext)) || ((*to)[JB_PC] == 0) || ((*to)[JB_SP] == 0
 /* global thread ID used by 'etp' */
 int ThreadF__myId = 1;
 
-/* low-level runtime lock */
-int ThreadF__inCritical = 0;
-
 /* global, per-thread linked list of exception handlers */
 void* ThreadF__handlerStack = 0;
 
--- m3-libs/m3core/src/runtime/SOLsun/RTThreadC.c
+++ m3-libs/m3core/src/runtime/SOLsun/RTThreadC.c
@@ -20,9 +20,6 @@ void RTThread__Transfer (ucontext_t *from, ucontext_t *to)
 /* global thread ID used by 'etp' */
 int ThreadF__myId = 1;
 
-/* low-level runtime lock */
-int ThreadF__inCritical = 0;
-
 /* global, per-thread linked list of exception handlers */
 void* ThreadF__handlerStack = 0;
 
