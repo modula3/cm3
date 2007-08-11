f00a1c63c6477b127cd594ee5a44f8bdeac142ae
Jay Michael Krell <jay.krell@cornell.edu>
2007-08-11 16:35:21 +0000

    Don't clear %CM3% so that we can attempt to follow Tony's bootstrapping instructions.

--- scripts/win/clearenv.cmd
+++ scripts/win/clearenv.cmd
@@ -1,4 +1,4 @@
-@rem $Id: clearenv.cmd,v 1.2 2006-12-30 11:36:38 jkrell Exp $
+@rem $Id: clearenv.cmd,v 1.3 2007-08-11 16:35:21 jkrell Exp $
 
 @if "%_echo%" == "" @echo off
 
@@ -8,7 +8,7 @@
 @rem
 
 for %%i in (ACTION ARGS BUILDGLOBAL BUILDLOCAL CLEANGLOBAL       ) do if defined %%i set %%i=
-for %%i in (CLEANLOCAL CM3 CM3BINSEARCHPATH CM3LIBSEARCHPATH     ) do if defined %%i set %%i=
+for %%i in (CLEANLOCAL CM3BINSEARCHPATH CM3LIBSEARCHPATH         ) do if defined %%i set %%i=
 for %%i in (CM3ROOT CM3VERSION EXE GCC_BACKEND GREP IGNORE_MISS  ) do if defined %%i set %%i=
 for %%i in (INSTALLROOT M3BUILD M3GDB M3OSTYPE M3SHIP PKG_ACTION ) do if defined %%i set %%i=
 for %%i in (PKGS PKGSDB REALCLEAN ROOT SCRIPTS SYSINFO_DONE      ) do if defined %%i set %%i=
