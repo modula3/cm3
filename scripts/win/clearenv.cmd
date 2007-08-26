2b4bf016cf8b22cf21fcc0f8af2b94dd1b98d9f1
Jay Michael Krell <jay.krell@cornell.edu>
2007-08-26 10:44:17 +0000

    cleanup mess made by make-dist

--- scripts/win/clearenv.cmd
+++ scripts/win/clearenv.cmd
@@ -1,4 +1,4 @@
-@rem $Id: clearenv.cmd,v 1.3 2007-08-11 16:35:21 jkrell Exp $
+@rem $Id: clearenv.cmd,v 1.4 2007-08-26 10:44:17 jkrell Exp $
 
 @if "%_echo%" == "" @echo off
 
@@ -14,3 +14,16 @@ for %%i in (INSTALLROOT M3BUILD M3GDB M3OSTYPE M3SHIP PKG_ACTION ) do if defined
 for %%i in (PKGS PKGSDB REALCLEAN ROOT SCRIPTS SYSINFO_DONE      ) do if defined %%i set %%i=
 for %%i in (SHIP SL SYSLIBDIR SYSLIBS TAR TARGET TMPDIR          ) do if defined %%i set %%i=
 for %%i in (CM3_SYSCALL_WRAPPERS_EXIST                           ) do if defined %%i set %%i=
+call :Clear INSTALLROOT_PREVIOUS
+call :Clear INSTALLROOT_COMPILER_WITH_PREVIOUS
+call :Clear INSTALLROOT_COMPILER_WITH_SELF
+call :Clear INSTALLROOT_MIN
+call :Clear INSTALLROOT_STD
+call :Clear INSTALLROOT_CORE
+call :Clear INSTALLROOT_BASE
+
+goto :eof
+
+:Clear
+if defined %1 set %1=
+goto :eof
