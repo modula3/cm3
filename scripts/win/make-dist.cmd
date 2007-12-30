80f366577d73e49a7bb96e01674421ac597bb9f4
Jay Michael Krell <jay.krell@cornell.edu>
2007-12-30 07:19:26 +0000

    delete non-critical *.m3 and .m3web files from distribution to save space   maybe the .m3 files are meant as examples or for source debugging?
    require STAGE to not exist ahead of time, unless
     there is a parameter, which is the scenario of like
     make-dist goto_zip to skip ahead to making the .zip
    
    get the actual commands into the logs, of course

--- scripts/win/make-dist.cmd
+++ scripts/win/make-dist.cmd
@@ -1,4 +1,4 @@
-@rem $Id: make-dist.cmd,v 1.19 2007-12-29 19:20:30 jkrell Exp $
+@rem $Id: make-dist.cmd,v 1.20 2007-12-30 07:19:26 jkrell Exp $
 
 @if "%_echo%" == "" @echo off
 
@@ -14,6 +14,12 @@ call %~dp0pkgcmds || call :ReportFatalError || exit /b 1
 
 if not defined STAGE (
     set STAGE=%TEMP%\cm3\%~n0\%random%
+)
+if /i "%1" == "" (
+    if exist %STAGE% (
+        echo ERROR: %%STAGE%% ^(%STAGE%^) must not exist ahead of time, in order that we may be safely destructive to it.
+        exit /b 1
+    )
 )
 @mkdir %STAGE%\logs 2>nul
 @set LogCounter=0
@@ -225,6 +231,11 @@ for /f %%a in ('dir /s/b/a-d *.pdb') do move %%a symbols
 
 del *.bz2 *.zip *.exe *.tar
 
+del /s *.m3
+del /s .m3web
+rem Can be useful for bootstrapping standalone cm3.exe in future.
+rem del /s *.lib.sa
+
 rem set symbols=cm3-min-%M3OSTYPE%-%TARGET%-%CM3VERSION%-symbols.tar.bz2
 rem tar cfvj %symbols% symbols
 
@@ -304,6 +315,7 @@ goto :eof
     echo %TIME%>>%STAGE%\logs\%LogCounter%_%~n1.log
     echo.>> %STAGE%\logs\all.log
     echo.>> %STAGE%\logs\%LogCounter%_%~n1.log
+    echo %x% >> %STAGE%\logs\%LogCounter%_%~n1.log
     echo %x% ^>^> %STAGE%\logs\%LogCounter%_%~n1.log
     call %x% >> %STAGE%\logs\%LogCounter%_%~n1.log || (
         echo %TIME%>>%STAGE%\logs\%LogCounter%_%~n1.log
