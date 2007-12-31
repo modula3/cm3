6be99f63cb8bd734af69583a6ca86281fd36d36a
jkrell <>
2007-12-31 12:00:40 +0000

    better

--- scripts/python/chext.py
+++ scripts/python/chext.py
@@ -4,4 +4,10 @@ import glob
 import os
 
 for a in glob.glob("*.sh"):
-	os.rename(a, os.path.splitext(a)[0] + ".py")
+    base = os.path.splitext(a)[0]
+    dest = (base + ".py")
+    if os.path.isfile(dest):
+        print("skipping " + dest + " because it already exists")
+    else:
+        print(a + " => " + dest)
+        os.rename(a, dest + ".py")
