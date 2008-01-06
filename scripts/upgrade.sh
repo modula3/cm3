a34fcfd67d9e0723ad4994619d31501bbdd2ce49
jkrell <>
2008-01-06 18:53:22 +0000

    typo in comment

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -47,7 +47,7 @@ echo "$ROOT/scripts/do-pkg.sh" "buildship" ${P}
 
 # Now build the compiler with the installed version of the runtime;
 # do _not_ compile m3core and libm3 here.
-# We start with the fron end...
+# We start with the front end...
 P=""
 P="${P} m3middle"
 [ "${M3OSTYPE}" = "WIN32" ] && P="${P} m3objfile"
