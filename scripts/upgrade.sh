e64ad72d5b84c6a151014290b15295e1f87e38fb
Olaf Wagner <wagner@elegosoft.com>
2008-01-08 23:02:49 +0000

    redo some of the recent changes while keeping some good ideas in upgrade.sh (don't use globals and confusing procedures) also fix several problems and adapt code layout

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: upgrade.sh,v 1.11 2008-01-07 09:04:37 jkrell Exp $
+# $Id: upgrade.sh,v 1.12 2008-01-08 23:02:49 wagner Exp $
 
 if [ -n "$ROOT" -a -d "$ROOT" ] ; then
   sysinfo="$ROOT/scripts/sysinfo.sh"
@@ -32,102 +32,39 @@ ${GEN_OPTS}"
 show_usage $@
 
 OPTIONS=`extract_options $@`
-#ACTION=`map_action $@`
-#ADDARGS=`add_action_opts $@`
 
-#
-# We pass around P as a global
-# because we want to insert realclean or buildship
-# between $@ and P as the previous version did.
-#
-
-Run()
-{
-    echo "$@"
-    "$@"
+run() {
+  echo "$@"
+  ( "$@" )
 }
-
-DoPackage()
-{
-    P=`FilterPackages "${P}"`
-    [ "${P}" = "" ] || Run "$ROOT/scripts/do-pkg.sh" "$@" "${P}"
-}
-
-Clean()
-{
-	CM3_ALL=yes DoPackage "$@" "realclean"
-}
-
-BuildShip()
-{
-    DoPackage "$@" "buildship"
-}
-
-CleanBuildShip()
-{
-    Clean "$@"
-    BuildShip "$@"
-}
-
-#
-# clean everything
-#
-P=" \
-m3bundle \
-m3middle \
-m3quake \
-patternmatching \
-cminstall \
-m3middle \
-m3objfile \
-m3linker \
-m3back \
-m3staloneback \
-m3front \
-m3quake \
-cm3 \
-mklib \
-m3cc \
-"
-
-Clean "$@"
 
 # Try to make sure that m3bundle and cminstall are available.
 # These are only needed in case of cm3.cfg updates later. We need
 # to build them in advance though, but ignore errors in this step.
 P="m3bundle m3middle m3quake patternmatching cminstall"
-BuildShip "$@"
+run "$ROOT/scripts/do-pkg.sh" "$@" "buildship" ${P} || true
 
 # Now build the compiler with the installed version of the runtime;
 # do _not_ compile m3core and libm3 here.
 # We start with the front end...
-P="\
-m3middle \
-m3objfile \
-m3linker \
-m3back \
-m3staloneback \
-m3front \
-m3quake \
-cm3 \
-mklib \
-"
+P=`FilterPackages m3middle m3objfile m3linker m3back m3staloneback \
+   m3front m3quake cm3 mklib`
+run "$ROOT/scripts/do-pkg.sh" "$@" "buildship" ${P} || exit 1
 
-BuildShip "$@" || exit 1
-
-# ... and continue with the backend, if needed
-P="m3cc"
-BuildShip "$@" || exit 1
+if [ "${GCC_BACKEND}" = yes ]; then
+  # ... and continue with the backend, if needed
+  run "$ROOT/scripts/do-pkg.sh" "$@" "build" m3cc || exit 1
+fi
 
 # Up to now, the compiler binaries have not been installed.
 # We do this now but keep backups of the old ones.
-Run "$ROOT/scripts/install-cm3-compiler.sh" $OPTIONS upgrade || exit 1
+run "$ROOT/scripts/install-cm3-compiler.sh" $OPTIONS upgrade || exit 1
 
 # Now try the new compiler but building the core system (without
 # m3cc, as this is written in C) from scratch with it.
+OMIT_GCC=yes run "$ROOT/scripts/do-cm3-core.sh" "$@" "realclean" || exit 1
 
-OMIT_GCC=yes Run "$ROOT/scripts/do-cm3-core.sh" "$@" "realclean"
-OMIT_GCC=yes Run "$ROOT/scripts/do-cm3-core.sh" "$@" "buildship" || (
+run "$ROOT/scripts/do-cm3-core.sh" "$@" "buildship" || (
 
   # If we fail, this may be caused by incompatible changes in cm3.cfg.
   # We try to install a new one with cminstall...
@@ -143,13 +80,11 @@ OMIT_GCC=yes Run "$ROOT/scripts/do-cm3-core.sh" "$@" "buildship" || (
 
   echo "trying recompile after cleanup..."
 
-  OMIT_GCC=yes Run "$ROOT/scripts/do-cm3-core.sh" "realclean" || exit 1
+  OMIT_GCC=yes run "$ROOT/scripts/do-cm3-core.sh" "$@" "realclean" || exit 1
 
-  Run "$ROOT/scripts/do-cm3-core.sh" "$@" "buildship"
+  run "$ROOT/scripts/do-cm3-core.sh" "$@" "buildship"
 
 ) || exit 1
 
 # If everything has been successfull, we do another compiler upgrade.
-Run "$ROOT/scripts/install-cm3-compiler.sh" $OPTIONS upgrade || exit 1
-
-echo "$0: Success."
+run "$ROOT/scripts/install-cm3-compiler.sh" $OPTIONS upgrade || exit 1
