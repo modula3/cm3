8c12a1523ace89327c73a904bc53bbf7a4d6d0e3
Jay Michael Krell <jay.krell@cornell.edu>
2008-01-13 07:25:03 +0000

    smush things down unnecessarily, and fix for use with Visual C++ 2.0  (signature wasn't being split from function name)

--- m3-win/import-libs/src/t.cmd
+++ m3-win/import-libs/src/t.cmd
@@ -1,5 +1,14 @@
 
-del t.obj t.o a.dll b.dll a.exe b.exe 2>nul
+del t.obj t.o a.dll b.dll a.exe b.exe *.pdb 2>nul
+
+if exist ..\nt386 (
+    mkdir ..\nt386gnu 2>nul
+    copy /y ..\nt386\*.lib ..\nt386gnu
+)
+if exist ..\nt386gnu (
+    mkdir ..\nt386 2>nul
+    copy /y ..\nt386gnu\*.lib ..\nt386
+)
 
 gcc -c t.c
 ld t.o -o a.dll -lc -trace ../NT386GNU/user32.lib ../NT386GNU/winspool.lib ../NT386GNU/kernel32.lib -dll
