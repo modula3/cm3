11cc7c6242f4bb5741b38ab4f19fb6585adc645f
Jay Michael Krell <jay.krell@cornell.edu>
2008-01-14 04:59:42 +0000

    undo -- flubbed the submit comment, diff was ok

--- scripts/win/clearenv.cmd
+++ scripts/win/clearenv.cmd
@@ -1,4 +1,4 @@
-@rem $Id: clearenv.cmd,v 1.7 2008-01-14 04:58:27 jkrell Exp $
+@rem $Id: clearenv.cmd,v 1.8 2008-01-14 04:59:42 jkrell Exp $
 
 @if "%_echo%" == "" @echo off
 
@@ -12,7 +12,7 @@ for %%i in (CLEANLOCAL CM3BINSEARCHPATH CM3LIBSEARCHPATH         ) do if defined
 for %%i in (CM3ROOT CM3VERSION EXE GCC_BACKEND GREP IGNORE_MISS  ) do if defined %%i set %%i=
 for %%i in (INSTALLROOT M3BUILD M3GDB M3OSTYPE M3SHIP PKG_ACTION ) do if defined %%i set %%i=
 for %%i in (PKGS PKGSDB REALCLEAN ROOT SCRIPTS SYSINFO_DONE      ) do if defined %%i set %%i=
-for %%i in (SHIP SL SYSLIBDIR SYSLIBS TAR TMPDIR                 ) do if defined %%i set %%i=
+for %%i in (SHIP SL SYSLIBDIR SYSLIBS TAR TARGET TMPDIR          ) do if defined %%i set %%i=
 for %%i in (CM3_SYSCALL_WRAPPERS_EXIST                           ) do if defined %%i set %%i=
 call :Clear INSTALLROOT_PREVIOUS
 call :Clear INSTALLROOT_COMPILER_WITH_PREVIOUS
