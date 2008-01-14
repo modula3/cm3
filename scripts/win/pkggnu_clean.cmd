eefa6da41e9ee6d13ec181e3dce4d057847f536d
jkrell <>
2008-01-14 04:41:41 +0000

    clean out NT386GNU package store

---
+++ scripts/win/pkggnu_clean.cmd
@@ -0,0 +1,19 @@
+@rem $Id$
+
+@rem
+@rem This is a little helper to bring up NT386GNU.
+@rem It cleans out all the INSTALLROOT\pkg\*\NT386GNU directories.
+@rem
+
+@setlocal
+
+@call %~dp0clearenv || exit /b 1
+@call %~dp0sysinfo || exit /b 1
+@call %~dp0pkgcmds || exit /b 1
+
+@echo on
+echo INSTALLROOT is %INSTALLROOT%
+
+for /d %%a in (%INSTALLROOT%\pkg\*) do (
+    rmdir /q/s %%a\NT386GNU
+)
