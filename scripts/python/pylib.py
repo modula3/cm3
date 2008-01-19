f65b0a8171cf661a957390a1c9c757bd3e574e88
Jay Michael Krell <jay.krell@cornell.edu>
2008-01-19 01:48:00 +0000

    workaround crash seen booting with 5.1.3 that doesn't repro in debugger; better autoconf for booting NT386GNU; dos2unix of previous

--- scripts/python/pylib.py
+++ scripts/python/pylib.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-# $Id: pylib.py,v 1.34 2008-01-19 00:07:07 jkrell Exp $
+# $Id: pylib.py,v 1.35 2008-01-19 01:48:00 jkrell Exp $
 
 import os
 from os import getenv
@@ -367,6 +367,15 @@ DEFS += " -DCM3_VERSION_TEXT=%(Q)s%(CM3VERSION)s%(Q)s"
 DEFS += " -DCM3_VERSION_NUMBER=%(Q)s%(CM3VERSIONNUM)s%(Q)s"
 DEFS += " -DCM3_LAST_CHANGED=%(Q)s%(CM3LASTCHANGED)s%(Q)s"
 DEFS = (DEFS % vars())
+
+#
+# workaround crash when booting from 5.1.3 that is
+# difficult to debug -- crashes earlier in debugger
+# without this switch, no repro with this switch
+#
+# This has no effect with current tools/libraries.
+#
+DEFS += " @M3novm"
 
 if BuildArgs:
     BuildArgs = " " + BuildArgs
@@ -1526,10 +1535,11 @@ def SetupEnvironment():
 
         _SetupEnvironmentVariableAll("PATH", ["cl", "link"], VCBin)
 
-    elif Target == "NT386GNU":
+    if Target == "NT386GNU" or (Target == "NT386" and GCC_BACKEND):
 
-        _ClearEnvironmentVariable("LIB")
-        _ClearEnvironmentVariable("INCLUDE")
+        if Target == "NT386GNU":
+            _ClearEnvironmentVariable("LIB")
+            _ClearEnvironmentVariable("INCLUDE")
 
         _SetupEnvironmentVariableAll(
             "PATH",
