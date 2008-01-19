ebb6f35c5bfd8122fb4e9559775c5a827fcb7b4f
jkrell <>
2008-01-19 04:46:30 +0000

    fix

--- scripts/python/bootntgnu.py
+++ scripts/python/bootntgnu.py
@@ -26,6 +26,7 @@ DoPackage(argv_RealClean, PackageSets["all"]) or sys.exit(1)
 DoPackage(argv_BuildShip, ["m3cc"]) or sys.exit(1)
 
 DoPackage(
+    argv_BuildShip, [
     "m3core",
     "libm3",
     "import-libs",
@@ -41,8 +42,7 @@ DoPackage(
     "m3quake",
     #"mklib", # not yet working
     "cm3",
-    argv_BuildShip,
-    ) or sys.exit(1)
+    ]) or sys.exit(1)
 
 # not yet, not working
 # ShipCompiler() or sys.exit(1)
