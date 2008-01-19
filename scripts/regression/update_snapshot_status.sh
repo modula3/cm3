466399dafdf2a810a00781a6cabe0ff781418f51
Olaf Wagner <wagner@elegosoft.com>
2008-01-19 14:16:35 +0000

    add snapshot index generation for WWW
    added: scripts/regression/update_snapshot_status.sh

---
+++ scripts/regression/update_snapshot_status.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+SNAPS=${SNAPS:-/var/www/modula3.elegosoft.com/cm3/snaps}
+OSTYPE=${OSTYPE:-POSIX}
+FNPAT1=${FNPAT1:-"cm3-min-${OSTYPE}-"}
+FNPATSUF=${FNPATSUF:-.tgz}
+FNPATLS=${FNPAT:-${FNPAT1}'*-*'${FNPATSUF}}
+INDEX=${INDEX:-snapshot-index.html}
+cd $SNAPS || exit 1
+
+TARGETS=`ls -1 ${FNPATLS} |
+  sed -e "s/${FNPAT1}\([A-Za-z0-6_]*\)-.*${FNPATSUF}/\1/" |
+  sort -u`
+
+if [ -f "${INDEX}" ]; then
+  mv ${INDEX} ${INDEX}.old
+fi
+cat > ${INDEX} << EOF
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+    <title>CM3 Snapshots</title>
+    <META HTTP-EQUIV="Content-Type" CONTENT="text/html">
+    <META HTTP-EQUIV="Content-Style-Type" CONTENT="text/css">
+    <META HTTP-EQUIV="Resource-type" CONTENT="document"> 
+    <META HTTP-EQUIV="Reply-to" CONTENT="m3-support@elego.de"> 
+    <LINK HREF="normal.css" REL="stylesheet" TYPE="text/css">
+    <META NAME="robots" content="noindex">
+  </head>
+
+  <body bgcolor="#ffffff">
+    <h2>CM3 Snapshots</h2>
+
+EOF
+
+for t in ${TARGETS}; do
+  all=`ls -1 ${FNPAT1}${t}-*${FNPATSUF}`
+  last=`ls -1 ${FNPAT1}${t}-*${FNPATSUF} | tail -1`
+  last10=`ls -1 ${FNPAT1}${t}-*${FNPATSUF} | tail -10`
+  ln -sf "${last}" "${FNPAT1}${t}${FNPATSUF}"
+  echo "<h3>Target Platform ${t}</h3>"
+  for f in ${last10}; do
+    echo "<a href=\"$f\">$f</a><br>"
+  done
+done >> ${INDEX}
+
+cat >> ${INDEX} <<EOF
+    <hr>
+    <address><a href="mailto:m3-support{at}elego.de">m3-support{at}elego.de</a></address>
+<!-- Created: `date` -->
+  </body>
+</html>
+EOF
+
+rm -f ${INDEX}.old
