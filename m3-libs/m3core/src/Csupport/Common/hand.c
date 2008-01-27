47a3c80ee148dd6392414c478a78e1d6bafa972d
Anthony Hosking <hosking@cs.purdue.edu>
2008-01-27 15:35:22 +0000

    Remove ^M.
    I wonder if Jay's recent changes are what is breaking the set operation
    regression tests.

--- m3-libs/m3core/src/Csupport/Common/hand.c
+++ m3-libs/m3core/src/Csupport/Common/hand.c
@@ -1,4 +1,3 @@
-
 /* Copyright (C) 1992, Digital Equipment Corporation        */
 /* All rights reserved.                                     */
 /* See the file COPYRIGHT for a full description.           */
