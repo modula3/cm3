0693e18b0c1c208f0415f8e4a08419918ded21f9
Jay Michael Krell <jay.krell@cornell.edu>
2008-02-10 04:37:03 +0000

    make it not optional

--- m3-libs/m3core/src/unix/cygwin/Usignal.i3.c
+++ m3-libs/m3core/src/unix/cygwin/Usignal.i3.c
@@ -1,4 +1,4 @@
-/* $Id: Usignal.i3.c,v 1.14 2008-02-10 04:36:18 jkrell Exp $ */
+/* $Id: Usignal.i3.c,v 1.15 2008-02-10 04:37:03 jkrell Exp $ */
 
 #include <signal.h>
 #include <stdio.h>
@@ -21,7 +21,7 @@ int main()
 "(*      modified on Tue Feb 14 20:58:12 GMT 1995 by rrw1000@cam.ac.uk *)", 0,
 "(*      modified on Tue Mar  2 17:18:02 PST 1993 by muller            *)", 0,
 "", 0,
-"(* $Id: Usignal.i3.c,v 1.14 2008-02-10 04:36:18 jkrell Exp $ *)", 0,
+"(* $Id: Usignal.i3.c,v 1.15 2008-02-10 04:37:03 jkrell Exp $ *)", 0,
 "", 0,
 "(* This file was generated from " __FILE__ ". Do not edit it. *)", 0,
 "", 0,
@@ -89,9 +89,7 @@ int main()
 "", 0,
 "  (* System V definitions *)", 0,
 "  (* SIGCLD = SIGCHLD; *)", 0,
-#ifdef SIGABRT
 "  SIGABRT = 16_%08x;", SIGABRT,
-#endif
 "", 0,
 "CONST", 0,
 "  SIGSET_NWORDS = 16_%08x;", sizeof(sigset_t) / sizeof(int),
