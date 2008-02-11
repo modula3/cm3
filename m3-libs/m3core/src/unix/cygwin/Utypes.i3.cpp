102cbb4690eb7707cabac71688c0b1c719d579d0
jkrell <>
2008-02-11 00:28:44 +0000

    switch to C++ for compile type dispatch

--- m3-libs/m3core/src/unix/cygwin/Utypes.i3.cpp
+++ m3-libs/m3core/src/unix/cygwin/Utypes.i3.cpp
@@ -1,9 +1,33 @@
 /* $Id$ */
 
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
 
-#define SIZE(a) (((sizeof(a) + sizeof(int) - 1)) / sizeof(int))
+static void AssertTypes()
+{
+    dev_t * a = 0;
+    unsigned long * b = 0;
+
+    b = a;
+    a = b;
+}
+
+const char* PickType(unsigned size)
+{
+    switch (size)
+    {
+    case 1:
+        return "u_char";
+    case 2:
+        return "u_short";
+    case 4:
+        return "u_int";
+    default:
+        printf("unknown size %u\n", size);
+        exit(1);
+    }
+}
 
 int main()
 {
@@ -75,7 +99,7 @@ int main()
 "  swblk_t      = long;", 0,
 "  size_t       = u_int;", 0,
 "  time_t       = long;", 0,
-"  dev_t        = long_long_uint;", 0,
+"  dev_t        = u_long;", 0,
 "  off_t        = long;", 0,
 "  paddr_t      = long;                (* sys V compatibility *)", 0,
 "  key_t        = long;                (* sys V compatibility *)", 0,
