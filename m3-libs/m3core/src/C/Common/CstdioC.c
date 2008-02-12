8b57bac198a4fa4515f6142394d774ea3cd6e5c5
Jay Michael Krell <jay.krell@cornell.edu>
2008-02-12 13:21:27 +0000

    reformat

--- m3-libs/m3core/src/C/Common/CstdioC.c
+++ m3-libs/m3core/src/C/Common/CstdioC.c
@@ -29,9 +29,21 @@ FILE* Cstdio__stdin(void)
 
 #if defined(_WIN32) && !defined(__CYGWIN__)
 
-/* remove dependence on oldnames.lib */
-int __cdecl pclose(FILE * a) { return _pclose(a); }
-FILE * __cdecl popen(const char * a, const char * b) { return _popen(a, b); }
-int __cdecl unlink(const char * a) { return _unlink(a); }
+/* remove dependence on oldnames.lib interesting? */
+
+int __cdecl pclose(FILE * a)
+{
+    return _pclose(a);
+}
+
+FILE * __cdecl popen(const char * a, const char * b)
+{
+    return _popen(a, b);
+}
+
+int __cdecl unlink(const char * a)
+{
+    return _unlink(a);
+}
 
 #endif
