8da6f23a9e447888cf47cdfce5d4c0fd482e173c
Jay Michael Krell <jay.krell@cornell.edu>
2008-03-16 14:47:38 +0000

    use pkginfo.txt to define the packages sets, and the ordering implement centralized filtering

--- scripts/win/do-cm3-min.cmd
+++ scripts/win/do-cm3-min.cmd
@@ -1,13 +1,18 @@
-@rem $Id: do-cm3-min.cmd,v 1.6 2007-12-31 10:09:31 jkrell Exp $
+@rem $Id: do-cm3-min.cmd,v 1.7 2008-03-16 14:47:37 jkrell Exp $
 
 @echo off
-setlocal
+
+SetLocal EnableExtensions EnableDelayedExpansion
+
 call %~dp0clearenv || exit /b 1
 call %~dp0sysinfo || exit /b 1
 call %~dp0pkgcmds || exit /b 1
 
 call %~dp0pkgcmds map_action %* || exit /b 1
 
-call %~dp0do-pkg %* import-libs m3core libm3 || exit /b 1
+set P=
+for /f "tokens=1" %%a in ('findstr /c:" min" %~dp0..\pkginfo.txt') do set P=!P! %%a
+
+call %~dp0do-pkg %* %P% || exit /b 1
 
 @echo %~n0 : Success.
--- scripts/win/do-cm3-std.cmd
+++ scripts/win/do-cm3-std.cmd
@@ -1,8 +1,8 @@
-@rem $Id: do-cm3-std.cmd,v 1.4 2007-12-31 10:09:31 jkrell Exp $
+@rem $Id: do-cm3-std.cmd,v 1.5 2008-03-16 14:47:37 jkrell Exp $
 
 @if "%_echo%" == "" @echo off
 
-setlocal
+SetLocal EnableExtensions EnableDelayedExpansion
 
 call %~dp0clearenv || exit /b 1
 call %~dp0sysinfo || exit /b 1
@@ -10,10 +10,8 @@ call %~dp0pkgcmds || exit /b 1
 
 call %~dp0pkgcmds map_action %* || exit /b 1
 
-call %~dp0def-std-pkgs || (
-	echo error : def-std-pkgs failed
-	exit /b 1
-)
+set P=
+for /f "tokens=1" %%a in ('findstr /c:" std" %~dp0..\pkginfo.txt') do set P=!P! %%a
 
 REM
 REM UNDONE
