3e2740be0cce4c41b04733409684910fcaec911d
jkrell <>
2008-04-21 17:06:12 +0000

    fix

--- scripts/python/upgrade.py
+++ scripts/python/upgrade.py
@@ -75,6 +75,6 @@ DoPackage(argv_BuildShip, [ "import-libs", "m3bundle", "m3middle", "m3quake", "m
                             "cm3", "mklib", "m3cc", "m3core", "libm3",
     ]) or sys.exit(1)
 
-ShipCompiler() or sys.exit(1)
+ShipFront() or sys.exit(1)
 
 print("%s: Success." % os.path.basename(sys.argv[0]))
