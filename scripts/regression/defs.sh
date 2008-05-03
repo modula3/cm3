0201822a7c2eead0bc9b98a9673bcc4c86bfaf9f
wagner <>
2008-05-03 21:00:46 +0000

    always use posix tools for m3tests during regression

--- scripts/regression/defs.sh
+++ scripts/regression/defs.sh
@@ -613,7 +613,7 @@ test_m3tests()
 
   cd "${WS}/cm3/m3-sys/m3tests" || exit 1
 
-  cm3 -build -DHTML 2>${M3TERR} | tee ${M3TOUT}
+  cm3 -build -DHTML -DUSE_POSIX_TOOLS 2>${M3TERR} | tee ${M3TOUT}
   WWWDEST=${WWWDEST:-${WWWSERVER}:/var/www/modula3.elegosoft.com/cm3/m3tests}
   if [ -r "m3tests.html" ]; then
     scp "m3tests.html" "${WWWDEST}/m3tests-${CM3_TARGET}-${DS}.html" \
