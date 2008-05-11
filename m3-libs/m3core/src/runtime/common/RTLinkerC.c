f9fb1ca4ee8aa75862e7e292c4cd44f315a7f6ea
jkrell <>
2008-05-11 21:29:38 +0000

    remove dependency on header missing from a small number of build environments

--- m3-libs/m3core/src/runtime/common/RTLinkerC.c
+++ m3-libs/m3core/src/runtime/common/RTLinkerC.c
@@ -1,7 +1,3 @@
-#ifdef _WIN32
-#include <windows.h>
-#endif
-
 /*
 EnvFromMain is either char** from main, or char* GetEnvironmentStringsA from WinMain.
 Rather than make a coordinated compiler/runtime change, we just ignore
@@ -11,6 +7,7 @@ One additional copy of the environment variables is leaked per .exe/.dll.
 void* RTLinker__GetEnvironmentStrings (void* EnvFromMain)
 {
 #ifdef _WIN32
+	__declspec(dllimport) char** __stdcall GetEnvironmentStringsA(void);
     return GetEnvironmentStringsA ();
 #else
     return EnvFromMain;
