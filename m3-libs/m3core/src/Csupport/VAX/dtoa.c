75939019ebd0d7571979920ac808cab9f7aacc56
Jay Michael Krell <jay.krell@cornell.edu>
2008-05-25 03:02:17 +0000

    cleanup a lot of duplication and dead code (float.h were dead) 32bit vs. 64bit can be probed via limits.h big-endian and little-endian are the resulting two versions of dtoa.c  plus VAX

--- m3-libs/m3core/src/Csupport/VAX/dtoa.c
+++ m3-libs/m3core/src/Csupport/VAX/dtoa.c
@@ -2,15 +2,9 @@
 /* All rights reserved.                                                      */
 /* See the file COPYRIGHT for a full description.                            */
 
-/* Last modified on Tue Feb 11 14:23:50 PST 1992 by muller                   */
-
-#ifndef KR_headers
 #define KR_headers
-#endif
 
-#ifndef VAX
 #define VAX
-#endif
 
 #define MULTIPLE_THREADS
 #define ACQUIRE_DTOA_LOCK(n) RTOS__LockHeap()
--- m3-libs/m3core/src/Csupport/big-endian/dtoa.c
+++ m3-libs/m3core/src/Csupport/big-endian/dtoa.c
@@ -2,18 +2,8 @@
 /* All rights reserved.                                                      */
 /* See the file COPYRIGHT for a full description.                            */
 
-/* Last modified on Fri Aug 13 09:02:20 PDT 1993 by kalsow                   */
-/*      modified on Tue Feb 11 14:23:53 PST 1992 by muller                   */
-
-#ifndef KR_headers
 #define KR_headers
-#endif
-
-#ifndef IEEE_8087
-#define IEEE_8087
-#endif
-
-#define Long int
+#define IEEE_MC68k
 
 #define MULTIPLE_THREADS
 #define ACQUIRE_DTOA_LOCK(n) RTOS__LockHeap()
