d6fa771b4930695d5ee5b745a8a2038e55f9ed81
Jay Michael Krell <jay.krell@cornell.edu>
2008-08-14 15:05:54 +0000

    go back a version unless proven to be needed

--- scripts/find-packages.sh
+++ scripts/find-packages.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: find-packages.sh,v 1.6 2008-08-14 14:54:07 jkrell Exp $
+# $Id: find-packages.sh,v 1.7 2008-08-14 15:05:54 jkrell Exp $
 
 if [ -n "$ROOT" -a -d "$ROOT" ] ; then
   sysinfo="$ROOT/scripts/sysinfo.sh"
@@ -19,7 +19,7 @@ fi
 
 (
   cd "$ROOT" &&  find . -follow -type f -name m3makefile -print | 
-  grep /src/m3makefile | grep -v examples/web | grep -v _darcs | sed -e 's;/src/m3makefile$;;' | 
+  grep /src/m3makefile | grep -v _darcs | sed -e 's;/src/m3makefile$;;' | 
   sort | uniq | sed -e "s;^./;;"
 ) > "$PKGSDB"
 
