61b85df34addb392c7684deaa3fbd0a01163b644
jkrell <>
2008-11-09 08:05:35 +0000

    this is Python, not Quake

--- scripts/python/pylib.py
+++ scripts/python/pylib.py
@@ -2005,9 +2005,9 @@ def CopyConfigForDistribution(To):
         if os.path.isfile(File):
             print(File + " => " + To + "\n")
             CopyFile(File, To)
-    foreach a in ["Unix", "cm3cfg"]:
-        print(a + ".common => " + To + "\n")
-        CopyFile(os.path.join(a, "config-no-install", a + ".common"), To)
+    for a in ["Unix.common", "cm3cfg.common"]:
+        print(a + " => " + To + "\n")
+        CopyFile(os.path.join(a, "config-no-install", a), To)
     open(os.path.join(To, "cm3.cfg"), "w").write("include(\"" + Config + "\")\n")
     return True
 
