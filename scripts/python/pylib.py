8d87568e11205d5e0e9e343a4ce3a3608b97dcb6
jkrell <>
2008-11-12 14:36:37 +0000

    turning off gc here isn't where it is 'needed' anyway, it is the invocations of 'stubgen'

--- scripts/python/pylib.py
+++ scripts/python/pylib.py
@@ -531,7 +531,6 @@ elif UName.startswith("linux"):
         Host = "PPC_LINUX"
     elif UNameArchM == "x86_64":
         Host = "AMD64_LINUX"
-        TurnOffGarbageCollection = True
     elif UNameArchM == "sparc64":
         Host = "SPARC32_LINUX"
     else:
@@ -770,8 +769,7 @@ Root = NativeRoot
 DEFS += " @M3novm"
 
 if TurnOffGarbageCollection:
-    #DEFS += " @M3nogc"
-    DEFS += " @M3paranoidgc"
+    DEFS += " @M3nogc"
 
 #
 #
