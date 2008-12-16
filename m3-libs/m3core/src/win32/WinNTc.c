6911107c8f80a4ad83b7e90d71e6675a206ebd5a
Jay Michael Krell <jay.krell@cornell.edu>
2008-12-16 15:31:48 +0000

    remove dependency on newer headers, so that e.g. I386_CYGWIN (aka NT386GNU) builds

--- m3-libs/m3core/src/win32/WinNTc.c
+++ m3-libs/m3core/src/win32/WinNTc.c
@@ -6,14 +6,27 @@
 extern "C" {
 #endif
 
+#if defined(_WIN32) || defined(__CYGWIN__)
 
 void m3_MemoryBarrier(void)
 {
-#if defined(_WIN32) || defined(__CYGWIN__)
+#if defined(MemoryBarrier)
+    /* IA64 and AMD64 make this a macro, good, we can test for it. */
     MemoryBarrier();
+#else
+    /* x86 MemoryBarrier is plain exchange, so InterlockedExchange should work */
+    static volatile long a;
+    InterlockedExchange(&a, a);
 #endif
 }
 
+#else
+
+void m3_MemoryBarrier(void)
+{
+}
+
+#endif
 
 #ifdef __cplusplus
 }
