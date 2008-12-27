99ada6069c84b3519b5143e963c4b77b72f114e4
Jay Michael Krell <jay.krell@cornell.edu>
2008-12-27 00:35:05 +0000

    file sizes and offsets should always be 64 bits functions that traffic in file sizes offsets should always have "64" at the end?

---
+++ m3-libs/m3core/src/unix/Common/m3unix.h
@@ -0,0 +1,17 @@
+/* TBD: what is the meaning of each of these? */
+#define __USE_LARGEFILE64
+#define _FILE_OFFSET_BITS 64
+#define _LARGEFILE_SOURCE
+#define _LARGEFILE64_SOURCE
+#define __USE_FILE_OFFSET64
+
+#ifndef _MSC_VER
+#define __int64 long long
+#endif
+
+#include <stddef.h>
+
+typedef ptrdiff_t INTEGER;
+typedef __int64 LONGINT;
+typedef __int64 INT64;
+typedef unsigned __int64 UINT64;
