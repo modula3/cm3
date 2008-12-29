c8636fb09cfa4842b2a2252443faa450f8b9dbc4
Jay Michael Krell <jay.krell@cornell.edu>
2008-12-29 10:23:41 +0000

    Remove the only use of Umman.i3 in systems without user thread support.
    As a side effect, since one RTOS.GetMemory shall be rewritten in C, so
    shall they all. It would be nice if the <*external*> was not needed
    as long as the names were chosen right, and I avoided m3makefile tricks
    of only inserting <*external*> in one of the three implementations.
    
    Punt on writing the "entire" few lines of Win32 GetMemory in C,
    and just a smaller subset. The whole thing is not a big deal, but
    does it buy much or anything?
    
    As a bonus, fix Win32 "page size" so that there is no "rounding loss",
     noticed watching the allocations to see if the "simplification"
    resulted
     in much lost adjacency and hole production. In the sample of watching
     mklib be built, the second allocation is indeed no adjacent to the
    first,
     but allocations are adjacent thereafter.
     Adjacency is desirable because RTAllocater/Collecter builds data
    structures
      spanning the heap, including holes. The historic Win32 allocater
    reserved
      64M address space up front, and then individual VirtualAlloc calls
    thereafter.
      Implementation here just calls VirtualAlloc.
      HeapAlloc and calloc should also be viable, and calloc is portable,
    but the
       allocations are large, and never freed, so maybe not.
    
    As a bonus, found RTCollector.i3 bug for pagesize=64K (or larger?)
    
    Also eliminate an unnecessary global in Win32/RTOS.m3.

---
+++ m3-libs/m3core/src/runtime/WIN32/RTOSc.c
@@ -0,0 +1,58 @@
+#if 0
+
+#include <stdlib.h>
+
+void* RTOS__GetMemory(size_t Size)
+{
+    return calloc(Size, 1);
+}
+
+#elif 0
+
+#include <windows.h>
+
+void* RTOS__GetMemory(size_t Size)
+{
+    return HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, Size);
+}
+
+#elif 1
+
+#include <windows.h>
+
+void* RTOS__GetMemory(size_t Size)
+{
+    return VirtualAlloc(NULL, Size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+}
+
+#elif 0
+
+#include <windows.h>
+/* Same as previous, but with some internal logging, which 
+reveals the lossage that results from using VirtualAlloc with RTMachine.PageSize < GetSystemInfo.AllocationGranularity.
+*/
+typedef struct _RTOSMemoryLogEntry_t
+{
+    size_t Size;
+    void* Result;
+} RTOSMemoryLogEntry_t;
+
+RTOSMemoryLogEntry_t RTOSMemoryLog[128];
+size_t RTOSMemoryLogIndex;
+#define NUMBER_OF(a) (sizeof(a)/sizeof((a)[0]))
+
+void* RTOS__GetMemory(size_t Size)
+{
+    RTOSMemoryLogEntry_t LogEntry;
+    
+    LogEntry.Size = Size;
+    LogEntry.Result = VirtualAlloc(NULL, Size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+    /* It does not matter if this is thread safe or not. */
+
+    RTOSMemoryLog[(RTOSMemoryLogIndex++) % NUMBER_OF(RTOSMemoryLog)] = LogEntry;
+
+    return LogEntry.Result;
+}
+
+#endif
