3f843a6ea73c30ae3b42b4080038072ae30f7dda
jkrell <>
2009-01-04 07:04:33 +0000

    replace the lone use of wrapped readdir with a smaller bit of C

---
+++ m3-libs/libm3/src/os/POSIX/FSPosixC.c
@@ -0,0 +1,19 @@
+/* Copyright (C) 1992, Digital Equipment Corporation. */
+/* All rights reserved. */
+/* See the file COPYRIGHT for a full description. */
+
+#define _FILE_OFFSET_BITS 64
+
+#include <sys/types.h>
+#include <dirent.h>
+typedef struct dirent dirent_t;
+
+const char* m3_readdir_name(DIR* dir)
+{
+    dirent_t* e;
+
+    if (dir && (e = readdir(dir)))
+        return e->d_name;
+
+    return 0;
+}
