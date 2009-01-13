a04f677a69f9272a8f0d7587c149ba106eb7378a
Jay Michael Krell <jay.krell@cornell.edu>
2009-01-13 12:01:29 +0000

    and these

---
+++ m3-libs/m3core/src/unix/Common/Uexec.c
@@ -0,0 +1,27 @@
+/* Copyright (C) 1990, Digital Equipment Corporation           */
+/* All rights reserved.                                        */
+/* See the file COPYRIGHT for a full description.              */
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <assert.h>
+#include <stddef.h>
+
+const int Uexec_WNOHANG = WNOHANG;
+
+void Uexec_RepackStatus(int* var_status)
+{
+    int status;
+
+    assert(var_status != NULL);
+
+    /* Posix says you must pass "the original" to the macros. */
+    status = ((WTERMSIG(*var_status) << 8) | WEXITSTATUS(*var_status));
+#ifdef WCOREDUMP
+    status |= (WCOREDUMP(*var_status) ? 0x8000 : 0);
+#endif
+    *var_status = status;
+}
+
+/* If needed, define functions Uexec_WTERMSIG, Uexec_WEXITSTATUS, etc. */
