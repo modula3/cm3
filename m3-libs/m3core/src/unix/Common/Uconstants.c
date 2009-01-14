a875dc71fb6881c2bd0382fe249e55a94c3da303
jkrell <>
2009-01-14 09:34:20 +0000

    Move all constants-implemented-as-variables to one file dedicated   to them -- Uconstants.c. This way we don't have to keep picking   up nickels and dimes on platforms that need to statically   link to them (cygwin), and we keep functions and data separate.   Not all of the constants in this file are yet active, but they   are ok to be there ahead of time. More will migrate here soon.   We should perhaps rely on Posix mandating some of the values,    or perhaps that some of the values happen to be identical on    all platforms, and then make them consts, or we can just    play it very safe and err toward making them variables.    As well we will look into removing their uses behind C wrappers.

---
+++ m3-libs/m3core/src/unix/Common/Uconstants.c
@@ -0,0 +1,50 @@
+/* Copyright (C) 1990, Digital Equipment Corporation.         */
+/* All rights reserved.                                       */
+/* See the file COPYRIGHT for a full description.             */
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <assert.h>
+#include <stddef.h>
+#include <errno.h>
+#include <signal.h>
+
+/* enable this and preprocess file to get approximate *.i3 contents, fix them in up in editor */
+#if 0
+#define X(x) <* EXTERNAL Uerror_##x *> VAR #x: int;
+#include "UerrorX.h"
+#undef X
+#endif
+
+/* check that Max is enough; if you get an error here, raise it in Uerror.i3 and here */
+typedef int CheckMax[128 - sizeof(union{
+#define X(x) char a##x[x];
+#include "UerrorX.h"
+#undef X
+})];
+
+#define X(x) const int Uerror_##x = x;
+#include "UerrorX.h"
+#undef X
+
+const int Uexec_WNOHANG = WNOHANG;
+
+const int Usignal_SIGINT = SIGINT;
+const int Usignal_SIGKILL = SIGKILL;
+
+const int Unix_FIONREAD = FIONREAD;
+
+const int Unix_O_RDONLY = O_RDONLY;
+const int Unix_O_RDWR = O_RDWR;
+const int Unix_O_CREAT = O_CREAT;
+const int Unix_O_EXCL = O_EXCL;
+const int Unix_O_TRUNC = O_TRUNC;
+const int Unix_O_NONBLOCK = O_NONBLOCK;
+const int Unix_O_NDELAY = O_NONBLOCK; /* compat */
+const int Unix_M3_NONBLOCK = O_NONBLOCK; /* compat */
+
+const int Unix_F_OK = F_OK;
+const int Unix_X_OK = X_OK;
+const int Unix_W_OK = W_OK;
+const int Unix_R_OK = R_OK;
