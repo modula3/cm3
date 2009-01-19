f4747b65e6708461a126c52091c11287152f6d84
Jay Michael Krell <jay.krell@cornell.edu>
2009-01-19 16:37:09 +0000

    prepare to remove Uresource.i3; once confirmed this is the only user

---
+++ m3-libs/m3core/src/runtime/POSIX/RTProcessPosixC.c
@@ -0,0 +1,35 @@
+/* Copyright (C) 1990, Digital Equipment Corporation           */
+/* All rights reserved.                                        */
+/* See the file COPYRIGHT for a full description.              */
+
+#include <time.h>
+#include <sys/resource.h>
+#include <assert.h>
+typedef struct timeval timeval_t;
+typedef struct rusage rusage_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define TimevalSecs RTProcessPosixC_TimevalSecs
+#define TimeUsed RTProcessPosixC_TimeUsed
+
+float TimevalSecs(timeval_t* t)
+/* Return the number of seconds represented by "t" as a floating-
+   point number. */
+{
+    return (((float) t->tv_sec) + (((float) t->tv_usec) / 1.0e6));
+}
+
+float TimeUsed(void)
+{
+    rusage_t usage;
+    int ret = getrusage(RUSAGE_SELF, &usage);
+    assert(ret == 0);
+    return TimevalSecs(&usage.ru_utime) + TimevalSecs(&usage.ru_stime);
+}
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
