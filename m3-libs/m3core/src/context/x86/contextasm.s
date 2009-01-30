a15fba9cb2d2aac1b514a92d897c1dc7934ef65f
jkrell <>
2009-01-30 23:10:46 +0000

    adapt to Cygwin -- which uses coff, so symbols with underscores, and different validity/meaning of .type and .size; and its make doesn't understand $>

--- m3-libs/m3core/src/context/x86/contextasm.s
+++ m3-libs/m3core/src/context/x86/contextasm.s
@@ -2,41 +2,41 @@
 
  .text
 
- .globl getcontext
- .type getcontext, @function
+ .global getcontext
+ .global _getcontext
 getcontext:
+_getcontext:
  pushal
  pushl $1
  call internal_getcontext
  addl $36, %esp
  movl $0, %eax
  ret
- .size getcontext, .-getcontext
 
- .globl setcontext
- .type setcontext, @function
+ .global setcontext
+ .global _setcontext
 setcontext:
+_setcontext:
  call internal_setcontext
  movl 4(%esp), %eax
  movl %eax, %esp
  popal
  movl 12(%eax), %esp
  jmp *32(%eax)
- .size setcontext, .-setcontext
 
- .globl internal_endcontext
- .type internal_endcontext, @function
+ .global internal_endcontext
+ .global _internal_endcontext
 internal_endcontext:
+_internal_endcontext:
  addl -4(%ebp), %esp
  ret
- .size internal_endcontext, .-internal_endcontext
 
- .globl swapcontext
- .type swapcontext, @function
+ .global swapcontext
+ .global _swapcontext
 swapcontext:
+_swapcontext:
  pushal
  pushl $0
  call internal_getcontext
  addl $40, %esp
  jmp setcontext
- .size swapcontext, .-swapcontext
