f391f934ef46d3c7529257110a027f1ae0aa857d
jkrell <>
2009-02-03 21:13:50 +0000

    add get/set/make/swapcontext for OpenBSD, and this should work for all others as well as we get to them

---
+++ m3-libs/m3core/src/unix/Common/Uucontext.h
@@ -0,0 +1,30 @@
+/* Copyright (C) 1990, Digital Equipment Corporation.         */
+/* All rights reserved.                                       */
+/* See the file COPYRIGHT for a full description.             */
+
+#if _MSC_VER > 1000
+#pragma once
+#endif
+
+#ifndef UUCONTEXT_INCLUDED
+#define UUCONTEXT_INCLUDED
+
+#if defined(__OpenBSD__)
+#include "context.h"
+#else
+#include <ucontext.h>
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stddef.h>
+
+void Uucontext__set_stack(ucontext_t* a, void* Start, size_t Size);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif
