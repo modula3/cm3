58633e739ef5f24d826af0c8d545a6814e9a5337
Anthony Hosking <hosking@cs.purdue.edu>
2009-02-16 04:43:10 +0000

    Restore wrapper for TimeUsed.  RTProcess.TimeUsed calls RTProcessPosix.TimeUsed.

---
+++ m3-libs/m3core/src/runtime/POSIX/RTProcessPosix.i3
@@ -0,0 +1,10 @@
+(* Copyright (C) 1990, Digital Equipment Corporation           *)
+(* All rights reserved.                                        *)
+(* See the file COPYRIGHT for a full description.              *)
+
+INTERFACE RTProcessPosix;
+
+<*EXTERNAL "RTProcessPosix__TimeUsed"*>
+PROCEDURE TimeUsed (): REAL;
+
+END RTProcessPosix.
--- m3-libs/m3core/src/runtime/POSIX/RTProcessPosixC.c
+++ m3-libs/m3core/src/runtime/POSIX/RTProcessPosixC.c
@@ -13,7 +13,7 @@ typedef struct rusage rusage_t;
 extern "C" {
 #endif
 
-#define TimevalSecs RTProcess__TimevalSecs
+#define TimevalSecs RTProcessPosix__TimevalSecs
 
 float TimevalSecs(timeval_t* t)
 /* Return the number of seconds represented by "t" as a floating-
@@ -22,7 +22,7 @@ float TimevalSecs(timeval_t* t)
     return (((float) t->tv_sec) + (((float) t->tv_usec) / 1.0e6));
 }
 
-float RTProcess__TimeUsed(void)
+float RTProcessPosix__TimeUsed(void)
 {
     rusage_t usage;
     int ret = getrusage(RUSAGE_SELF, &usage);
