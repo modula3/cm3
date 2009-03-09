ec92dd1fa875bc39b962eeb5ac63679da9e27571
Jay Michael Krell <jay.krell@cornell.edu>
2009-03-09 20:35:13 +0000

    fix warning on Cygwin; rename stat fields to omit prefix to avoid the problem the wrapper functions were avoiding and comment this in stat and netdb

--- m3-libs/m3core/src/unix/Common/UstatC.c
+++ m3-libs/m3core/src/unix/Common/UstatC.c
@@ -1,51 +1,38 @@
 #include "m3unix.h"
 #include <assert.h>
-
-typedef struct _m3_stat_t {
-/* Sorted by size, then by name; make everything LONGINT if possible, else INTEGER;
-Limit on LONGINT is compatibility with existing Modula-3 code. Blowing up the sizes
-larger than necessary is a slight deoptimization for the sake of simplicity and
-commonality. */
-    LONGINT st_mtime;
-    LONGINT st_rdev;
-    LONGINT st_size;
-    INTEGER st_gid;
-    INTEGER st_mode;
-    INTEGER st_uid;
-} m3_stat_t;
-
-static void m3stat_set_mtime(m3_stat_t* m3st, LONGINT mtime)
-{ m3st->st_mtime = mtime; }
-static void m3stat_set_rdev(m3_stat_t* m3st, LONGINT rdev)
-{ m3st->st_rdev = rdev; }
-static void m3stat_set_size(m3_stat_t* m3st, LONGINT size)
-{ m3st->st_size = size; }
-static void m3stat_set_gid(m3_stat_t* m3st, INTEGER gid)
-{ m3st->st_gid = gid; }
-static void m3stat_set_mode(m3_stat_t* m3st, INTEGER mode)
-{ m3st->st_mode = mode; }
-static void m3stat_set_uid(m3_stat_t* m3st, INTEGER uid)
-{ m3st->st_uid = uid; }
-
-/* These headers sometimes #define away our identifiers st_mtime, etc.
-   That is why we have the wrapper functions. */
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
 typedef struct stat stat_t;
 
+typedef struct _m3_stat_t {
+/* Sorted by size, then by name; make everything LONGINT if possible, else INTEGER;
+Limit on LONGINT is compatibility with existing Modula-3 code. Blowing up the sizes
+larger than necessary is a slight deoptimization for the sake of simplicity and
+commonality.
+    
+"st_" prefix is omitted from the names in case they are macros, which does happen */
+
+    LONGINT mtime;
+    LONGINT rdev;
+    LONGINT size;
+    INTEGER gid;
+    INTEGER mode;
+    INTEGER uid;
+} m3_stat_t;
+
 static int m3stat_from_stat(int result, m3_stat_t* m3st, stat_t* st)
 {
     assert(result == 0 || result == -1);
     if (result == 0)
     {
-        m3stat_set_mode(m3st, st->st_mode);
-        m3stat_set_mtime(m3st, st->st_mtime);
-        m3stat_set_size(m3st, st->st_size);
-        m3stat_set_rdev(m3st, st->st_rdev);
-        m3stat_set_uid(m3st, st->st_uid);
-        m3stat_set_gid(m3st, st->st_gid);
+        m3st->mtime = st->st_mtime;
+        m3st->rdev = st->st_rdev;
+        m3st->size = st->st_size;
+        m3st->gid = st->st_gid;
+        m3st->mode = st->st_mode;
+        m3st->uid = st->st_uid;
     }
     return result;
 }
