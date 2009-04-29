dacfc493144bb497a7f37f6232a78a455776ed5c
Jay Michael Krell <jay.krell@cornell.edu>
2009-04-29 13:49:35 +0000

    combine  MemAlloc + pthread_mutex_init => pthread_mutex_new  MemAlloc + pthread_cond_init => pthread_cond_new  pthread_mutex_destroy + MemFree => pthread_mutex_delete  pthread_cond_destroy + MemFree => pthread_cond_delete
    This was upon revisiting MemAlloc and was going to change to use NEW,
    however a REF ARRAY OF CHAR and its first byte are not at the same
    address, which is a bit annoying, so didn't go that route.
    
    
    This isn't clearly worth anything, but it also shouldn't be controversial.
      A small net reduction in the number of C symbols: since "size" and "init"
       now combine down to "new".
     MemAlloc and MemFree go away now, but the allocations are still done with calloc,
      but from the C code.
    While there, treat ENOMEM like other out of memory cases.
      Any other errors including EGAIN trigger assertion failures.
    
    
    Remove the avoidance of RAISE.
     Two of the uses are within a TRY, one is not.
     But the reasons are moot now, now that initialization order is better.
    
    Rename "UNDERUNDER_THREAD" to "M3CONFIG_THREAD_LOCAL_STORAGE",
    to be used shortly.

--- m3-libs/m3core/src/thread/PTHREAD/ThreadPThreadC.c
+++ m3-libs/m3core/src/thread/PTHREAD/ThreadPThreadC.c
@@ -3,6 +3,9 @@
 /* See the file COPYRIGHT-PURDUE for a full description.           */
 
 #include <assert.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <pthread.h>
 
 #ifdef __APPLE__
 /* MacOSX diverges in a good way and therefore many functions
@@ -16,11 +19,7 @@ not to call (statically, but the compiler can't or won't tell). */
 #ifdef __hpux
 #include <stdio.h>
 #endif /* hpux */
-#if defined(__hpux) || defined(__osf)
-#include <errno.h>
-#endif /* hpux || osf */
 #endif
-#include <pthread.h>
 
 /* const is extern const in C, but static const in C++,
  * but gcc gives a warning for the correct portable form "extern const" */
@@ -217,7 +216,7 @@ void* ThreadPThread__pthread_getspecific_##name(void) \
     return pthread_getspecific(name); \
 } \
 
-#if 0 /* CONFIG_UNDERUNDER_THREAD */
+#if 0 /* M3CONFIG_THREAD_LOCAL_STORAGE */
 #define THREAD_LOCAL(name) THREAD_LOCAL_FAST(name)
 #else
 #define THREAD_LOCAL(name) THREAD_LOCAL_SLOW(name)
@@ -233,23 +232,56 @@ MUTEX(heap)
 CONDITION_VARIABLE(heap)
 THREAD_LOCAL(activations)
 
-EXTERN_CONST int sizeof_pthread_mutex_t = sizeof(pthread_mutex_t);
-EXTERN_CONST int sizeof_pthread_cond_t = sizeof(pthread_cond_t);
+/* This could be a function, but then we'd have to cast the init parameter
+and casting function parameters is a little bit dangerous and in this case
+probably not worthwhile. */
+#define THREADPTHREAD__PTHREAD_GENERIC_NEW(type, init) \
+    int r; \
+    type* p = (type*)calloc(1, sizeof(*p)); \
+    if (p == NULL) \
+        return p; \
+    r = init(p, NULL); \
+    if (r == ENOMEM) \
+    { \
+        free(p); \
+        return NULL; \
+    } \
+    assert(r == 0); \
+    return p; \
 
-int
-ThreadPThread__pthread_mutex_destroy(
-    pthread_mutex_t* mutex)
+void* ThreadPThread__pthread_mutex_new(void)
 {
+    THREADPTHREAD__PTHREAD_GENERIC_NEW(pthread_mutex_t, pthread_mutex_init);
+}
+
+void* ThreadPThread__pthread_cond_new(void)
+{
+    THREADPTHREAD__PTHREAD_GENERIC_NEW(pthread_cond_t, pthread_cond_init);
+}
+
+void ThreadPThread__pthread_mutex_delete(pthread_mutex_t* p)
+{
+    int e;
+    if (p == NULL) return;
 #if defined(__hpux) || defined(__osf)
     /* workaround Tru64 5.1 and HP-UX bug
     pthread_mutex_destroy() intermittently returns
     EBUSY even when there are no threads accessing the mutex. */
-    int e;
-    while ((e = pthread_mutex_destroy(mutex)) == EBUSY) { }
-    return e;
+    while ((e = pthread_mutex_destroy(p)) == EBUSY) { }
 #else
-    return pthread_mutex_destroy(mutex);
+    e = pthread_mutex_destroy(p);
 #endif
+    assert(e == 0);
+    free(p);
+}
+
+void ThreadPThread__pthread_cond_delete(pthread_cond_t* p)
+{
+    int r;
+    if (p == NULL) return;
+    r = pthread_cond_destroy(p);
+    assert(r == 0);
+    free(p);
 }
 
 #ifdef __cplusplus
