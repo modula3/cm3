044903338fc965d91676cbed8e4214a2450414a2
jkrell <>
2009-05-21 19:18:07 +0000

    run them separately

--- scripts/iphone/1/all.sh
+++ scripts/iphone/1/all.sh
@@ -1,12 +1,10 @@
-. ./0env.sh
-
 set -e
 set -x
 
-. ./1iphonefilesystem.sh
-. ./trimfilesystem.sh
-. ./2csu.sh
-. ./3cctools.sh
-. ./4headers.sh
-. ./5llvm.sh
-. ./6hack.sh
+sh -c ./1iphonefilesystem.sh
+sh -c ./trimfilesystem.sh
+sh -c ./2csu.sh
+sh -c ./3cctools.sh
+sh -c ./4headers.sh
+sh -c ./5llvm.sh
+sh -c ./6hack.sh
