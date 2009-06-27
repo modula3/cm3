48520d3bdb0ca9f0b5207f3d2d78184f74326616
Jay Michael Krell <jay.krell@cornell.edu>
2009-06-27 10:45:15 +0000

    work in progress: Debian packages have some problems   with hardlinks apparently.
      So what we do here is we find the hardlinks before
      making the package, replace one of them with an
      empty file. We know they come only in pairs, though
      generally they could be any number. Make the package,
      and then restore the links. And then the package has
      postinst code that looks finds any zero length files
      in pkg with the same name as any file in lib and
      deletes the zero length file and links them. We know
      this is the pattern. If we had to do something more
      general, we'd inspect the inode number of every
      file, hash them all up, and find the relations that way.
    
      We could just record all the paths in a text file, but
      I figure leaving the zero length markers is likely to
      aid uninstall.
    
      In order to speed up testing this while it is in development,
        use gzip instead of lzma and fix -no-m3ship-resolution.
        And only min for now.
    
      Also fix make-deb.py so it doesn't keep creating new
       empty temporary directories. (Because it generates
       a new tempdir, creates it, then notices the
       one on the command line.)

--- scripts/python/make-iexpress.py
+++ scripts/python/make-iexpress.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-# $Id: make-iexpress.py,v 1.1 2009-06-27 04:30:31 jkrell Exp $
+# $Id: make-iexpress.py,v 1.2 2009-06-27 10:45:15 jkrell Exp $
 
 import sys
 import os.path
@@ -11,9 +11,8 @@ from pylib import *
 #
 
 SetEnvironmentVariable("STAGE", sys.argv[1])
-reload(pylib)
 
 for name in ["min", "std"]:
-    MakeIExpressPackage(FormInstallRoot(name), STAGE + "/cm3-" + name + ".exe")
+    MakeIExpressPackage(FormInstallRoot(name), GetStage() + "/cm3-" + name + ".exe")
 
 print("%s: Success." % os.path.basename(sys.argv[0]))
