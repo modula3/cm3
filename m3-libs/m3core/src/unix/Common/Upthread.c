73e51e074f535db996aca53768e35c671323a1e5
Jay Michael Krell <jay.krell@cornell.edu>
2009-07-06 21:08:25 +0000

    mind that m3_pthread_t is void* and thread_t might be different

--- m3-libs/m3core/src/unix/Common/Upthread.c
+++ m3-libs/m3core/src/unix/Common/Upthread.c
@@ -10,24 +10,34 @@ extern "C" {
 
 #ifndef _WIN32
 
-int Upthread__detach(pthread_t thread)
+/*
+m3_pthread_t is void*.
+pthread_t might be any of: size_t, ptrdiff_t, int, void*, another pointer.
+pthread_t will not be larger than a pointer/size_t. (see Unix__Assertions)
+Only convert integers to/from integers, and pointer-sized integers to/from pointers.
+*/
+#define PTHREAD_TO_M3(x)   ((m3_pthread_t)(size_t)(x))
+#define PTHREAD_FROM_M3(x) ((pthread_t)(size_t)(x))
+    
+int Upthread__detach(m3_pthread_t thread)
 {
-    return pthread_detach(thread);
+    pthread_t a = PTHREAD_FROM_M3(thread);
+    return pthread_detach(a);
 }
 
-pthread_t Upthread__self(void)
+m3_pthread_t Upthread__self(void)
 {
-    return pthread_self();
+    return PTHREAD_TO_M3(pthread_self());
 }
 
-int Upthread__equal(pthread_t t1, pthread_t t2)
+int Upthread__equal(m3_pthread_t t1, m3_pthread_t t2)
 {
-    return pthread_equal(t1, t2);
+    return pthread_equal(PTHREAD_FROM_M3(t1), PTHREAD_FROM_M3(t2));
 }
 
-int Upthread__kill(pthread_t thread, int sig)
+int Upthread__kill(m3_pthread_t thread, int sig)
 {
-    return pthread_kill(thread, sig);
+    return pthread_kill(PTHREAD_FROM_M3(thread), sig);
 }
 
 #endif
