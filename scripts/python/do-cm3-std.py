18821cd16cc978a7c6034e7a9d0ad74eb98babea
Jay Michael Krell <jay.krell@cornell.edu>
2009-07-16 19:26:49 +0000

    don't build m3bundle out of dependency order

--- scripts/python/do-cm3-std.py
+++ scripts/python/do-cm3-std.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-# $Id: do-cm3-std.py,v 1.6 2008-05-25 01:16:59 jkrell Exp $
+# $Id: do-cm3-std.py,v 1.7 2009-07-16 19:26:49 jkrell Exp $
 
 import sys
 import os.path
@@ -8,8 +8,8 @@ from pylib import *
 
 SetupEnvironment()
 
-if not SearchPath("m3bundle"):
-    DoPackage(sys.argv, ["m3bundle"])
+#if not SearchPath("m3bundle"):
+#    DoPackage(sys.argv, ["m3bundle"])
 
 DoPackage(sys.argv, pylib.PackageSets["std"]) or sys.exit(1)
 
