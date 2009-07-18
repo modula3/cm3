38ea24b412dbc1bdf080ce8a4bc84ac039c10a0c
jkrell <>
2009-07-18 18:50:44 +0000

    no m3gdb on Darwin, code that checked here for DARWIN occuring in TARGET would be nice

--- scripts/pkginfo.sh
+++ scripts/pkginfo.sh
@@ -38,6 +38,11 @@ UsePackage()
     m3cc) [ "${GCC_BACKEND}" = yes ] && [ "${OMIT_GCC}" = "" ] ;;
     m3gdb)
       ([ "${M3GDB}" = yes ] || [ "${CM3_GDB}" = yes ]) \
+         && [ "${TARGET}" != "ARM_DARWIN" ] \
+         && [ "${TARGET}" != "PPC_DARWIN" ] \
+         && [ "${TARGET}" != "PPC64_DARWIN" ] \
+         && [ "${TARGET}" != "I386_DARWIN" ] \
+         && [ "${TARGET}" != "AMD64_DARWIN" ] \
          && [ "${TARGET}" != "NT386" ] ;;
 
     *) true;;
