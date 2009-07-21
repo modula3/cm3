d40f0351a279d38c9f624f066d56df435c4db031
Jay Michael Krell <jay.krell@cornell.edu>
2009-07-21 08:30:58 +0000

    remove more cruft inherited from the *.sh code:   GCWRAPFLAGS   GREP   SCRIPTS   SL   SYSLIBDIR   SYSLIBS   TMPDIR   TAR   CM3LIBSEARCHPATH   CM3BINSEARCHPATH

--- scripts/win/clearenv.cmd
+++ scripts/win/clearenv.cmd
@@ -1,4 +1,4 @@
-@rem $Id: clearenv.cmd,v 1.10 2008-05-08 11:36:42 jkrell Exp $
+@rem $Id: clearenv.cmd,v 1.11 2009-07-21 08:30:58 jkrell Exp $
 
 @if "%_echo%" == "" @echo off
 
@@ -8,12 +8,11 @@
 @rem
 
 for %%i in (ACTION ARGS BUILDGLOBAL BUILDLOCAL CLEANGLOBAL       ) do if defined %%i set %%i=
-for %%i in (CLEANLOCAL CM3BINSEARCHPATH CM3LIBSEARCHPATH         ) do if defined %%i set %%i=
-for %%i in (CM3ROOT CM3VERSION EXE GCC_BACKEND GREP IGNORE_MISS  ) do if defined %%i set %%i=
+for %%i in (CLEANLOCAL                                           ) do if defined %%i set %%i=
+for %%i in (CM3ROOT CM3VERSION EXE GCC_BACKEND IGNORE_MISS       ) do if defined %%i set %%i=
 for %%i in (INSTALLROOT M3BUILD M3GDB M3OSTYPE M3SHIP PKG_ACTION ) do if defined %%i set %%i=
-for %%i in (PKGS PKGSDB REALCLEAN ROOT SCRIPTS SYSINFO_DONE      ) do if defined %%i set %%i=
-for %%i in (SHIP SL SYSLIBDIR SYSLIBS TAR TMPDIR                 ) do if defined %%i set %%i=
-for %%i in (CM3_SYSCALL_WRAPPERS_EXIST                           ) do if defined %%i set %%i=
+for %%i in (PKGS PKGSDB REALCLEAN ROOT SYSINFO_DONE              ) do if defined %%i set %%i=
+for %%i in (SHIP                                                 ) do if defined %%i set %%i=
 call :Clear INSTALLROOT_PREVIOUS
 call :Clear INSTALLROOT_COMPILER_WITH_PREVIOUS
 call :Clear INSTALLROOT_COMPILER_WITH_SELF
