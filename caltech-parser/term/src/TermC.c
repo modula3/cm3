fc5d22e33af5deb7ba8eca6c3ab43ab6ff1f7995
jkrell <>
2009-07-21 10:30:31 +0000

    initial versions, not yet active

--- caltech-parser/term/src/TermC.c
+++ caltech-parser/term/src/TermC.c
@@ -1,121 +1,26 @@
-(* $Id$ *)
+#ifdef __cplusplus
+extern "C"
+#endif
 
-UNSAFE MODULE Term;
-IMPORT Stdio;
-IMPORT Wr AS Wrr;
-IMPORT Rd;
-IMPORT Termios;
-IMPORT Text;
-IMPORT RTCollector;
-IMPORT Thread;
+#ifndef _WIN32
 
-<* FATAL Thread.Alerted, Wrr.Failure, Rd.Failure, Rd.EndOfFile *>
+#include <termios.h>
+#include <unistd.h> 
+typedef struct termios termios_t;
 
-VAR
-  Endl: TEXT := "\n";
-  Raw: BOOLEAN := FALSE;
-  TermCooked, TermRaw: Termios.T := NIL;
+static termios_t TermCooked;
+static termios_t TermRaw;
 
-PROCEDURE MakeRaw(flag: BOOLEAN) =
-  VAR
-    termNew: Termios.T;
-  BEGIN
-    TRY
-      RTCollector.Disable();
-      IF flag # Raw THEN
-        Raw := flag;
-        Wrr.Flush(Stdio.stdout);
-        IF Raw THEN
-            IF TermCooked = NIL THEN
-              TermCooked := NEW(Termios.T);
-              TermRaw := NEW(Termios.T);
-              Termios.tcgetattr(Termios.Stdin, TermCooked);
-              TermRaw^ := TermCooked^;
-              Termios.cfmakeraw(TermRaw);
-            END;
-            termNew := TermRaw;
-            Endl := "\015\012";
-          ELSE
-            termNew := TermCooked;
-            Endl := "\n";
-          END;
-        Termios.tcsetattr(Termios.Stdin, Termios.Tcsanow, termNew);
-        END;
-    FINALLY
-      RTCollector.Enable()
-    END
-  END MakeRaw;
+void* Term__GetTermRaw(void) { return &TermRaw; }
+void* Term__GetTermCooked(void) { return &TermCooked; }
 
-PROCEDURE GetCharDR(): CHAR RAISES {SpecialChar} =
-  BEGIN
-    TRY
-      RETURN GetCharE(RawSpecial);
-    EXCEPT
-      SpecialChar => 
-    END;
-    MakeRaw(FALSE);
-    RAISE SpecialChar;
-  END GetCharDR;
+#else
 
-PROCEDURE GetCharD(): CHAR =
-  <* FATAL SpecialChar *>
-  BEGIN
-    IF Raw THEN
-      RETURN GetCharDR()
-    ELSE
-      RETURN GetChar();
-    END;
-  END GetCharD;
+void* Term__GetTermRaw(void) { return 0; }
+void* Term__GetTermCooked(void) { return 0; }
 
-PROCEDURE GetCharE(special: TEXT): CHAR RAISES {SpecialChar} =
-  VAR
-    cin: CHAR;
-  BEGIN
-    cin := GetChar();
-    IF Text.FindChar(special, cin) = -1 THEN
-      RETURN cin;
-    ELSE
-      RAISE SpecialChar;
-    END;
-  END GetCharE;
+#endif
 
-PROCEDURE GetChar(): CHAR =
-  BEGIN
-    Wrr.Flush(Stdio.stdout);
-    RETURN Rd.GetChar(Stdio.stdin);
-  END GetChar;
-
-PROCEDURE Wr(s: TEXT) =
-
-  BEGIN
-    IF Raw THEN
-      VAR
-        i,j: INTEGER := 0;
-      BEGIN
-        REPEAT
-          j := Text.FindChar(s, '\n', i);
-          IF j # -1 THEN
-            Wrr.PutText(Stdio.stdout, Text.Sub(s, i, j - i));
-            Wrr.PutText(Stdio.stdout, Endl);
-            i := j + 1;
-          END;
-        UNTIL j = -1;
-        IF i # Text.Length(s) THEN
-          Wrr.PutText(Stdio.stdout, Text.Sub(s, i, LAST(CARDINAL)));
-        END;
-      END;
-    ELSE
-      Wrr.PutText(Stdio.stdout, s);
-    END;
-  END Wr;
-
-PROCEDURE WrLn(s: TEXT; flush := FALSE) =
-  BEGIN
-    Wrr.PutText(Stdio.stdout, s);
-    Wrr.PutText(Stdio.stdout, Endl);
-    IF flush THEN
-      Wrr.Flush(Stdio.stdout);
-    END;
-  END WrLn;
-
-BEGIN END Term.
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
--- caltech-parser/term/src/Termios.c
+++ caltech-parser/term/src/Termios.c
@@ -1,21 +1,88 @@
-(* $Id$ *)
-UNSAFE INTERFACE Termios;
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-CONST
-  Stdin = 0;
-  Stdout = 1;
-  Stderr = 2;
+/* const is extern const in C, but static const in C++,
+ * but gcc gives a warning for the correct portable form "extern const" */
+#if defined(__cplusplus) || !defined(__GNUC__)
+#define EXTERN_CONST extern const
+#else
+#define EXTERN_CONST const
+#endif
 
-  Tcsanow = 0;
-  Tcsadrain = 1;
-  Tcsaflush = 2;
-  Tcsasoft = 16;
+#ifndef _WIN32
 
-TYPE
-  T = REF ARRAY [0..511] OF CHAR;
+#include <termios.h>
+#include <unistd.h>
 
-<*EXTERNAL tcgetattr*> PROCEDURE tcgetattr(fd: INTEGER; t: T);
-<*EXTERNAL cfmakeraw*> PROCEDURE cfmakeraw(t: T);
-<*EXTERNAL tcsetattr*> PROCEDURE tcsetattr(fd, action: INTEGER; t: T);
+#ifndef STDIN_FILENO
+#define STDIN_FILENO 0
+#endif
 
-END Termios.
+#ifndef STDOUT_FILENO
+#define STDOUT_FILENO 1
+#endif
+
+#ifndef STDERR_FILENO
+#define STDERR_FILENO 2
+#endif
+
+EXTERN_CONST int Termios__Stdin = STDIN_FILENO;
+EXTERN_CONST int Termios__Stdout = STDOUT_FILENO ;
+EXTERN_CONST int Termios__Stderr = STDERR_FILENO;
+EXTERN_CONST int Termios__Tcsanow = TCSANOW;
+EXTERN_CONST int Termios__Tcsadrain = TCSADRAIN;
+EXTERN_CONST int Termios__Tcsaflush = TCSAFLUSH;
+
+typedef struct termios termios_t;
+
+void Termios__tcgetattr(int fd, termios_t* t)
+{
+    tcgetattr(fd, t);
+}
+
+void Termios__tcsetattr(int fd, int action, termios_t* t)
+{
+    tcsetattr(fd, action, t);
+}
+
+void Termios__cfmakeraw(termios_t* t)
+{
+#ifdef __CYGWIN__
+/* http://linux.about.com/library/cmd/blcmdl3_cfmakeraw.htm */
+    t->c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL|IXON);
+    t->c_oflag &= ~OPOST;
+    t->c_lflag &= ~(ECHO|ECHONL|ICANON|ISIG|IEXTEN);
+    t->c_cflag &= ~(CSIZE|PARENB);
+    t->c_cflag |= CS8;
+#else
+    cfmakeraw(t);
+#endif
+}
+
+#else
+
+EXTERN_CONST int Termios__Stdin = 0;
+EXTERN_CONST int Termios__Stdout = 0;
+EXTERN_CONST int Termios__Stderr = 0;
+EXTERN_CONST int Termios__Tcsanow = 0;
+EXTERN_CONST int Termios__Tcsadrain = 0;
+EXTERN_CONST int Termios__Tcsaflush = 0;
+
+void Termios__tcgetattr(int fd, void* t)
+{
+}
+
+void Termios__tcsetattr(int fd, int action, void* t)
+{
+}
+
+void Termios__cfmakeraw(void* t)
+{
+}
+
+#endif
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
