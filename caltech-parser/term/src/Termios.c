a3a4234408da9c431254231c15fef7b0a5ba077b
jkrell <>
2009-07-21 11:18:42 +0000

    after the initial more faithful repair, prune down all the unused stuff

--- caltech-parser/term/src/Termios.c
+++ caltech-parser/term/src/Termios.c
@@ -19,67 +19,22 @@ extern "C" {
 #define STDIN_FILENO 0
 #endif
 
-/* not used */
-#ifndef STDOUT_FILENO
-#define STDOUT_FILENO 1
-#endif
-
-/* not used */
-#ifndef STDERR_FILENO
-#define STDERR_FILENO 2
-#endif
-
 EXTERN_CONST int Termios__Stdin = STDIN_FILENO;
-EXTERN_CONST int Termios__Stdout = STDOUT_FILENO; /* not used */
-EXTERN_CONST int Termios__Stderr = STDERR_FILENO; /* not used */
 EXTERN_CONST int Termios__Tcsanow = TCSANOW;
-EXTERN_CONST int Termios__Tcsadrain = TCSADRAIN; /* not used */
-EXTERN_CONST int Termios__Tcsaflush = TCSAFLUSH; /* not used */
 
 typedef struct termios termios_t;
-
-void Termios__tcgetattr(int fd, termios_t* t)
-{
-    tcgetattr(fd, t);
-}
 
 void Termios__tcsetattr(int fd, int action, termios_t* t)
 {
     tcsetattr(fd, action, t);
 }
 
-void Termios__cfmakeraw(termios_t* t)
-{
-#ifdef __CYGWIN__
-/* http://linux.about.com/library/cmd/blcmdl3_cfmakeraw.htm */
-    t->c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL|IXON);
-    t->c_oflag &= ~OPOST;
-    t->c_lflag &= ~(ECHO|ECHONL|ICANON|ISIG|IEXTEN);
-    t->c_cflag &= ~(CSIZE|PARENB);
-    t->c_cflag |= CS8;
-#else
-    cfmakeraw(t);
-#endif
-}
-
 #else
 
 EXTERN_CONST int Termios__Stdin = 0;
-EXTERN_CONST int Termios__Stdout = 0; /* not used */
-EXTERN_CONST int Termios__Stderr = 0; /* not used */
 EXTERN_CONST int Termios__Tcsanow = 0;
-EXTERN_CONST int Termios__Tcsadrain = 0; /* not used */
-EXTERN_CONST int Termios__Tcsaflush = 0; /* not used */
-
-void Termios__tcgetattr(int fd, void* t)
-{
-}
 
 void Termios__tcsetattr(int fd, int action, void* t)
-{
-}
-
-void Termios__cfmakeraw(void* t)
 {
 }
 
--- caltech-parser/term/src/Termios.i3
+++ caltech-parser/term/src/Termios.i3
@@ -5,17 +5,11 @@ FROM Ctypes IMPORT int;
 
 (*CONST*)
 <*EXTERNAL Termios__Stdin*>     VAR Stdin: int;
-<*EXTERNAL Termios__Stdout*>    VAR Stdout: int; (* not used *)
-<*EXTERNAL Termios__Stderr*>    VAR Stderr: int; (* not used *)
 <*EXTERNAL Termios__Tcsanow*>   VAR Tcsanow: int;
-<*EXTERNAL Termios__Tcsadrain*> VAR Tcsadrain: int; (* not used *)
-<*EXTERNAL Termios__Tcsaflush*> VAR Tcsaflush: int; (* not used *)
 
 TYPE
   T = ADDRESS;
 
-<*EXTERNAL Termios__tcgetattr*> PROCEDURE tcgetattr(fd: int; t: T); (* not used *)
-<*EXTERNAL Termios__cfmakeraw*> PROCEDURE cfmakeraw(t: T); (* not used *)
 <*EXTERNAL Termios__tcsetattr*> PROCEDURE tcsetattr(fd, action: int; t: T);
 
 END Termios.
