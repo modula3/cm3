1070d9e21cee84b926b9ee4e2c90a8dc9cf3a611
Jay Michael Krell <jay.krell@cornell.edu>
2009-07-21 09:39:56 +0000

    crudely adapt to Win32 host -- or should we just edit the checked in files?

--- caltech-parser/parserlib/klexlib/src/boot/installcmd.cmd
+++ caltech-parser/parserlib/klexlib/src/boot/installcmd.cmd
@@ -1,5 +1,6 @@
-#!/bin/sh -x
-cp ../src/boot/*3 .
-sed s/SeekRd\.DiscardPrevious\(self\.rd\)\;//g ../src/boot/RegExpLex.m3 >itemp
-sed s/SeekRd/FileRdErr/g itemp >RegExpLex.m3
-rm itemp
+set COPYCMD=/y
+copy ..\src\boot\*3 .
+
+echo MODULE RegExpLex; >RegExpLex.m3
+echo IMPORT FileRdErr AS SeekRd; >>RegExpLex.m3
+findstr /v /c:"SeekRd.DiscardPrevious(self.rd);" /c:"IMPORT SeekRd;" /c:"MODULE RegExpLex;" ..\src\boot\RegExpLex.m3 >>RegExpLex.m3
