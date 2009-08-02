b1ae998f9acd42f9bd43b5dcf6ed3db0cf0a3a87
Jay Michael Krell <jay.krell@cornell.edu>
2009-08-02 10:18:59 +0000

    merge from release

--- m3-libs/m3core/src/runtime/SOLsun/RTMachineASM.s
+++ m3-libs/m3core/src/runtime/SOLsun/RTMachineASM.s
@@ -1,5 +1,8 @@
-        .seg    "text"
-        .globl  RTMachine__SaveRegsInStack
+  .file "RTMachineASM.s"
+  .section ".text",#alloc,#execinstr
+  .align 8
+  .global RTMachine__SaveRegsInStack
+  .type RTMachine__SaveRegsInStack,#function
 RTMachine__SaveRegsInStack:
         ta      0x3   ! ST_FLUSH_WINDOWS
         mov     %sp,%o0
