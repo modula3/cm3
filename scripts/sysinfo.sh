5d2af7c6da1551b1d69790662cdcd8f5d6dce300
jkrell <>
2009-08-25 07:12:23 +0000

    favor /usr/ccs/bin/strip over strip if it is executable

--- scripts/sysinfo.sh
+++ scripts/sysinfo.sh
@@ -82,7 +82,11 @@ cygpath() {
 }
 
 strip_exe() {
-  strip $@
+  if [ -x /usr/ccs/bin/strip ]; then
+    /usr/ccs/bin/strip $@
+  else
+    strip $@
+  end
 }
 
 #-----------------------------------------------------------------------------
