dd13c13cbdc65d79577082abc37aeaef35365a7f
Jay Michael Krell <jay.krell@cornell.edu>
2009-09-04 07:43:17 +0000

    my buildin of Juno was inadvertantly using preexisting stubgen, m3bundle, etc. so add more packages here, NOT experimentally derived or verified, but approximate/guessing and I got it working eventually (running/crashing, not fixed the bug yet..)

--- scripts/examples/make-juno.cmd
+++ scripts/examples/make-juno.cmd
@@ -5,20 +5,27 @@ set CM3=c:\cm3\bin\cm3
 set TARGET=NT386
 
 set p=^
-    m3-tools/mtex ^
-    m3-comm/tcp ^
-    m3-comm/netobj ^
-    m3-ui/ui ^
-    m3-ui/vbtkit ^
-    m3-www/web ^
-    m3-ui/jvideo ^
-    m3-ui/videovbt ^
-    m3-ui/formsvbtpixmaps ^
-    m3-ui/formsvbt ^
-    m3-ui/juno-2/juno-machine ^
-    m3-ui/juno-2/juno-compiler ^
-    m3-ui/juno-2/juno-app/pkl-fonts ^
-    m3-ui/juno-2/juno-app
+  m3-libs/m3tk-misc ^
+  m3-sys/m3scanner ^
+  m3-sys/m3tools ^
+  m3-tools/m3scan ^
+  m3-tools/m3bundle ^
+  m3-tools/m3tk ^
+  m3-tools/mtex ^
+  m3-comm/netobj ^
+  m3-comm/stubgen ^
+  m3-comm/tcp ^
+  m3-ui/ui ^
+  m3-ui/vbtkit ^
+  m3-www/web ^
+  m3-ui/jvideo ^
+  m3-ui/videovbt ^
+  m3-ui/formsvbtpixmaps ^
+  m3-ui/formsvbt ^
+  m3-ui/juno-2/juno-machine ^
+  m3-ui/juno-2/juno-compiler ^
+  m3-ui/juno-2/juno-app/pkl-fonts ^
+  m3-ui/juno-2/juno-app
 
 for %%a in (%p%) do call :clean %%a || exit /b 1
 for %%a in (%p%) do call :buildship %%a || exit /b 1
