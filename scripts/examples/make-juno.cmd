2494b86ae8e759bffc940703c05667f449c270e3
Jay Michael Krell <jay.krell@cornell.edu>
2009-09-04 06:42:59 +0000

    examples

---
+++ scripts/examples/make-juno.cmd
@@ -0,0 +1,43 @@
+SetLocal
+
+set CM3_ROOT=C:\dev2\cm3.2008-01-01
+set CM3=c:\cm3\bin\cm3
+set TARGET=NT386
+
+set p=^
+    m3-tools/mtex ^
+    m3-comm/tcp ^
+    m3-comm/netobj ^
+    m3-ui/ui ^
+    m3-ui/vbtkit ^
+    m3-www/web ^
+    m3-ui/jvideo ^
+    m3-ui/videovbt ^
+    m3-ui/formsvbtpixmaps ^
+    m3-ui/formsvbt ^
+    m3-ui/juno-2/juno-machine ^
+    m3-ui/juno-2/juno-compiler ^
+    m3-ui/juno-2/juno-app/pkl-fonts ^
+    m3-ui/juno-2/juno-app
+
+for %%a in (%p%) do call :clean %%a || exit /b 1
+for %%a in (%p%) do call :buildship %%a || exit /b 1
+
+goto :eof
+
+:clean
+SetLocal
+set a=%1
+set a=%a:/=\%
+cd %CM3_ROOT%\%a% || exit /b 1
+rmdir /q/s %TARGET%
+goto :eof
+
+:buildship
+SetLocal
+set a=%1
+set a=%a:/=\%
+cd %CM3_ROOT%\%a% || exit /b 1
+%CM3% || exit /b 1
+%CM3% -ship || exit /b 1
+goto :eof
