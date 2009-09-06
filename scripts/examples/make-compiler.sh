70c826d57399d30ae9a94f135e26a0813ccde8fd
Jay Michael Krell <jay.krell@cornell.edu>
2009-09-06 10:42:56 +0000

    enough to start building m3cc

--- scripts/examples/make-compiler.sh
+++ scripts/examples/make-compiler.sh
@@ -1,38 +1,116 @@
 #!/bin/sh
 
+#
+# echo everything and fail on any error
+#
+
+set -e
+set -x
+
+#
+# establish some parameters
+#
+
 if [ "x$CM3_VERSION" = "x" ]; then
-    CM3_VERSION=2009-05-01
+    CM3_VERSION=2009-02-01
 fi
 
-CM3_ROOT=/dev2/cm3.$CM3_VERSION
-CM3=/cm3/$CM3_VERSION/bin/cm3
-CM3_FLAGS=-DROOT=$CM3_ROOT
+#
+# config files come from LATEST_SOURCE
+# initial cm3/cm3cg come from BOOT
+#
+
+LATEST_SOURCE=/dev2/cm3
+SOURCE=/dev2/cm3.$CM3_VERSION
+INSTALL=/cm3.$CM3_VERSION
+BOOT=/cm3
+CM3_FLAGS=-DROOT=$SOURCE
 TARGET=I386_DARWIN
 
-PATH=$CM3_ROOT/bin:/cm3/bin:$PATH
+CM3_TARGET=$TARGET
+export CM3_TARGET
+
+#for a in $LATEST_SOURCE $SOURCE; do
+#  if [ -d $a/m3-sys ]; then : else
+#    echo "$a/m3-sys does not exist"
+#    exit 1
+#  fi
+#done
+
+#
+# create install directories
+#
+
+mkdir -p $INSTALL/bin/config
+
+#
+# set PATH
+#
+
+PATH="$INSTALL/bin:$PATH"
 export PATH
 
+
+#
+# create config files from current source
+#
+
+echo "
+  INSTALL_ROOT = path() & \"/..\"
+  include(path() & \"/config/$TARGET\")
+" > $INSTALL/bin/cm3.cfg
+
+for a in $LATEST_SOURCE/m3-sys/cminstall/src/config-no-install/*; do
+  if [ -f $a ]; then
+    cp $a $INSTALL/bin/config
+  fi
+done
+rm $INSTALL/bin/config/cm3.cfg
+
+#
+# copy over cm3, cm3cg, mklib
+#
+
+for a in cm3 cm3cg mklib; do
+  for b in $a $a.exe; do
+    if [ -x $BOOT/bin/$b ]; then
+      cp $BOOT/bin/$b $INSTALL/bin
+    fi
+  done
+done
+
 clean() {
-    cd $CM3_ROOT/$1 || exit 1
+    cd $SOURCE/$1 || exit 1
     rm -rf $TARGET
 }
 
-buildship() {
-	cd $CM3_ROOT/$1 || exit 1
-	$CM3 $CM3_FLAGS || exit 1
-	$CM3 $CM3_FLAGS -ship || exit 1
+buildlocal() {
+	cd $SOURCE/$1 || exit 1
+	type cm3
+	cm3 -x $CM3_FLAGS || exit 1
 }
 
-p=\
-    m3-sys/m3cc
+buildship() {
+	cd $SOURCE/$1 || exit 1
+	type cm3
+	cm3 $CM3_FLAGS || exit 1
+	cm3 $CM3_FLAGS -ship || exit 1
+}
 
-    rm -rf $CM3_ROOT/pkg
-    for a in $p; do clean $a || exit 1; done
-    for a in $p; do buildship $a || exit 1; done
-    cp $CM3_ROOT/m3-sys/m3cc/$TARGET/cm3cg `dirname $CM3`/bin
+#
+# build cm3cg
+#
 
-p=\
-    m3-libs/m3core \
+rm -rf $INSTALL/pkg
+clean m3-sys/m3cc || exit 1
+buildship m3-sys/m3cc || exit 1
+cp $SOURCE/m3-sys/m3cc/$TARGET/cm3cg `dirname $CM3`/bin
+
+#
+# build up to cm3 and mklib
+#
+
+p=  m3-libs/m3core \
     m3-libs/libm3 \
     m3-libs/sysutils \
     m3-sys/m3middle \
@@ -48,10 +126,16 @@ p=\
 # Do it twice so it builds itself.
 #
 
-for b in 1 2; do
-    rm -rf $CM3_ROOT/pkg
-    for a in $p; do clean $a || exit 1; done
-    for a in $p; do buildship $a || exit 1; done
-    cp $CM3_ROOT/m3-sys/cm3/$TARGET/cm3 `dirname $CM3`/bin
-    cp $CM3_ROOT/m3-sys/mklib/$TARGET/mklib `dirname $CM3`/bin
+rm -rf $INSTALL/pkg
+for a in $p; do clean $a || exit 1; done
+for a in $p; do buildlocal $a || exit 1; done
+for a in cm3 mklib; do
+  cp $SOURCE/m3-sys/$a/$TARGET/$a* $INSTALL/bin
+done
+
+rm -rf $INSTALL/pkg
+for a in $p; do clean $a || exit 1; done
+for a in $p; do buildship $a || exit 1; done
+for a in cm3 mklib; do
+  cp $SOURCE/m3-sys/$a/$TARGET/$a* $INSTALL/bin
 done
