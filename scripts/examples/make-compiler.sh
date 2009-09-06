99ba53bbf187a6db0854daf8b8cc13039c23c816
Jay Michael Krell <jay.krell@cornell.edu>
2009-09-06 13:26:03 +0000

    In order to build older libm3 with a bad dependency on cm3,  copy m3core and libm3 from boot and don't build them for the first stage. Since sysutils has a history of dependending on contemporary m3core, copy it too. Since older cm3cg is broken in a way I haven't debugged, slow to build, and   not relevant to current investigation, don't build it.

--- scripts/examples/make-compiler.sh
+++ scripts/examples/make-compiler.sh
@@ -17,14 +17,15 @@ fi
 
 #
 # config files come from LATEST_SOURCE
-# initial cm3/cm3cg come from BOOT
+# initial cm3/cm3cg/m3core/libm3 come from BOOT
 #
 
 LATEST_SOURCE=/dev2/cm3
 SOURCE=/dev2/cm3.$CM3_VERSION
 INSTALL=/cm3.$CM3_VERSION
 BOOT=/cm3
-CM3_FLAGS="-DROOT=$SOURCE -DGNU_CC=\\* -DGNU_CFLAGS=\\*"
+CM3_FLAGS="-DROOT=$SOURCE -DGNU_CC='*' -DGNU_CFLAGS='*'"
+CM3_FLAGS="-DROOT=$SOURCE"
 TARGET=I386_DARWIN
 
 CM3_TARGET=$TARGET
@@ -59,12 +60,6 @@ copy_exe() {
 clean() {
     cd $SOURCE/$1 || exit 1
     rm -rf $TARGET
-}
-
-buildlocal() {
-	cd $SOURCE/$1 || exit 1
-	type cm3 || exit 1
-	cm3 -x $CM3_FLAGS || exit 1
 }
 
 buildship() {
@@ -113,30 +108,37 @@ done
 rm $INSTALL/bin/config/cm3.cfg
 
 #
-# copy over cm3, cm3cg, mklib
+# copy over cm3, cm3cg, mklib, m3core, libm3
 #
 
 for a in cm3 cm3cg mklib; do
   copy_exe $BOOT/bin/$a $INSTALL/bin
 done
-
-#
-# build cm3cg
-#
-
 rm -rf $INSTALL/pkg
+mkdir $INSTALL/pkg
+for a in m3core libm3 sysutils; do
+	cp -pR $BOOT/pkg/$a $INSTALL/pkg/$a
+done
+
+#
+# build cm3cg, maybe
+# It isn't working, it is slow, and it isn't likely relevant to current investigation.
+#
+
+rm -f $SOURCE/m3-sys/m3cc/$TARGET/cm3cg*
+rm -f $SOURCE/m3-sys/m3cc/$TARGET/gcc/m3cgc1*
+#rm -rf $INSTALL/pkg
 #clean m3-sys/m3cc || exit 1
-buildship m3-sys/m3cc || exit 1
-copy_exe $SOURCE/m3-sys/m3cc/$TARGET/cm3cg $INSTALL/bin
+#buildship m3-sys/m3cc || exit 1
+#copy_exe $SOURCE/m3-sys/m3cc/$TARGET/cm3cg $INSTALL/bin
 
 #
 # build up to cm3 and mklib
+# Do it twice so it builds itself.
+# the first time, skip m3core and libm3
 #
 
-p=" m3-libs/m3core \
-    m3-libs/libm3 \
-    m3-libs/sysutils \
-    m3-sys/m3middle \
+p=" m3-sys/m3middle \
     m3-sys/m3quake \
     m3-sys/m3front \
     m3-sys/m3linker \
@@ -145,22 +147,22 @@ p=" m3-libs/m3core \
     m3-sys/cm3 \
     m3-sys/mklib"
 
-#
-# Do it twice so it builds itself.
-#
+q=" m3-libs/m3core \
+    m3-libs/libm3 \
+    m3-libs/sysutils \
+    $p"
 
-rm -rf $INSTALL/pkg
-for a in $p; do clean $a || exit 1; done
-for a in $p; do buildlocal $a || exit 1; done
-for a in cm3 mklib; do
-  copy_exe $SOURCE/m3-sys/$a/$TARGET/$a $INSTALL/bin
-done
-
-rm -rf $INSTALL/pkg
-for a in $p; do clean $a || exit 1; done
+for a in $q; do clean $a || exit 1; done
 for a in $p; do buildship $a || exit 1; done
 for a in cm3 mklib; do
   copy_exe $SOURCE/m3-sys/$a/$TARGET/$a $INSTALL/bin
 done
 
-echo success
+rm -rf $INSTALL/pkg
+for a in $q; do clean $a || exit 1; done
+for a in $q; do buildship $a || exit 1; done
+for a in cm3 mklib; do
+  copy_exe $SOURCE/m3-sys/$a/$TARGET/$a $INSTALL/bin
+done
+
+echo "success"
