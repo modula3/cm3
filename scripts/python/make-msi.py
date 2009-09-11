6c54295bb794603754ca63ada1a7a7534a7ef966
jkrell <>
2009-09-11 15:32:41 +0000

    some small fixes to msi creation and license install (such as actually install the licenses before making the msi)

--- scripts/python/make-msi.py
+++ scripts/python/make-msi.py
@@ -4,11 +4,13 @@
 import sys
 from pylib import *
 
+program = GetLastPathElement(sys.argv[0])
+
 if len(sys.argv) != 2:
-    print("usage: %s rootdir" % GetLastPathElement(sys.argv[0]))
+    print("usage: %s rootdir" % program)
     sys.exit(1)
 
 MakeMSIWithWix(sys.argv[1])
 
-print("%s: Success, result is %s.*, particularly %s.msi" % (GetLastPathElementsys.argv[0], sys.argv[1], sys.argv[1]))
+print("%s: Success, result is %s.*, particularly %s.msi" % (program, sys.argv[1], sys.argv[1]))
 sys.exit(0)
