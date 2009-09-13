376096e1fb9289cfc517788f582b3162064a796d
Jay Michael Krell <jay.krell@cornell.edu>
2009-09-13 17:35:34 +0000

    initial guess, might need to put back tools/tk/scanner/parser/bundle

--- scripts/examples/make-formsedit.sh
+++ scripts/examples/make-formsedit.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-#
 # echo everything and fail on any error
-#
 
 set -e
 set -x
 
-#
 # establish some parameters
-#
 
 if [ "x$CM3_VERSION" = "x" ]; then
     CM3_VERSION=2009-05-01
@@ -23,9 +19,7 @@ TARGET=I386_DARWIN
 CM3_TARGET=$TARGET
 export CM3_TARGET
 
-#
 # functions
-#
 
 copy_exe() {
   a=`basename $1`
@@ -40,26 +34,17 @@ clean() {
 }
 
 buildship() {
-	cd $SOURCE/$1 || exit 1
-	type cm3 || exit 1
-	cm3 $CM3_FLAGS || exit 1
-	cm3 $CM3_FLAGS -ship || exit 1
+    cd $SOURCE/$1 || exit 1
+    type cm3 || exit 1
+    cm3 $CM3_FLAGS || exit 1
+    cm3 $CM3_FLAGS -ship || exit 1
 }
 
 PATH="$INSTALL/bin:$PATH"
 export PATH
 hash -r || true
 
-p=" m3-libs/m3tk-misc \
-    m3-sys/m3scanner \
-    m3-sys/m3tools \
-    m3-tools/m3scan \
-    m3-tools/m3bundle \
-    m3-tools/m3tk \
-    m3-tools/mtex \
-    m3-comm/tcp \
-    m3-comm/netobj \
-    m3-comm/stubgen \
+p=" m3-comm/tcp \
     m3-ui/X11R4 \
     m3-ui/ui \
     m3-ui/vbtkit \
@@ -68,10 +53,7 @@ p=" m3-libs/m3tk-misc \
     m3-ui/videovbt \
     m3-ui/formsvbtpixmaps \
     m3-ui/formsvbt \
-    m3-ui/juno-2/juno-machine \
-    m3-ui/juno-2/juno-compiler \
-    m3-ui/juno-2/juno-app/pkl-fonts \
-    m3-ui/juno-2/juno-app"
+    m3-ui/formsedit"
 
 for a in $p; do clean $a || exit 1; done
 for a in $p; do buildship $a || exit 1; done
