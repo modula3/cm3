7555339b8c3a8d3ba6f29858dbce822d3ee07e0a
jkrell <>
2009-09-21 21:19:44 +0000

    merge with release

--- scripts/pkgmap.sh
+++ scripts/pkgmap.sh
@@ -343,33 +343,32 @@ write_pkg_report() {
 
 if [ -n "${REPORT}" ]; then
   report_header
+
+  make_date() {
+    cat >date.c <<End
+#include <stdio.h>
+#include <time.h>
+int main()
+{
+  printf("%lu\n", (unsigned long)time(NULL));
+  return 0;
+}
+End
+    rm -f m3date m3date.exe
+    for cc in /opt/SUNWspro/bin/cc /usr/sfw/bin/gcc /usr/bin/cc /usr/bin/gcc cc gcc; do
+      if [ type $cc >/dev/null 2>&1 ]; then
+        $cc date.c -o m3date
+        if [ -x m3date ]; then
+          return
+        fi
+      fi
+    done
+  }
+  make_date
 fi
 
 M3GDB=yes
 OK=yes
-
-make_date() {
-  cat >date.c <<End
-    #include <stdio.h>
-    #include <time.h>
-    int main()
-    {
-      printf("%lu\n", (unsigned long)time(NULL));
-      return 0;
-    }
-End
-  rm m3date
-  for cc in /opt/SUNWspro/bin/cc /usr/sfw/bin/gcc /usr/bin/cc /usr/bin/gcc cc gcc; do
-    if [ type $cc >/dev/null 2>&1 ]; then
-      $cc date.c -o m3date
-      if [ -x m3date ]; then
-        return
-      fi
-    fi
-  done
-}
-
-make_date
 
 for PKG in ${PKGS}; do
   echo "=== package ${PKG} ==="
