d341afb8e0df768a22cf0ebd1c0e93c095298aaa
Jay Michael Krell <jay.krell@cornell.edu>
2009-09-27 12:47:16 +0000

    put back false; the point is to prime the loop condition (which might need more work now with my .msi/.deb upload)

--- scripts/make-dist.sh
+++ scripts/make-dist.sh
@@ -1,5 +1,5 @@
 #bash
-# $Id: make-dist.sh,v 1.26.2.42 2009-09-27 10:00:29 jkrell Exp $
+# $Id: make-dist.sh,v 1.26.2.43 2009-09-27 12:47:16 jkrell Exp $
 
 if test "x${CM3CVSUSER}" != "x"; then
   CM3CVSUSER_AT="${CM3CVSUSER}@"
@@ -379,7 +379,7 @@ fi
 if [ "$SHIPRC" = "y" -o "$SHIPRC" = "yes" ]; then
   RSYNC=${RSYNC:-"rsync -vu"}
   type rsync || RSYNC=scp
-  while [ $? != 0 ]; do
+  false; while [ $? != 0 ]; do
     $RSYNC ${STAGE}/cm3-*-${DS}.tgz $DESTHOST:/var/www/modula3.elegosoft.com/cm3/releng
     # Can we just list these on the previous line, or only if they exist?
     for ext in msi deb; do
