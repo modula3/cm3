3cc905e58fc7c86236807e26f3d59fb3949a4bf7
Jay Michael Krell <jay.krell@cornell.edu>
2009-10-24 14:35:59 +0000

    fix warnings and make calling convention explicit

--- m3-libs/m3core/src/runtime/common/RTUntracedMemoryC.c
+++ m3-libs/m3core/src/runtime/common/RTUntracedMemoryC.c
@@ -7,6 +7,17 @@ reducing C runtime dependency.
 
 #include <stddef.h>
 
+#ifdef _MSC_VER
+#pragma warning(disable:4201) /* nonstandard extension (windows.h) */
+#pragma warning(disable:4214) /* nonstandard extension (windows.h) */
+#pragma warning(disable:4115) /* named type definition in paren (windows.h) */
+#pragma warning(disable:4514) /* unreferenced inline function */
+#else
+#ifndef __cdecl
+#define __cdecl /* nothing */
+#endif
+#endif
+
 #ifdef _WIN32
 #include <windows.h>
 #define WIN(x) x
@@ -33,27 +44,24 @@ extern "C" {
 #endif
 
 
-void* RTUntracedMemory__AllocZ ANSI((size_t count)) KR((count) size_t count;)
+void* __cdecl RTUntracedMemory__AllocZ ANSI((size_t count)) KR((count) size_t count;)
 /* Z = zeroed = calloc */
 {
     return WIN(HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, count))
            POSIX(calloc(count, 1));
 }
 
-void* RTUntracedMemory__AllocZV ANSI((size_t count, size_t size)) KR((count, size) size_t count COMMA size;)
+void* __cdecl RTUntracedMemory__AllocZV ANSI((size_t count, size_t size)) KR((count, size) size_t count COMMA size;)
 /* ZV = zeroed vector = calloc */
 {
     size_t max = (~(size_t)0);
     if ((count > 1) && (size > 1) && count > max / size) /* implies count * size > max */
         return 0;
-#ifdef _WIN32
-    return WIN(HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, count));
-#else
-    return calloc(count, size);
-#endif
+    return WIN(HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, count))
+           POSIX(calloc(count, size));
 }
 
-void RTUntracedMemory__Free ANSI((void** p)) KR((p) void** p;)
+void __cdecl RTUntracedMemory__Free ANSI((void** p)) KR((p) void** p;)
 {
     void* q = *p;
     *p = 0;
