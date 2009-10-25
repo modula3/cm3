2e7dc013c0686499a00fd0236f6f6082b72b6087
Jay Michael Krell <jay.krell@cornell.edu>
2009-10-25 10:07:35 +0000

    experiment with ANSI vs. K&R

--- m3-libs/m3core/src/runtime/common/RTUntracedMemoryC.c
+++ m3-libs/m3core/src/runtime/common/RTUntracedMemoryC.c
@@ -8,9 +8,10 @@ reducing C runtime dependency.
 #include <stddef.h>
 
 #ifdef _MSC_VER
+#pragma warning(disable:4115) /* named type definition in paren (windows.h) */
+#pragma warning(disable:4131) /* old style */
 #pragma warning(disable:4201) /* nonstandard extension (windows.h) */
 #pragma warning(disable:4214) /* nonstandard extension (windows.h) */
-#pragma warning(disable:4115) /* named type definition in paren (windows.h) */
 #pragma warning(disable:4514) /* unreferenced inline function */
 #else
 #ifndef __cdecl
@@ -29,46 +30,45 @@ reducing C runtime dependency.
 #endif
 
 #if defined(__STDC__) || defined(__cplusplus) || defined(_MSC_VER)
-#define ANSI(x) x
-#define KR(x)
+#define PROTO1(t1, a1) (t1 a1)
+#define PROTO2(t1, a1, t2, a2) (t1 a1, t2 a2)
+#define VOID_VALUE /* nothing */
 #else
-#define ANSI(x)
-#define KR(x) x
 #define void char
+#define PROTO1(t1, a1) (a1) t1 a1;
+#define PROTO2(t1, a1, t2, a2) (a1, a2) t1 a1; t2 a2;
+#define VOID_VALUE 0
 #endif
-
-#define COMMA ,
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-
-void* __cdecl RTUntracedMemory__AllocZ ANSI((size_t count)) KR((count) size_t count;)
+void* __cdecl RTUntracedMemory__AllocZ PROTO1(size_t, count)
 /* Z = zeroed = calloc */
 {
     return WIN(HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, count))
            POSIX(calloc(count, 1));
 }
 
-void* __cdecl RTUntracedMemory__AllocZV ANSI((size_t count, size_t size)) KR((count, size) size_t count COMMA size;)
+void* __cdecl RTUntracedMemory__AllocZV PROTO2(size_t, count, size_t, size)
 /* ZV = zeroed vector = calloc */
 {
-    size_t max = (~(size_t)0);
-    if ((count > 1) && (size > 1) && count > max / size) /* implies count * size > max */
+    size_t max = ~(size_t)0;
+    if (count > 1 && size > 1 && count > (max / size)) /* implies count * size > max */
         return 0;
     return WIN(HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, count))
            POSIX(calloc(count, size));
 }
 
-void __cdecl RTUntracedMemory__Free ANSI((void** p)) KR((p) void** p;)
+void __cdecl RTUntracedMemory__Free PROTO1(void**, p)
 {
     void* q = *p;
     *p = 0;
     if (q)
         WIN(HeapFree(GetProcessHeap(), 0, q))
         POSIX(free(q));
-    KR(return 0;)
+    return VOID_VALUE;
 }
 
 #ifdef __cplusplus
