e6749e54c31036f9597a247c82f2976391581cb5
hosking <>
2009-10-25 13:45:26 +0000

    Revert to using RTMachine__SaveRegsInStack.

--- m3-libs/m3core/src/thread/PTHREAD/ThreadPThread.s
+++
@@ -1,8 +0,0 @@
-        .seg    "text"
-        .globl  ThreadPThread__FlushWindows
-ThreadPThread__FlushWindows:
-        ta      0x3   ! ST_FLUSH_WINDOWS
-        mov     %sp,%o0
-        retl
-        nop
-        .size ThreadPThread__FlushWindows, (.-ThreadPThread__FlushWindows)
