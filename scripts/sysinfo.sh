dca431352c2478b89227ea9cc96ddaa22f7d99a5
Jay Michael Krell <jay.krell@cornell.edu>
2009-11-03 12:02:38 +0000

    reduce echoing that causes problems

--- scripts/sysinfo.sh
+++ scripts/sysinfo.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: sysinfo.sh,v 1.74.2.19 2009-09-27 19:09:38 jkrell Exp $
+# $Id: sysinfo.sh,v 1.74.2.20 2009-11-03 12:02:38 jkrell Exp $
 
 if [ "$SYSINFO_DONE" != "yes" ] ; then
 
@@ -98,9 +98,10 @@ find_in_list() {
         for a in $2; do
             for b in $a ${a}.exe; do
                 if type $b >/dev/null 2>/dev/null; then
-                    echo $1=$b
+                    # These echos break m3-sys/cm3/version.quake
+                    # echo $1=$b
                     eval $1=$b
-                    echo export $1
+                    # echo export $1
                     export $1
                     return
                 fi
