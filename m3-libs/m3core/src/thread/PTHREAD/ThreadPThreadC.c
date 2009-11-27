c0bf8879f01af56962e4e7d9d52a4a1b3f7f35cd
jkrell <>
2009-11-27 12:27:26 +0000

    attempt to solve some of the problems and flaws  - only compile ThreadInterix.c on interix, remove dummy  - modify SYSTEM_CC to point to headers  - clevely implement pthread_self as returning NT thread handle (DuplicateObject(CurrentThread())

--- m3-libs/m3core/src/thread/PTHREAD/ThreadPThreadC.c
+++ m3-libs/m3core/src/thread/PTHREAD/ThreadPThreadC.c
@@ -383,11 +383,15 @@ ThreadPThread__pthread_detach_self(void)
   return pthread_detach(pthread_self());
 }
 
+#ifndef __INTERIX
+
 m3_pthread_t ThreadPThread__pthread_self(void)
 {
   pthread_t a = pthread_self();
   return PTHREAD_TO_M3(a);
 }
+
+#endif
 
 int
 ThreadPThread__pthread_equal(m3_pthread_t t1, m3_pthread_t t2)
