a1a1d6219d21dbfa413c20d2984e82c80de694c2
Jay Michael Krell <jay.krell@cornell.edu>
2009-11-27 08:26:39 +0000

    any file that uses errno or h_errno, at least, needs #define _REENTRANT for Interix (might change in the config file); this file is only for bootstrapping against older builds so probably could be left 'broken'

--- m3-libs/sysutils/src/SystemC.c
+++ m3-libs/sysutils/src/SystemC.c
@@ -4,6 +4,10 @@
 copied from m3core/src/C/Common/Cerrno.i3 for bootstrapping against older releases 
  */
 
+#if defined(__INTERIX) && !defined(_REENTRANT)
+#define _REENTRANT
+#endif
+
 #include <errno.h>
 
 int
