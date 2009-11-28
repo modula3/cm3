f69a8e010195b8e226e454336fc5fea1e0cb2833
Jay Michael Krell <jay.krell@cornell.edu>
2009-11-28 07:38:55 +0000

    need -f on rm commands to avoid error for nonexistant files

--- scripts/python/pylib.py
+++ scripts/python/pylib.py
@@ -1291,10 +1291,10 @@ def Boot():
         if Target.find("INTERIX") == -1:
             a.write("$(Link) -o cm3 *.o\n")
         else:
-            a.write("rm ntdll.def ntdll.lib ntdll.dll ntdll.o ntdll.c.o a.out a.exe cm3 cm3.exe libntdll.a\n")
+            a.write("rm -f ntdll.def ntdll.lib ntdll.dll ntdll.o ntdll.c.o a.out a.exe cm3 cm3.exe libntdll.a\n")
             a.write("gcc -c ntdll.c\n")
             a.write("/opt/gcc.3.3/i586-pc-interix3/bin/dlltool --dllname ntdll.dll --kill-at --output-lib libntdll.a --export-all-symbols ntdll.o\n")
-            a.write("rm ntdll.o ntdll.c.o _m3main.c.o _m3main.o\n")
+            a.write("rm -f ntdll.o ntdll.c.o _m3main.c.o _m3main.o\n")
             a.write("gcc -g -o cm3 _m3main.c *.o -lm -L . -lntdll\n")
 
     Common = "Common"
