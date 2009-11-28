a906616f4a26e8bae2f0da2ffeab652fdb43461d
Jay Michael Krell <jay.krell@cornell.edu>
2009-11-28 11:20:39 +0000

    extern C

--- m3-win/ntdll/src/ntdll.c
+++ m3-win/ntdll/src/ntdll.c
@@ -6,7 +6,15 @@
 #define __stdcall /* nothing */
 #endif
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 long __stdcall NtDuplicateObject(int a, int b, int c, int d, int e, int f, int g) { return -1; }
 long __stdcall NtSuspendThread(int thread, int suspendCount) { return -1; }
 long __stdcall NtResumeThread(int thread, int suspendCount) { return -1; }
 long __stdcall NtGetContextThread(int thread, int context) { return -1; }
+
+#ifdef __cplusplus
+}
+#endif
