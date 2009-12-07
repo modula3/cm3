579a14bd704271c200991059f872f0bfa558e1ff
jkrell <>
2009-12-07 09:11:21 +0000

    put extern "C" in the macros so that the really simple files can be even a few lines shorter (really we could just delete the extern "C" probably, but I still like the idea)

--- m3-libs/m3core/src/unix/Common/Umman.c
+++ m3-libs/m3core/src/unix/Common/Umman.c
@@ -5,18 +5,10 @@
 #include "m3unix.h"
 #define M3MODULE Umman
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #ifndef _WIN32
 
 M3WRAP3(int, mprotect, ADDRESS, size_t, int)
 M3WRAP6(ADDRESS, mmap, ADDRESS, size_t, int, int, int, m3_off_t)
 M3WRAP2(int, munmap, ADDRESS, size_t)
 
-#endif
-
-#ifdef __cplusplus
-} /* extern "C" */
 #endif
