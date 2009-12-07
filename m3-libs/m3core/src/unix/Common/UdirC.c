d57e361f847af75d9c310a2f25c35753bb5eb21e
jkrell <>
2009-12-07 09:07:17 +0000

    mechanize a few more wrappers

--- m3-libs/m3core/src/unix/Common/UdirC.c
+++ m3-libs/m3core/src/unix/Common/UdirC.c
@@ -10,21 +10,6 @@ extern "C" {
 
 #ifndef _WIN32
 
-/* direct references to opendir on NetBSD:
-
-$ cat 1.c
-#include <stdio.h>
-void opendir(void);
-int main()
-{ printf("%p\n", &opendir);
-  return 0;
-}
-$ cc 1.c
-/var/tmp//ccCjD8oF.o: In function `main':
-1.c:(.text+0x15): warning: warning: reference to compatibility opendir(); include <dirent.h> for correct reference
-
-*/
-
 #define M3MODULE Udir
 M3WRAP1(DIR*, opendir, const char*)
 M3WRAP1(void*, readdir, DIR*)
