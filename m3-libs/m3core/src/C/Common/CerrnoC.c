5e4d05050739c09058ceae8ce4021104beeab995
jkrell <>
2009-12-09 14:20:15 +0000

    bring files from head notice no change to the *3 files, so most of this goes unused when running binaries built against the release *.i3 files, but for binaries from head, there will be some extra exports that might let them work and some existing wrappers are "rewritten" as well

--- m3-libs/m3core/src/C/Common/CerrnoC.c
+++ m3-libs/m3core/src/C/Common/CerrnoC.c
@@ -2,10 +2,17 @@
  * Helper functions for accessing standard C's "errno" value.
  */
 
+#if defined(__INTERIX) && !defined(_REENTRANT)
+#define _REENTRANT
+#endif
 #include <errno.h>
 
 #ifdef __cplusplus
 extern "C" {
+#endif
+
+#ifdef _MSC_VER
+#pragma optimize("gty", on)
 #endif
 
 int
