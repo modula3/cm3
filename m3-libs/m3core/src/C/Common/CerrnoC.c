6c7d059e9428bd539bab3db8fb907e611b72c34d
jkrell <>
2009-12-13 06:57:48 +0000

    provide one m3core.lib.sa that should work with both   libcmt.lib and msvcrt.lib (ie: suppress __declspec(dllimport)   on functions, and #if out data (stdin/out/err))

--- m3-libs/m3core/src/C/Common/CerrnoC.c
+++ m3-libs/m3core/src/C/Common/CerrnoC.c
@@ -2,17 +2,20 @@
  * Helper functions for accessing standard C's "errno" value.
  */
 
+#ifdef _MSC_VER
+#pragma optimize("gty", on)
+#undef _DLL
+#define _MT
+#endif
+
 #if defined(__INTERIX) && !defined(_REENTRANT)
 #define _REENTRANT
 #endif
+
 #include <errno.h>
 
 #ifdef __cplusplus
 extern "C" {
-#endif
-
-#ifdef _MSC_VER
-#pragma optimize("gty", on)
 #endif
 
 int
