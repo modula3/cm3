ac56250554ba3508f729aad117f8cd47d643b8df
Jay Michael Krell <jay.krell@cornell.edu>
2009-12-14 13:57:30 +0000

    working version of http://www.engelschall.com/pw/usenix/2000/pmt-html/  - This should be moved to m3core/src/thread/POSIX  - Notice how I switched from virtual timer to real timer,     in the test case that mimics m3core/src/thread/POSIX.     "This seems to work much better."     In particular, if the main thread does while (1) sleep(0)       to let things run, nothing runs. The virtual time doesn't       run down at all. If I make it a busy wait "while (1) ;" then       it does work.
     Note that Apple seems very down on supplying get/make/set/swapcontext (read the mailing lists).
     This solution might be preferred? Though it is slower to create a thread here, due to
      extra gymnastics to establish the initial context.
    
     Also still some experiments to try.
       _setjmp vs. setjmp vs. sigsetjmp.
       Best is probably sigsetjmp(1).
       using the same signal in both parts

---
+++ m3-libs/m3core/src/unix/Common/context/sigaltstack/context.c
@@ -0,0 +1,109 @@
+/*
+see http://www.opengroup.org/onlinepubs/009695399/functions/swapcontext.html
+see http://www.engelschall.com/pw/usenix/2000/pmt-html/
+*/
+
+#include "context.h"
+#include <string.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/signal.h>
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+static Context_t mctx_caller;
+static sig_atomic_t volatile mctx_called;
+static Context_t * volatile mctx_create;
+static void (* volatile mctx_create_func)(void *);
+static void * volatile mctx_create_arg;
+static sigset_t mctx_create_sigs;
+
+static void mctx_create_boot(void)
+{
+    void (*mctx_start_func)(void*);
+    void* mctx_start_arg;
+    
+    sigprocmask(SIG_SETMASK, &mctx_create_sigs, NULL);
+    
+    mctx_start_func = mctx_create_func;
+    mctx_start_arg = mctx_create_arg;
+    
+    SwapContext(mctx_create, &mctx_caller);
+    
+    mctx_start_func(mctx_start_arg);
+
+    printf("end thread\n");
+    while(1) sleep(100);
+    abort(); /* not reached */
+}
+
+static void mctx_create_trampoline(int sig)
+{
+    if (_setjmp(mctx_create->jb) == 0)
+    {
+        mctx_called = 1;
+        return;
+    }
+    
+    mctx_create_boot();
+}
+
+void
+MakeContext( 
+    Context_t *context, 
+    void (*function)(void *),
+    void *arg, 
+    void *stack,
+    size_t stack_size) 
+{
+    struct sigaction sa = { 0 };
+    struct sigaction osa = { 0 };
+    stack_t ss = { 0 };
+    stack_t oss = { 0 };
+    sigset_t osigs = { 0 };
+    sigset_t sigs = { 0 };
+
+    sigemptyset(&sigs);
+    sigaddset(&sigs, SIGUSR2);
+    sigprocmask(SIG_BLOCK, &sigs, &osigs);
+    
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = mctx_create_trampoline;
+    sa.sa_flags = SA_ONSTACK;
+    sigemptyset(&sa.sa_mask);
+    sigaction(SIGUSR2, &sa, &osa);
+    
+    ss.ss_sp = stack;
+    ss.ss_size = stack_size;
+    ss.ss_flags = 0;
+    sigaltstack(&ss, &oss);
+    
+    mctx_create = context;
+    mctx_create_func = function;
+    mctx_create_arg = arg;
+    mctx_create_sigs = osigs;
+    mctx_called = 0;
+    kill(getpid(), SIGUSR2);
+    sigfillset(&sigs);
+    sigdelset(&sigs, SIGUSR2);
+    while (!mctx_called)
+        sigsuspend(&sigs);
+        
+    sigaltstack(NULL, &ss);
+    ss.ss_flags = SS_DISABLE;
+    sigaltstack(&ss, NULL);
+    if (!(oss.ss_flags & SS_DISABLE))
+        sigaltstack(&oss, NULL);
+    sigaction(SIGUSR2, &osa, NULL);
+    sigprocmask(SIG_SETMASK, &osigs, NULL);
+    
+    SwapContext(&mctx_caller, context);
+}
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
