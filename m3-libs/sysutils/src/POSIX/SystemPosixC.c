64865c3ef6d4fc08d925bf2f1f693cf0317a9973
Jay Michael Krell <jay.krell@cornell.edu>
2009-12-14 06:54:23 +0000

    trim this down; maybe remove it? removing it will increase what minimum toolset can be used to build; but we really need to know here if we have user threads or not, I'm seeing a hang on SPARC64_OPENBSD..though I386_OPENBSD works ok?

--- m3-libs/sysutils/src/POSIX/SystemPosixC.c
+++ m3-libs/sysutils/src/POSIX/SystemPosixC.c
@@ -2,48 +2,14 @@
 /* All rights reserved.                                        */
 /* See the file COPYRIGHT for a full description.              */
 
-#define _FILE_OFFSET_BITS 64
-
-#ifdef __INTERIX
-#ifndef _ALL_SOURCE
-#define _ALL_SOURCE
-#endif
-#ifndef _REENTRANT
-#define _REENTRANT
-#endif
-#endif
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <assert.h>
-#include <stddef.h>
-#include <time.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <string.h>
-#include <signal.h>
-#include <sys/ioctl.h>
-#include <sys/mman.h>
-#include <sys/socket.h>
-#include <sys/time.h>
 #include <sys/wait.h>
-#include <dirent.h>
-#include <grp.h>
-#include <netdb.h>
-#include <pthread.h>
-#include <unistd.h>
-#include <pwd.h>
-#define ZeroMemory(a,b) (memset((a), 0, (b)))
-#ifdef __INTERIX
-#include <utime.h>
-#endif
+#include <stddef.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef ptrdiff_t INTEGER;
-typedef INTEGER m3_pid_t;
+typedef ptrdiff_t m3_pid_t;
 
 m3_pid_t System__waitpid(m3_pid_t pid, int* status, int options)
 {
