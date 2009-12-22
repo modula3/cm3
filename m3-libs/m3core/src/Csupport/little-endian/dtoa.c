1fdc0f95a3680fb8abe429cb925128fe5df1f231
jkrell <>
2009-12-22 10:43:55 +0000

    from head: fixes in case anyone wants to compile as C++  extern C, "new" (20 year old) function style

--- m3-libs/m3core/src/Csupport/little-endian/dtoa.c
+++ m3-libs/m3core/src/Csupport/little-endian/dtoa.c
@@ -2,7 +2,9 @@
 /* All rights reserved.                                                      */
 /* See the file COPYRIGHT for a full description.                            */
 
+#ifndef __cplusplus
 #define KR_headers
+#endif
 #define IEEE_8087
 #undef IEEE_MC68k
 
