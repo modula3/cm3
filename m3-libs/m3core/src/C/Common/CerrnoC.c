aeb9a7fd8bc602a8fdfc376ec7638a02dceb9b8d
Jay Michael Krell <jay.krell@cornell.edu>
2009-12-23 17:59:12 +0000

    from head: calling convention

--- m3-libs/m3core/src/C/Common/CerrnoC.c
+++ m3-libs/m3core/src/C/Common/CerrnoC.c
@@ -8,6 +8,10 @@
 #define _MT
 #endif
 
+#if !defined(_MSC_VER) && !defined(__cdecl)
+#define __cdecl /* nothing */
+#endif
+
 #if defined(__INTERIX) && !defined(_REENTRANT)
 #define _REENTRANT
 #endif
@@ -19,12 +23,14 @@ extern "C" {
 #endif
 
 int
+__cdecl
 Cerrno__GetErrno(void)
 {
     return errno;
 }
 
 void
+__cdecl
 Cerrno__SetErrno(int e)
 {
     errno = e;
