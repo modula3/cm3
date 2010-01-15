6d79f6edd591e14141b9fafb92cd7b06f153080b
Jay Michael Krell <jay.krell@cornell.edu>
2010-01-15 22:31:29 +0000

    provide 32bit, 64bit, and integer, though we can't expose the 64bit one

--- m3-libs/sysutils/src/FSUtilsUnsafe.c
+++ m3-libs/sysutils/src/FSUtilsUnsafe.c
@@ -1,9 +1,29 @@
 #if !defined(_MSC_VER) && !defined(__cdecl)
 #define __cdecl /* nothing */
 #endif
+#define _FILE_OFFSET_BITS 64
+
+#ifdef _MSC_VER
+typedef signed __int64 INT64;
+/*#pragma warning(disable:4820)*/
+/*#pragma warning(disable:4668)*/
+/*#pragma warning(disable:4255)*/
+#else
+typedef signed long long INT64;
+#endif
 
 #include <stddef.h>
 #include <limits.h>
+
+typedef ptrdiff_t INTEGER;
+
+#ifdef _WIN64
+#define INTEGER_MAX INT64_MAX
+#define INTEGER_MIN INT64_MIN
+#else
+#define INTEGER_MAX LONG_MAX
+#define INTEGER_MIN LONG_MIN
+#endif
 
 #ifdef _WIN32
 #include <windows.h>
@@ -15,23 +35,33 @@
 extern "C" {
 #endif
 
-ptrdiff_t __cdecl FSUtilsUnsafe__GetFileSize32(const char* a)
+INT64 __cdecl FSUtilsUnsafe__GetFileSize64(const char* path)
 {
 #ifdef _WIN32
     WIN32_FIND_DATAA data;
-    HANDLE h = FindFirstFileA(a, &data);
+    HANDLE h = FindFirstFileA(path, &data);
     if (h == INVALID_HANDLE_VALUE)
         return 0;
     FindClose(h);
-    if (data.nFileSizeHigh || data.nFileSizeLow > INT_MAX)
-        return 0;
-    return (ptrdiff_t)data.nFileSizeLow;
+    return ((((INT64)data.nFileSizeHigh) << 32) | data.nFileSizeLow);
 #else
     struct stat st;
-    if (stat(a, &st) || st.st_size > INT_MAX)
+    if (stat(path, &st))
         return 0;
-    return (ptrdiff_t)st.st_size;
+    return st.st_size;
 #endif
+}
+
+INTEGER __cdecl FSUtilsUnsafe__GetFileSize(const char* path)
+{
+    INT64 size = FSUtilsUnsafe__GetFileSize64(path);
+    return ((size >= INTEGER_MIN && size <= INTEGER_MAX) ? (INTEGER)size : 0);
+}
+
+int __cdecl FSUtilsUnsafe__GetFileSize32(const char* path)
+{
+    INT64 size = FSUtilsUnsafe__GetFileSize64(path);
+    return ((size >= INT_MIN && size <= INT_MAX) ? (int)size : 0);
 }
 
 #ifdef __cplusplus
