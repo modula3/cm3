828016d9144c4b26792beb514545c65814296cc2
jkrell <>
2010-01-15 13:43:44 +0000

    from head: don't use libm3 File.T.status().size, so it can vary between  INTEGER in old versions and LONGINT in new versions  (This is only a problem for a small part of the system,  that has to work with old libm3.)

---
+++ m3-sys/m3quake/src/QScannerC.c
@@ -0,0 +1,42 @@
+#include <stddef.h>
+#include <limits.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <sys/stat.h>
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef _WIN32
+
+ptrdiff_t QScanner__GetFileSize(const char* a)
+{
+    WIN32_FIND_DATAA data;
+    HANDLE h = FindFirstFileA(a, &data);
+    if (h == INVALID_HANDLE_VALUE)
+        return 0;
+    FindClose(h);
+    if (data.nFileSizeHigh || data.nFileSizeLow > INT_MAX)
+        return 0;
+    return (ptrdiff_t)data.nFileSizeLow;
+}
+
+#else
+
+ptrdiff_t QScanner__GetFileSize(const char* a)
+{
+    struct stat st;
+    if (stat(a, &st) || st.st_size > INT_MAX)
+        return 0;
+    return (ptrdiff_t)st.st_size;
+}
+
+#endif
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
