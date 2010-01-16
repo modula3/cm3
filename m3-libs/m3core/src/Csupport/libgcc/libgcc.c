8d31a07ac7a4a768d0af568657022274cc720c90
jkrell <>
2010-01-16 11:46:20 +0000

    put in calling conventions; doesn't likely matter here, this file is mostly for SOLgnu -- gcc backend without gcc linker (maybe also Irix, AIX, HP-UX)

--- m3-libs/m3core/src/Csupport/libgcc/libgcc.c
+++ m3-libs/m3core/src/Csupport/libgcc/libgcc.c
@@ -25,6 +25,10 @@ NT386 linking to NT386GNU /cm3/lib/hand.obj, which should probably move to eithe
     /cm3/lib/target/hand.obj, probably the first.
 */
 
+#if !defined(_MSC_VER) && !defined(__cdecl)
+#define __cdecl /* nothing */
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -54,43 +58,43 @@ l is logical -- unsigned
 a is arithmetic -- signed
 */
 
- INT64 __ashldi3 ( INT64 x, int c) { return x << c; }
- INT64 __ashrdi3 ( INT64 x, int c) { return x >> c; }
-UINT64 __lshrdi3 (UINT64 x, int c) { return x >> c; }
+ INT64 __cdecl __ashldi3 ( INT64 x, int c) { return x << c; }
+ INT64 __cdecl __ashrdi3 ( INT64 x, int c) { return x >> c; }
+UINT64 __cdecl __lshrdi3 (UINT64 x, int c) { return x >> c; }
 
- INT64  __divdi3 ( INT64 x,  INT64 y) { return x / y; }
-UINT64 __udivdi3 (UINT64 x, UINT64 y) { return x / y; }
+ INT64  __cdecl __divdi3 ( INT64 x,  INT64 y) { return x / y; }
+UINT64 __cdecl __udivdi3 (UINT64 x, UINT64 y) { return x / y; }
 
- INT64  __moddi3 ( INT64 x,  INT64 y) { return x % y; }
-UINT64 __umoddi3 (UINT64 x, UINT64 y) { return x % y; }
+ INT64  __cdecl __moddi3 ( INT64 x,  INT64 y) { return x % y; }
+UINT64 __cdecl __umoddi3 (UINT64 x, UINT64 y) { return x % y; }
 
 /* signed and unsigned multiplication and negation are the same */
-INT64 __muldi3 (INT64 x, INT64 y) { return x * y; }
-INT64 __negdi2 (INT64 x) { return -x; }
+INT64 __cdecl __muldi3 (INT64 x, INT64 y) { return x * y; }
+INT64 __cdecl __negdi2 (INT64 x) { return -x; }
 
-UINT64 __udivmoddi4 (UINT64 x, UINT64 y, UINT64 *r) { *r = x % y; return x / y; }
+UINT64 __cdecl __udivmoddi4 (UINT64 x, UINT64 y, UINT64 *r) { *r = x % y; return x / y; }
 
-int  __cmpdi2 ( INT64 x , INT64 y) { return x < y ? 0 : x == y ? 1 : 2; }
-int __ucmpdi2 (UINT64 x, UINT64 y) { return x < y ? 0 : x == y ? 1 : 2; }
+int  __cdecl __cmpdi2 ( INT64 x , INT64 y) { return x < y ? 0 : x == y ? 1 : 2; }
+int __cdecl __ucmpdi2 (UINT64 x, UINT64 y) { return x < y ? 0 : x == y ? 1 : 2; }
 
 /* INT64 to float (double int to single float) */
-float __floatdisf (INT64 x) { return (float)x; }
+float __cdecl __floatdisf (INT64 x) { return (float)x; }
 
 /* INT64 to double (double int to double float) */
-double __floatdidf (INT64 x) { return (double)x; }
+double __cdecl __floatdidf (INT64 x) { return (double)x; }
 
 /*
 count leading zeros?
-int __clzsi2 (UINT32 x) { return __builtin_clz (x); }
+int __cdecl __clzsi2 (UINT32 x) { return __builtin_clz (x); }
 
 count trailing zeros?
-int __ctzsi2 (UINT32 x) { return __builtin_ctz (x); }
+int __cdecl __ctzsi2 (UINT32 x) { return __builtin_ctz (x); }
 
 1 if an odd number of bits set, 0 if an even number of bits set?
-int __paritysi2 (UINT32 x) { return __builtin_parity (x); }
+int __cdecl __paritysi2 (UINT32 x) { return __builtin_parity (x); }
 
 count set bits?
-int __popcountsi2 (UINT32 x) { return __builtin_popcount (x); }
+int __cdecl __popcountsi2 (UINT32 x) { return __builtin_popcount (x); }
 */
 
 #endif
