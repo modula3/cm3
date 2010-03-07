e1e2c4fc1c4141073d25a6714297aa73dcce27f5
jkrell <>
2010-03-07 03:50:15 +0000

    restore INT64_MIN, INT64_MAX (test code only)

--- m3-libs/m3core/src/Csupport/Common/test_hand.c
+++ m3-libs/m3core/src/Csupport/Common/test_hand.c
@@ -26,6 +26,28 @@ M3_EXTRACT_INSERT(m3test_extract32, m3test_extract_and_sign_extend32, m3test_ins
 #include <limits.h>
 #include <time.h>
 
+#if !defined(INT64_MAX)
+#if defined(LLONG_MAX)
+#define INT64_MAX LLONG_MAX
+#elif defined(__LONG_LONG_MAX__)
+#define INT64_MAX __LONG_LONG_MAX__
+#elif defined(_I64_MAX)
+#define INT64_MAX _I64_MAX
+#endif
+#endif
+
+#if !defined(INT64_MIN)
+#if defined(LLONG_MIN)
+#define INT64_MIN LLONG_MIN
+#elif defined(__LONG_LONG_MIN__)
+#define INT64_MIN __LONG_LONG_MIN__
+#elif defined(_I64_MIN)
+#define INT64_MIN _I64_MIN
+#elif defined(INT64_MAX)
+#define INT64_MIN (-INT64_MAX-(int64)1)
+#endif
+#endif
+
 static int64 values[] = {
     INT64_MIN,
     INT64_MIN + 1,
