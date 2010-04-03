83ef45aacc369631dd85f93ae8650f641c461e1b
jkrell <>
2010-04-03 23:14:06 +0000

    make all, now that the no symbols option is in

--- scripts/python/make-dist-multiple-jay.cmd
+++ scripts/python/make-dist-multiple-jay.cmd
@@ -1,52 +1,63 @@
+SetLocal
+
+pushd %~dp0
 rmdir /q/s \cm3
 xcopy /fiverdy \net\mod3\cm3-min-WIN32-NT386-5.1.3a \cm3
 
-rem call \dev2\j\env\cm3\cm3.vc20
-rem .\do-cm3-all.py realclean
-rem upgrade.py || goto :eof
-rem make-dist.py || goto :eof
+call \dev2\j\env\cm3\cm3.vc50
+.\do-cm3-all.py realclean
+set CM3_NO_SYMBOLS=1
+.\upgrade.py || goto :eof
+set CM3_NO_SYMBOLS=
 
-rem call \dev2\j\env\cm3\cm3.vc40
-rem .\do-cm3-all.py realclean
-rem upgrade.py || goto :eof
-rem make-dist.py || goto :eof
+call \dev2\j\env\cm3\cm3.vc20
+.\do-cm3-all.py realclean
+set CM3_NO_SYMBOLS=1
+.\upgrade.py || goto :eof
+set CM3_NO_SYMBOLS=
+.\make-dist.py || goto :eof
 
-rem call \dev2\j\env\cm3\cm3.vc41
-rem .\do-cm3-all.py realclean
-rem upgrade.py || goto :eof
-rem make-dist.py || goto :eof
+call \dev2\j\env\cm3\cm3.vc40
+.\do-cm3-all.py realclean
+.\upgrade.py || goto :eof
+.\make-dist.py || goto :eof
 
-rem call \dev2\j\env\cm3\cm3.vc42
-rem .\do-cm3-all.py realclean
-rem upgrade.py || goto :eof
-rem make-dist.py || goto :eof
+call \dev2\j\env\cm3\cm3.vc41
+.\do-cm3-all.py realclean
+.\upgrade.py || goto :eof
+.\make-dist.py || goto :eof
+
+call \dev2\j\env\cm3\cm3.vc42
+.\do-cm3-all.py realclean
+.\upgrade.py || goto :eof
+.\make-dist.py || goto :eof
 
 call \dev2\j\env\cm3\cm3.vc50
 .\do-cm3-all.py realclean
-upgrade.py || goto :eof
-make-dist.py || goto :eof
+.\upgrade.py || goto :eof
+.\make-dist.py || goto :eof
 
 call \dev2\j\env\cm3\cm3.vc60
 .\do-cm3-all.py realclean
-upgrade.py || goto :eof
-make-dist.py || goto :eof
+.\upgrade.py || goto :eof
+.\make-dist.py || goto :eof
 
 call \dev2\j\env\cm3\cm3.vc70
 .\do-cm3-all.py realclean
-upgrade.py || goto :eof
-make-dist.py || goto :eof
+.\upgrade.py || goto :eof
+.\make-dist.py || goto :eof
 
 call \dev2\j\env\cm3\cm3.vc71
 .\do-cm3-all.py realclean
-upgrade.py || goto :eof
-make-dist.py || goto :eof
+.\upgrade.py || goto :eof
+.\make-dist.py || goto :eof
 
 call \dev2\j\env\cm3\cm3.vc80
 .\do-cm3-all.py realclean
-upgrade.py || goto :eof
-make-dist.py || goto :eof
+.\upgrade.py || goto :eof
+.\make-dist.py || goto :eof
 
 call \dev2\j\env\cm3\cm3.vc90
 .\do-cm3-all.py realclean
-upgrade.py || goto :eof
-make-dist.py || goto :eof
+.\upgrade.py || goto :eof
+.\make-dist.py || goto :eof
