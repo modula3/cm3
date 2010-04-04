e41e260321eac31b6d65e901f38457ea0a7b4c1c
jkrell <>
2010-04-04 05:06:51 +0000

    skip some steps which I don't think are needed use 'x86' more in place of 'NT386'  (even this maybe is too verbose?   cm3-min-version.msi (x86 implied?)   cm3-all-version.msi (x86 implied?) or   cm3-version.msi (all)   cm3-amd64-version (hypothetial) or   cm3-x86-version.msi (all)   cm3-amd64-version.msi (hypothetical) or   cm3-min-version-timestamp.msi etc.?

--- scripts/python/make-dist-multiple-jay.cmd
+++ scripts/python/make-dist-multiple-jay.cmd
@@ -11,53 +11,31 @@ set CM3_NO_SYMBOLS=1
 set CM3_NO_SYMBOLS=
 
 call \dev2\j\env\cm3\cm3.vc20
-.\do-cm3-all.py realclean
-set CM3_NO_SYMBOLS=1
-.\upgrade.py || goto :eof
-set CM3_NO_SYMBOLS=
 .\make-dist.py || goto :eof
 
 call \dev2\j\env\cm3\cm3.vc40
-.\do-cm3-all.py realclean
-.\upgrade.py || goto :eof
 .\make-dist.py || goto :eof
 
 call \dev2\j\env\cm3\cm3.vc41
-.\do-cm3-all.py realclean
-.\upgrade.py || goto :eof
 .\make-dist.py || goto :eof
 
 call \dev2\j\env\cm3\cm3.vc42
-.\do-cm3-all.py realclean
-.\upgrade.py || goto :eof
 .\make-dist.py || goto :eof
 
 call \dev2\j\env\cm3\cm3.vc50
-.\do-cm3-all.py realclean
-.\upgrade.py || goto :eof
 .\make-dist.py || goto :eof
 
 call \dev2\j\env\cm3\cm3.vc60
-.\do-cm3-all.py realclean
-.\upgrade.py || goto :eof
 .\make-dist.py || goto :eof
 
 call \dev2\j\env\cm3\cm3.vc70
-.\do-cm3-all.py realclean
-.\upgrade.py || goto :eof
 .\make-dist.py || goto :eof
 
 call \dev2\j\env\cm3\cm3.vc71
-.\do-cm3-all.py realclean
-.\upgrade.py || goto :eof
 .\make-dist.py || goto :eof
 
 call \dev2\j\env\cm3\cm3.vc80
-.\do-cm3-all.py realclean
-.\upgrade.py || goto :eof
 .\make-dist.py || goto :eof
 
 call \dev2\j\env\cm3\cm3.vc90
-.\do-cm3-all.py realclean
-.\upgrade.py || goto :eof
 .\make-dist.py || goto :eof
