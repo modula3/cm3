760ff6e9c617670491d183019e8a973bae038c9c
jkrell <>
2010-04-14 14:00:22 +0000

    initial version, not yet active, but nicely much clearer than the corresponding Modula-3, owing to a union to split up 64bit integer into 32bit pieces, as well as having 64bit types (yes, if I used longint in TimeWin32.m3, it'd also be clearer, maybe should)

--- m3-libs/m3core/src/time/WIN32/TimeWin32.c
+++ m3-libs/m3core/src/time/WIN32/TimeWin32.c
@@ -1,51 +1,45 @@
-(* Copyright (C) 1993, Digital Equipment Corporation           *)
-(* All rights reserved.                                        *)
-(* See the file COPYRIGHT for a full description.              *)
-(*                                                             *)
-(* Last modified on Wed Aug 31 09:42:51 PDT 1994 by kalsow     *)
-(*      modified on Wed Sep 22 14:53:33 PDT 1993 by steveg     *)
-(*      modified on Thu Mar 11 13:01:04 PST 1993 by mjordan    *)
+/* Copyright (C) 1993, Digital Equipment Corporation           */
+/* All rights reserved.                                        */
+/* See the file COPYRIGHT for a full description.              */
+/*                                                             */
+/* Last modified on Wed Aug 31 09:42:51 PDT 1994 by kalsow     */
+/*      modified on Wed Sep 22 14:53:33 PDT 1993 by steveg     */
+/*      modified on Thu Mar 11 13:01:04 PST 1993 by mjordan    */
 
-(* $Id$ *)
+#include <windows.h>
 
-UNSAFE MODULE TimeWin32;
+/* Modula-3 Time.T is seconds since Unix Epoch (Jan 1 1970) in floating point.
+ */
 
-IMPORT WinBase, Word;
-IMPORT Time;
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-CONST
-  H = FLOAT(LAST(INTEGER), LONGREAL) + 1.0d0;
-  H2 = 2.0d0 * H;
-  I = Word.Plus(LAST(INTEGER), 1);
+FILETIME
+__cdecl
+TimeWin32__ToFileTime(double t)
+{
+    FILETIME ft;
+    LARGE_INTEGER li;
 
-PROCEDURE ToFileTime(n: Time.T): WinBase.FILETIME=
-  VAR low: LONGREAL;
-    fileTime: WinBase.FILETIME;
-  BEGIN
-    n := n * 1.0D7; 
-    fileTime.dwHighDateTime := TRUNC(n/H2);
-    low := n - FLOAT(fileTime.dwHighDateTime, LONGREAL) * H2;
-    IF low >= H THEN
-      fileTime.dwLowDateTime := Word.Plus(ROUND(low - H), I);
-    ELSE
-       fileTime.dwLowDateTime := ROUND(low)
-    END;
-    RETURN fileTime; 
-  END ToFileTime;
-  
+    t *= 1.0e7;
+    li.QuadPart = (__int64)t;
+    ft.dwLowDateTime = li.LowPart;
+    ft.dwHighDateTime = li.HighPart;
+    return ft;
+}
 
-PROCEDURE FromFileTime (fileTime: WinBase.FILETIME): Time.T =
-  VAR low, high: LONGREAL;
-  BEGIN
-    IF fileTime.dwLowDateTime < 0 THEN
-      low := H + FLOAT(Word.And(fileTime.dwLowDateTime, LAST(INTEGER)),
-                       LONGREAL);
-    ELSE
-      low := FLOAT(fileTime.dwLowDateTime, LONGREAL);
-    END;
-    high := FLOAT(fileTime.dwHighDateTime, LONGREAL) * H2;
-    RETURN (low + high) / 1.0D7;
-  END FromFileTime;
+double
+__cdecl
+TimeWin32__FromFileTime(FILETIME ft)
+{
+    LARGE_INTEGER li;
 
-BEGIN
-END TimeWin32.
+    li.LowPart = ft.dwLowDateTime;
+    li.HighPart = ft.dwHighDateTime;
+    return ((double)li.QuadPart) / 1.0e7;
+}
+
+#ifdef __cplusplus
+} /* extern C */
+#endif
