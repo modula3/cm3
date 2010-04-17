cd5ca3d527a02befddbe4aebdcbc846e76dd8650
Jay Michael Krell <jay.krell@cornell.edu>
2010-04-17 10:15:33 +0000

    switch pthreads and cygwin from select to poll  They only wait for one file at a time, so the change is easier.
    Leave userthreads using select.
     It waits for multiple, so the change is a bit more.

--- m3-libs/m3core/src/thread/Common/ThreadInternal.c
+++ m3-libs/m3core/src/thread/Common/ThreadInternal.c
@@ -3,6 +3,7 @@
 /* See the file COPYRIGHT for a full description.              */
 
 #include "m3core.h"
+#include <poll.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -10,7 +11,34 @@ extern "C" {
 
 #ifndef _WIN32
 
+enum {WaitResult_Ready, WaitResult_Error, WaitResult_FDError, WaitResult_Timeout};
+
 #define MILLION (1000 * 1000)
+
+int
+__cdecl
+ThreadInternal__Poll(int fd,
+                     int/*boolean*/ read,
+                     LONGREAL/*Time.T*/ m3timeout)
+{
+    int r;
+    struct pollfd p;
+
+    ZERO_MEMORY(p);
+    p.fd = fd;
+    p.events = POLLERR | (read ? POLLIN : POLLOUT);
+    p.revents = 0;
+    r = poll(&p, 1, (m3timeout < 0) ? -1 : (m3timeout * MILLION));
+    assert(r == -1 || r == 0 || r == 1);
+    if (r == -1)
+        return WaitResult_Error;
+    else if (r == 0)
+        return WaitResult_Timeout;
+    else if ((read && (p.revents & POLLIN)) || ((!read) && (p.revents & POLLOUT)))
+        return WaitResult_Ready;
+    else
+        return WaitResult_FDError;
+}
 
 int
 __cdecl
