2e1e032b672aa24896526087a655cf11944f8db6
Jay Michael Krell <jay.krell@cornell.edu>
2010-04-19 06:45:07 +0000

    extend better support to MacOSX, OpenBSD (tested)   and presumably FreeBSD and NetBSD (not tested, presumed to be same as OpenBSD)
    keeping support for Linux, Cygwin (tested)
      (but skipping loopback now, not that I saw it happen, but found this code..)
    
    
    keeping lame fallback for Solaris (tested, nothing better found, unless running as root)
    
    
    #error for anything else (OSF, Irix, AIX, Interix, VMS, HP-UX)
      somewhat guessing how to detect them, but also using known ways
      to check for supported systems so ok
    
    
    Code is present for OSF but should be tested.
    AIX, HP-UX, Interix solutions are on network, if they don't resemble what we have, VMS not known.
    Each system needs to be tested.
      (Ok, well, I didn't test FreeBSD And NetBSD, but if they compile, they
       probably work and I can get to those systems reasonably easily enough.)
    
    
    Strongly consider keeping the lame fallback only for Solaris.
    In particular, almost all machines do have MAC addresses.
    Many machines are on private networks, providing not very unique IPv4 addresses.
    Some machines may have IP address but no MAC e.g. for dialup networking.
    So the lame fallback left in for all platforms.
    
    
    Posix says gethostname is limited to 255, which isn't so large, so grow
    our buffer to the maximum 256.
    (This is aka netdb.h MAXHOSTNAMELEN, but we use 256 here.)

--- m3-libs/libm3/src/uid/POSIX/MachineIDPosixC.c
+++ m3-libs/libm3/src/uid/POSIX/MachineIDPosixC.c
@@ -5,83 +5,167 @@
 /* Last modified on Mon Sep 20 11:46:17 PDT 1993 by kalsow     */
 /*      modified on Thu Jul 15 16:23:08 PDT 1993 by swart      */
 
+#include "m3core.h"
 #include <unistd.h>
 #include <netdb.h>
 #include <string.h>
-
-#if defined(__linux__) || defined(__osf__) || defined(__CYGWIN__)
-
+#include <net/if.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h>
 #include <net/if.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+#if defined(__aix) \
+ || defined(__hpux) \
+ || defined(__INTERIX) \
+ || defined(__irix) \
+ || defined(__osf__) \
+ || defined(__sgi) \
+ || defined(__vms)
+/* osf might already work */
+#error Please test/port this.
+#endif
+
+#if !(defined(__APPLE__)    \
+   || defined(__CYGWIN__)   \
+   || defined(__FreeBSD__)  \
+   || defined(__linux__)    \
+   || defined(__NetBSD__)   \
+   || defined(__OpenBSD__)  \
+   || defined(__sun))
+#error Please test/port this.
+#endif
+
+#ifdef __sun
+/* need works but keep the old version */
+#endif
+
+#if defined(__OpenBSD__) || defined(__NetBSD__) || defined(__FreeBSD__) || defined(__APPLE__)
+#define HAS_GETIFADDRS
+#endif
+
+#ifdef HAS_GETIFADDRS
+#include <net/if_dl.h>
+#include <ifaddrs.h>
+#endif
+
+#ifdef __APPLE__
+#ifndef _SIZEOF_ADDR_IFREQ
+#error Apple => _SIZEOF_ADDR_IFREQ
+#endif
+#else
+#ifdef _SIZEOF_ADDR_IFREQ
+#error !Apple => !_SIZEOF_ADDR_IFREQ
+#endif
+#define _SIZEOF_ADDR_IFREQ(a) (sizeof(a))
 #endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int
-MachineIDPosixC__CanGet(char *id)
+int/*boolean*/
+__cdecl
+MachineIDPosixC__CanGet(unsigned char *id)
 {
-    int i;
-    char hostname[128];
-    struct hostent *hostent;
+    int result = { 0 };
+    int s = { 0 };
+#ifdef HAS_GETIFADDRS
+    struct ifaddrs* if1 = { 0 };
+    struct ifaddrs* if2 = { 0 };
+#elif defined(__linux__) || defined(__osf__) || defined(__CYGWIN__)
+    union {
+        struct ifreq req[64];
+        char b[4096];
+    } buf;
+    struct ifconf list = { 0 };
+    struct ifreq* req1 = { 0 };
+    struct ifreq req2 = { 0 };
+    size_t i = { 0 };
+#endif
 
-#if defined(__linux__) || defined(__osf__) || defined(__CYGWIN__)
-
-    struct ifreq req;
-    struct ifconf list;
-    int s;
-    struct ifreq buf[10];
+    memset(id, 0, 6);
 
     /* try to find an ethernet hardware address */
     s = socket(PF_UNIX, SOCK_STREAM, AF_UNSPEC);
     if (s >= 0)
     {
-        list.ifc_len = sizeof buf;
-        list.ifc_req = buf;
+#ifdef HAS_GETIFADDRS
+        getifaddrs(&if1);
+        for (if2 = if1; (!result) && if2; if2 = if2->ifa_next)
+        {
+            if (if2->ifa_addr->sa_family == AF_LINK)
+            {
+                struct sockaddr_dl* dl = (struct sockaddr_dl*)if2->ifa_addr;
+                unsigned char* mac = (unsigned char*)LLADDR(dl);
+                if (dl->sdl_alen == 6) /* 48 bits */
+                {
+                    memcpy(id, mac, 6);
+                    result = 1;
+                }
+            }
+        }
+        freeifaddrs(if1);
+#elif defined(__linux__) || defined(__CYGWIN__) || defined(__osf__)
+        list.ifc_len = sizeof(buf);
+        list.ifc_req = (struct ifreq*)&buf;
 
         if (ioctl(s, SIOCGIFCONF, &list) >= 0)
         {
-            for (i = 0; i < list.ifc_len / sizeof(struct ifreq); i++)
+            for (i = 0; (!result) && (i < list.ifc_len); i += _SIZEOF_ADDR_IFREQ(*req1))
             {
-                strncpy(req.ifr_name, buf[i].ifr_name, IFNAMSIZ);
+                req1 = (struct ifreq*)&buf.b[i];
+                memcpy(req2.ifr_name, req1->ifr_name, IFNAMSIZ);
 #if defined(__linux__) || defined(__CYGWIN__)
-                if (ioctl(s, SIOCGIFHWADDR, &req) < 0)
+                if (ioctl(s, SIOCGIFFLAGS, &req2) < 0)
                     continue;
-                memcpy(id, req.ifr_hwaddr.sa_data, 6);
+                if ((req2.ifr_flags & IFF_LOOPBACK) != 0)
+                    continue;
+                if (ioctl(s, SIOCGIFHWADDR, &req2) < 0)
+                    continue;
+                memcpy(id, req2.ifr_hwaddr.sa_data, 6);
 #elif defined(__osf__)
-                if (ioctl(s, SIOCRPHYSADDR, &req) < 0)
+                if (ioctl(s, SIOCRPHYSADDR, &req2) < 0)
                     continue;
-                memcpy(id, req.default_pa, 6);
+                memcpy(id, req2.default_pa, 6);
+#else
+#error unknown system
 #endif
-                close(s);
-                return 1;
+                result = 1;
             }
         }
+        else
+        {
+            printf("ioctl SIOCGIFCONF failed %d\n", errno);
+        }
+#endif
         close(s);
     }
-#endif
 
-    memset(id, 0, 6);
-
-    /* try using the machine's internet address */
-    if (gethostname(hostname, 128) == 0)
+    /* lame fallback esp. for Solaris */
+    if (result == 0)
     {
-        hostent = gethostbyname(hostname);
-        if (hostent && hostent->h_length == 4)
+        char hostname[256];
+        struct hostent *hostent;
+
+        /* try using the machine's internet address */
+        if (gethostname(hostname, sizeof(hostname)) == 0)
         {
-            id[2] = hostent->h_addr[0];
-            id[3] = hostent->h_addr[1];
-            id[4] = hostent->h_addr[2];
-            id[5] = hostent->h_addr[3];
-            return 1;
+            struct hostent* hostent = gethostbyname(hostname);
+            if (hostent && hostent->h_length == 4)
+            {
+                id[2] = hostent->h_addr[0];
+                id[3] = hostent->h_addr[1];
+                id[4] = hostent->h_addr[2];
+                id[5] = hostent->h_addr[3];
+                result = 1;
+            }
         }
     }
 
-    return 0;
+    return result;
 }
 
 
@@ -89,17 +173,18 @@ MachineIDPosixC__CanGet(char *id)
 } /* extern "C" */
 #endif
 
-#if 0 /* test code */
-
-#include <stdio.h>
+#if 1 /* test code */
 
 int main()
 {
-    unsigned char id[6];
-    int i;
+    unsigned char id[6] = { 0 };
+    int i = { 0 };
     
-    i = MachineIDPosixC__CanGet((char*)id);
-    printf("%d %02x%02x%02x%02x%02x%02x\n", i, id[0], id[1], id[2], id[3], id[4], id[5]);
+    i = MachineIDPosixC__CanGet(id);
+    printf("%d %02X-%02X-%02X-%02X-%02X-%02X\n", i, id[0], id[1], id[2], id[3], id[4], id[5]);
+    printf("   %u.%u.%u.%u.%u.%u\n", id[0], id[1], id[2], id[3], id[4], id[5]);
+    system("/sbin/ifconfig -a | grep ether");
+    system("/sbin/ifconfig -a | grep addr");
 
     return 0;
 }
