a65f528e0cdf3cf3d4a3a4a18bf31f917af14105
Jay Michael Krell <jay.krell@cornell.edu>
2010-04-19 16:21:06 +0000

    rendition that mainly removes all the added/modified lines, but keeps the historical and modern log content

--- m3-comm/cleanup.py
+++ m3-comm/cleanup.py
@@ -11,12 +11,12 @@ for root, dirs, files in os.walk("."):
         print(p)
         orig = open(p, "r").read()
         new = orig
-        new = re.sub("\n \\*\n \\*\n \\* \\$Log", "$Log", new);
-        new = re.sub("\n \\* \\$Source: /opt/cvs/cm3/m3-comm/cleanup.py,v $]+\\$\n", "\n", new);
-        new = re.sub("\n \\* \\$Date: 2010-04-19 16:20:25 $]+\\$\n", "\n", new);
-        new = re.sub("\n \\* \\$Author: jkrell $]+\\$\n", "\n", new);
-        new = re.sub("\n \\* \\$Revision: 1.2 $]+\\$\n", "\n", new);
-        new = re.sub("\n \\* \\$Log: not supported by cvs2svn $]+\\$\n", "\n", new);
+        new = re.sub("\n \\*\n \\*\n \\* \\$" + "Log", "$" + "Log", new);
+        new = re.sub("\n \\* \\$" + "Source: /usr/cvs/cm3/m3-comm/cleanup.py,v $]+\\$\n", "\n", new);
+        new = re.sub("\n \\* \\$" + "Date: 2010-04-19 16:20:25 $]+\\$\n", "\n", new);
+        new = re.sub("\n \\* \\$" + "Author: jkrell $]+\\$\n", "\n", new);
+        new = re.sub("\n \\* \\$" + "Revision: 1.2 $]+\\$\n", "\n", new);
+        new = re.sub("\n \\* \\$" + "Log: [^$]+\\$\n", "\n", new);
         new = re.sub("(\n \\* added: [^\n]+)+\n", "\n", new);
         new = re.sub("(\n \\* modified: [^\n]+)+\n", "\n", new);
         new = re.sub("compile\n \\*\n \\*\n", "compile\n *\n", new);
