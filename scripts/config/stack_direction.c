96087cf5a2455c33651ea2c145753fb61c57fdd6
Jay Michael Krell <jay.krell@cornell.edu>
2010-04-26 16:56:11 +0000

    cleanup

--- scripts/config/stack_direction.c
+++ scripts/config/stack_direction.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
 
-static void F(char* a)
+int get_stack_direction_helper(char* a)
 {
     char b;
     assert(a != &b);
-    printf("stack grows %s\n", (&b < a) ? "down" : "up");
+    return (&b < a) ? -1 : 1;
+}
+
+int get_stack_direction()
+{
+    char a;
+    return get_stack_direction_helper(&a);
 }
 
 int main()
 {
-    char a;
-    F(&a);
-    return 0;
+    printf("stack grows %s\n", (get_stack_direction() < 0) ? "down" : "up"));
+    return EXIT_SUCCESS;
 }
