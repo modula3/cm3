aa89243071380aed7427bf0d3e81425b9f72db09
Jay Michael Krell <jay.krell@cornell.edu>
2010-05-09 09:13:25 +0000

    cleanup:  extern "C" => extern "C" { typo  don't #include inside extern "C"  add calling conventions  move some common stuff to the header

--- caltech-parser/term/src/Termios.c
+++ caltech-parser/term/src/Termios.c
@@ -1,31 +1,15 @@
+#include "TermC.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/* const is extern const in C, but static const in C++,
- * but gcc gives a warning for the correct portable form "extern const" */
-#if defined(__cplusplus) || !defined(__GNUC__)
-#define EXTERN_CONST extern const
-#else
-#define EXTERN_CONST const
-#endif
-
 #ifndef _WIN32
-
-#include <termios.h>
-#include <unistd.h>
-#include "TermC.h"
-
-#ifndef STDIN_FILENO
-#define STDIN_FILENO 0
-#endif
 
 EXTERN_CONST int Termios__Stdin = STDIN_FILENO;
 EXTERN_CONST int Termios__Tcsanow = TCSANOW;
 
-typedef struct termios termios_t;
-
-void Termios__tcsetattr(int fd, int action, termios_t* t)
+void __cdecl Termios__tcsetattr(int fd, int action, struct termios* t)
 {
     if (TermC__Inited() && (t == TermC__GetTermRaw() || t == TermC__GetTermCooked()) && action == TCSANOW && fd == STDIN_FILENO)
     {
@@ -38,7 +22,7 @@ void Termios__tcsetattr(int fd, int action, termios_t* t)
 EXTERN_CONST int Termios__Stdin = 0;
 EXTERN_CONST int Termios__Tcsanow = 0;
 
-void Termios__tcsetattr(int fd, int action, void* t)
+void __cdecl Termios__tcsetattr(int fd, int action, void* t)
 {
 }
 
