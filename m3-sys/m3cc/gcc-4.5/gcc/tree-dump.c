bab4c591851fe8e417a4c3c23b4dae5da100dde5
Jay Michael Krell <jay.krell@cornell.edu>
2010-05-30 14:42:07 +0000

    merge in more of the static chain expr stuff   easy stuff like:    pretty print, dump    naming the variables to agree with m3gdb    being sure to keep the debug info (the 'false' => 'true' change)    comments
    There is still a bit more, that I have somewhat of a handle on.
    In particular Tony's changes from years ago.

--- m3-sys/m3cc/gcc-4.5/gcc/tree-dump.c
+++ m3-sys/m3cc/gcc-4.5/gcc/tree-dump.c
@@ -639,6 +639,10 @@ dequeue_and_dump (dump_info_p di)
       }
       break;
 
+    case STATIC_CHAIN_EXPR:
+      dump_child ("fn", TREE_OPERAND (t, 0));
+      break;
+
     case CONSTRUCTOR:
       {
 	unsigned HOST_WIDE_INT cnt;
