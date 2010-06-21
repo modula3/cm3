b6095fc18ff3367c29d109fbab4066cdd497104e
Jay Michael Krell <jay.krell@cornell.edu>
2010-06-21 08:31:43 +0000

    remove C9x dependency

--- m3-sys/m3tests/src/p2/p238/man-or-boy.c
+++ m3-sys/m3tests/src/p2/p238/man-or-boy.c
@@ -1,40 +1,68 @@
 /* man-or-boy.c */
 #include <stdio.h>
 #include <stdlib.h>
- 
-// --- thunks
-typedef struct arg {
-  int       (*fn)(struct arg*);
-  int        *k;
-  struct arg *x1, *x2, *x3, *x4, *x5;
-} ARG;
- 
-// --- lambdas
-int f_1 (ARG* _) { return -1; }
-int f0  (ARG* _) { return  0; }
-int f1  (ARG* _) { return  1; }
- 
-// --- helper
-int eval(ARG* a) { return a->fn(a); }
-#define MAKE_ARG(...) (&(ARG){ __VA_ARGS__ })
-#define FUN(...) MAKE_ARG(B,&k,__VA_ARGS__)
- 
-// --- functions
-int B(ARG* a) {
-  int A(ARG*);
-  int k = *a->k -= 1;
-  return A( FUN(a,a->x1,a->x2,a->x3,a->x4) );
+
+typedef struct frame
+{
+  int (*fn)(struct frame*);
+  int* k;
+  struct frame* x1;
+  struct frame* x2;
+  struct frame* x3;
+  struct frame* x4;
+  struct frame* x5;
+} FRAME;
+
+int Fn1 (FRAME* a) { return -1; }
+int F0  (FRAME* a) { return  0; }
+int F1  (FRAME* a) { return  1; }
+
+int eval(FRAME* a) { return a->fn(a); }
+
+int A(FRAME*);
+
+int B(FRAME* a)
+{
+  int k = (*a->k -= 1);
+  FRAME b = { 0 };
+  b.fn = B;
+  b.k = &k;
+  b.x1 = a;
+  b.x2 = a->x1;
+  b.x3 = a->x2;
+  b.x4 = a->x3;
+  b.x5 = a->x4;
+  return A(&b);
 }
- 
-int A(ARG* a) {
-  return *a->k <= 0 ? eval(a->x4)+eval(a->x5) : B(a);
+
+int A(FRAME* a)
+{
+  return *a->k <= 0 ? eval(a->x4) + eval(a->x5) : B(a);
 }
- 
-int main(int argc, char **argv) {
+
+int main(int argc, char** argv)
+{
   int k = argc == 2 ? strtol(argv[1],0,0) : 10;
-  printf("%d\n", A(FUN(MAKE_ARG(f1),
-                       MAKE_ARG(f_1),
-                       MAKE_ARG(f_1),
-                       MAKE_ARG(f1),
-                       MAKE_ARG(f0))));
+  FRAME fn1 = { 0 };
+  FRAME f1 = { 0 };
+  FRAME f0 = { 0 };
+  FRAME a = { 0 };
+
+  fn1.k = &k;
+  fn1.fn = Fn1;
+  f1.k = &k;
+  f1.fn = F1;
+  f0.k = &k;
+  f0.fn = F0;
+
+  a.k = &k;
+  a.fn = B;
+  a.x1 = &f1;
+  a.x2 = &fn1;
+  a.x3 = &fn1;
+  a.x4 = &f1;
+  a.x5 = &f0;
+
+  printf("%d\n", A(&a));
+  return 0;
 }
