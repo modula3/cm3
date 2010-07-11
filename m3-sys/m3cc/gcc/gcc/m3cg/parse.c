fd01db8f4c2a036852e2a6d9e8d9ff8384f115a8
Jay Michael Krell <jay.krell@cornell.edu>
2010-07-11 22:50:34 +0000

    gcc 4.5: mark barrier labels rtx with LABEL_REF_NONLOCAL_P, does it help/matter?

--- m3-sys/m3cc/gcc/gcc/m3cg/parse.c
+++ m3-sys/m3cc/gcc/gcc/m3cg/parse.c
@@ -3757,6 +3757,7 @@ m3cg_set_label (void)
           The instruction that altered rbp was, uh, surprising.
       DECL_NONLOCAL(l) = true;
       */
+      LABEL_REF_NONLOCAL_P (r) = 1;
 #else
       {
         rtx list = DECL_STRUCT_FUNCTION (current_function_decl)->x_nonlocal_goto_handler_labels;
