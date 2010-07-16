091e2ea00dc654f9e137f6dff4378686fc528346
Jay Michael Krell <jay.krell@cornell.edu>
2010-07-16 12:01:27 +0000

    move action to start, as the sh scripts require?

--- scripts/boot2.sh
+++ scripts/boot2.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 OMIT_GCC=yes ./do-cm3-all.sh realclean
-./do-pkg.sh m3cc m3core libm3 buildship
+./do-pkg.sh buildship m3cc m3core libm3
 OMIT_GCC=yes ./upgrade.sh
 OMIT_GCC=yes ./do-cm3-all.sh realclean
 ./do-cm3-all.sh buildship
