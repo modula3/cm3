d6f395777607e8e0b091e25ad27c463f6fa00f31
Jay Michael Krell <jay.krell@cornell.edu>
2010-07-16 12:11:16 +0000

    more sh for Tru64 4.0 work, while I wait for Python to compile Note this is different than install-config.py. It is based on make-bin-dist.sh. It makes config for ship, not development. ie. actual copies of the files, not the stub that tries to get back to the source tree.

---
+++ scripts/install-config.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+if [ -n "$ROOT" -a -d "$ROOT" ] ; then
+  sysinfo="$ROOT/scripts/sysinfo.sh"
+  root="${ROOT}"; export root
+else
+  root=`pwd`
+  while [ -n "$root" -a ! -f "$root/scripts/sysinfo.sh" ] ; do
+    root=`dirname $root`
+  done
+  sysinfo="$root/scripts/sysinfo.sh"
+  if [ ! -f "$sysinfo" ] ; then
+    echo "scripts/sysinfo.sh not found" 1>&2
+    exit 1
+  fi
+  export root
+fi
+
+. "$sysinfo"
+. "$ROOT/scripts/pkginfo.sh"
+. "$ROOT/scripts/pkgcmds.sh"
+
+set -e
+set -x
+
+for f in ${ROOT}/m3-sys/cminstall/src/config-no-install/*; do
+  rm -f "${INSTALLROOT}/bin/`basename ${f}`" > /dev/null
+done
+mkdir -p "${INSTALLROOT}/bin/config"
+for f in ${ROOT}/m3-sys/cminstall/src/config-no-install/*; do
+  [ -f ${f} ] && cp "${f}" "${INSTALLROOT}/bin/config"
+done
+(
+  echo "INSTALL_ROOT = path() & \"/..\""
+  echo "include(path() & \"/config/${TARGET}\")"
+) > "${INSTALLROOT}/bin/cm3.cfg"
