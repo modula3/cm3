05e582b4278022c3184ce637c2e9511b29b79851
Jay Michael Krell <jay.krell@cornell.edu>
2010-08-18 06:28:45 +0000

    more data

--- m3-libs/m3core/src/time/POSIX/test_time.c
+++ m3-libs/m3core/src/time/POSIX/test_time.c
@@ -3,7 +3,18 @@
 int main()
 {
     LONGREAL sample = ComputeGrainViaSampling();
-    LONGREAL res = ComputeGrainViaClockGetRes();
-    printf("%e %e\n", ComputeGrainViaSampling(), ComputeGrainViaClockGetRes());
+    struct timespec real = { 0 };
+    struct timespec high = { 0 };
+#ifdef CLOCK_HIGHRES
+    i = clock_getres(CLOCK_HIGHRES, &high);
+    assert(i == 0);
+#endif
+#ifdef CLOCK_REALTIME
+    i = clock_getres(CLOCK_REALTIME, &real);
+    assert(i == 0);
+#endif
+    printf("real: %lu.%lu %e\n", real.tv_sec, real.tv_nsec, TimePosix__FromNanotime(&real));
+    printf("high: %lu.%lu %e\n", high.tv_sec, high.tv_nsec, TimePosix__FromNanotime(&high));
+    printf("sample: %e\n", sample);
     return 0;
 }
