caa42fb92c6d46fd50b3907ebfc0e08fae43ea2a
jkrell <>
2010-08-23 00:35:53 +0000

    fix eliminate of iconv/locale

--- m3-sys/m3cc/gcc/libcpp/init.c
+++ m3-sys/m3cc/gcc/libcpp/init.c
@@ -264,7 +264,6 @@ cpp_destroy (cpp_reader *pfile)
 
   _cpp_destroy_hashtable (pfile);
   _cpp_cleanup_files (pfile);
-  _cpp_destroy_iconv (pfile);
 
   _cpp_free_buff (pfile->a_buff);
   _cpp_free_buff (pfile->u_buff);
