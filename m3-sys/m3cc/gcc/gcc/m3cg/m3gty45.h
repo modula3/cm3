a2eb1882be24d910b15487dd379dbfa09183c008
jkrell <>
2010-09-12 06:24:45 +0000

    work in progress:
    some tree repair per configure -enable-checking
      RETURN_EXPR in particular and some types
      [disabled pending more testing]
    
    restore bitfield use
      we crash without it
      even though it triggers -enable-checking errors
      to be investigated..
    
    tracing:
      add some newlines for multi-line things like records/proctypes/procedures
      probably should indent instead
    
    tweak garbage collector interaction
      and provide 3 ways to implement the type table:
        fixed size, growable, VEC
    
    These changes fixing some of the breakage from inlining,
    but not enough to enable it. We no longer get backend
    assertion failures, and inlining seemed to work in m3core+libm3+sysutils,
    but a full upgrade still produced a crashing cm3.
      probably the problem was either RETURN_EXPR or the garbage collection
    
    some debugging aids: m3_break_lineno, m3_breakpoint
    
    restore old type-less m3gdb stuff now that I changed
    the proper typed tree stuff to use different code

--- m3-sys/m3cc/gcc/gcc/m3cg/m3gty45.h
+++ m3-sys/m3cc/gcc/gcc/m3cg/m3gty45.h
@@ -63,8 +63,8 @@ struct GTY(()) language_function
     bool volatil; /* does function call setjmp/fork/vfork */
 };
 
-typedef struct GTY(()) m3type_t
+typedef struct GTY(()) m3type
 {
   unsigned long id;
-  tree GTY (()) t;
+  tree GTY(()) t;
 } m3type_t;
