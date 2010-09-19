86af5bc8105d65f03ad54d5c8f41c46117402014
jkrell <>
2010-09-19 14:50:29 +0000

    Provide RTDebug.IsDebuggerPresent and RTDebug.DebugBreak.
    Win32:
    RTDebug.IsDebuggerPresent => IsDebuggerPresent
    RTDebug.DebugBreak => DebugBreak
    
    Apple:
    RTDebug.IsDebuggerPresent => experimentally derived on my Intel 10.5 system.
      (could also test ppc emulation)
      sigaction(sigtrap, 0, &sa);
        peek at sa
    
    RTDebug.DebugBreak => works
      sigaction(sigtrap, 0, &old);
      signal(sigtrap, sigign);
      raise(sigtrap);
      sigaction(sigtrap, &old, 0);
    
    others:
    
    RTDebug.IsDebuggerPresent => false
      experimented on Linux and found Apple approach not viable
    RTDebug.DebugBreak => works, same as Apple
    
    smaller alternative would be definitely appreciated
    note that DebugBreak must be trivially continuable in debugger
      abort() is not it!
    
    bonus points if it is macro or builtin and small for C so the break occurs
      right in the code; on Win32 there is also __debugbreak() which
      generates one instruction right there, very nice
      (though notice that Win32 DebugBreak is "fatal" not in a debuger,
      whereas my Posix version isn't, because I can't implement
      IsDebuggerPresent)
    
    bonus points also if IsDebuggerPresent is fast, and
      updates itself as debugger is detached/reattached,
      though current version doesn't; Win32 does

---
+++ m3-libs/m3core/src/runtime/common/RTDebugC.c
@@ -0,0 +1,72 @@
+#ifdef _WIN32
+
+#include <windows.h>
+
+int __cdecl RTDebug__IsDebuggerPresent(void)
+{
+  return IsDebuggerPresent();
+}
+
+void  __cdecl RTDebug__DebugBreak(void)
+{
+  DebugBreak();
+}
+
+#else
+
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+
+static volatile char inited;
+static char is_debugger_present;
+
+static void init(void)
+/* todo: test detach/reattach, and maybe make them work */
+{
+  struct sigaction sa;
+
+  if (inited)
+    return;
+    
+  sigaction(SIGTRAP, 0, &sa);
+#ifdef __APPLE__
+  /* experimentally derived */
+  if ((sa.sa_mask == 0) != (sa.sa_flags == SA_RESTART))
+    printf("warning 1 in RTDebug.init\n");
+  is_debugger_present = (sa.sa_mask == 0) && (sa.sa_flags == SA_RESTART);
+#else
+  /* todo? */
+#endif
+  inited = 1;
+}
+
+int RTDebug__IsDebuggerPresent(void)
+{
+  init();
+  return is_debugger_present;
+}
+
+void RTDebug__DebugBreak(void)
+/* todo: this is so big/slow compared to Win32! */
+{
+  struct sigaction sa;
+
+  sigaction(SIGTRAP, 0, &sa);
+  signal(SIGTRAP, SIG_IGN);
+  sigaction(SIGTRAP, &sa, 0);
+}
+
+#endif
+
+#if 0 /* test code */
+
+int main()
+{
+  printf("IsDebuggerPresent %d\n", RTDebug__IsDebuggerPresent());
+  RTDebug__DebugBreak();
+  printf("after debugbreak (make sure it doesn't exit)\n");
+  return 0;
+}
+
+#endif
