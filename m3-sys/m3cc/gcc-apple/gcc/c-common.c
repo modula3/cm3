51a9e5b5e771393f2bfb33b62ea7fa52847d68b3
Jay Michael Krell <jay.krell@cornell.edu>
2010-10-02 11:20:06 +0000

    workaround ffs problem on VMS see http://gcc.gnu.org/bugzilla/show_bug.cgi?id=44037

--- m3-sys/m3cc/gcc-apple/gcc/c-common.c
+++ m3-sys/m3cc/gcc-apple/gcc/c-common.c
@@ -1,3 +1,6 @@
+/* some changes in Modula-3 fork
+   e.g. fix ffs for VMS */
+
 /* Subroutines shared by all languages that are variants of C.
    Copyright (C) 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000,
    2001, 2002, 2003, 2004, 2005 Free Software Foundation, Inc.
@@ -48,6 +51,9 @@ Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA
 #include "opts.h"
 #include "real.h"
 #include "cgraph.h"
+#ifndef TARGET_ABI_OPEN_VMS
+#define TARGET_ABI_OPEN_VMS 0
+#endif
 
 cpp_reader *parse_in;		/* Declared in c-pragma.h.  */
 
@@ -3730,6 +3736,7 @@ def_builtin_1 (enum built_in_function fncode,
 {
   tree decl;
   const char *libname;
+  const char *libname2;
 
   if (fntype == error_mark_node)
     return;
@@ -3739,11 +3746,16 @@ def_builtin_1 (enum built_in_function fncode,
 			   strlen ("__builtin_")));
 
   libname = name + strlen ("__builtin_");
+  libname2 = libname;
+  if (TARGET_ABI_OPEN_VMS && strcmp(libname, "ffs") == 0)
+    libname = "decc$ffs";
+
   decl = lang_hooks.builtin_function (name, fntype, fncode, fnclass,
 				      (fallback_p ? libname : NULL),
 				      fnattrs);
   if (both_p
       && !flag_no_builtin && !builtin_function_disabled_p (libname)
+      && !builtin_function_disabled_p (libname2)
       && !(nonansi_p && flag_no_nonansi_builtin))
     lang_hooks.builtin_function (libname, libtype, fncode, fnclass,
 				 NULL, fnattrs);
