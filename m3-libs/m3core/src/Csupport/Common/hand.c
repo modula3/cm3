d739db9ae8fa00431cdf15d6cd2c82fa17f06b5b
Jay Michael Krell <jay.krell@cornell.edu>
2010-10-16 10:31:19 +0000

    remove comments that say set_member and set_singleton are never used, as I'm inclined at least temporary to go back to function calls

--- m3-libs/m3core/src/Csupport/Common/hand.c
+++ m3-libs/m3core/src/Csupport/Common/hand.c
@@ -127,7 +127,6 @@ m3_mod64(INT64 b, INT64 a)
 WORD_T
 __stdcall
 set_member(WORD_T elt, WORD_T* set)
-/* never used by current backend */
 {
   register WORD_T word = elt / SET_GRAIN;
   register WORD_T bit  = elt % SET_GRAIN;
@@ -260,7 +259,6 @@ set_range(WORD_T b, WORD_T a, WORD_T* s)
 void
 __stdcall
 set_singleton(WORD_T a, WORD_T* s)
-/* never used by current backend */
 {
   WORD_T a_word = a / SET_GRAIN;
   WORD_T a_bit  = a % SET_GRAIN;
