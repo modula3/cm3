6e40400405441b03ef1a88a7fb25aa2edff2d7c7
Jay Michael Krell <jay.krell@cornell.edu>
2010-10-16 10:19:52 +0000

    remove K&R

--- m3-libs/m3core/src/runtime/common/RTUntracedMemoryC.c
+++ m3-libs/m3core/src/runtime/common/RTUntracedMemoryC.c
@@ -23,29 +23,18 @@ reducing C runtime dependency.
 #define POSIX(x) x
 #endif
 
-#if defined(__STDC__) || defined(__cplusplus) || defined(_MSC_VER)
-#define PROTO1(t1, a1) (t1 a1)
-#define PROTO2(t1, a1, t2, a2) (t1 a1, t2 a2)
-#define VOID_VALUE /* nothing */
-#else
-#define void char
-#define PROTO1(t1, a1) (a1) t1 a1;
-#define PROTO2(t1, a1, t2, a2) (a1, a2) t1 a1; t2 a2;
-#define VOID_VALUE 0
-#endif
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void* __cdecl RTUntracedMemory__AllocZ PROTO1(WORD_T, count)
+void* __cdecl RTUntracedMemory__AllocZ(WORD_T count)
 /* Z = zeroed = calloc */
 {
     return WIN(HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, count))
            POSIX(calloc(count, 1));
 }
 
-void* __cdecl RTUntracedMemory__AllocZV PROTO2(WORD_T, count, WORD_T, size)
+void* __cdecl RTUntracedMemory__AllocZV(WORD_T count, WORD_T size)
 /* ZV = zeroed vector = calloc */
 {
     WORD_T max = ~(WORD_T)0;
@@ -55,14 +44,13 @@ void* __cdecl RTUntracedMemory__AllocZV PROTO2(WORD_T, count, WORD_T, size)
            POSIX(calloc(count, size));
 }
 
-void __cdecl RTUntracedMemory__Free PROTO1(void**, p)
+void __cdecl RTUntracedMemory__Free(void** p)
 {
     void* q = *p;
     *p = 0;
     if (q)
         WIN(HeapFree(GetProcessHeap(), 0, q))
         POSIX(free(q));
-    return VOID_VALUE;
 }
 
 #ifdef __cplusplus
