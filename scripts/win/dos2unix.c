6d6680af1a87ce578c0d02d83fc9ea4f66e429d5
Jay Michael Krell <jay.krell@cornell.edu>
2010-10-19 10:28:17 +0000

    fix comment

--- scripts/win/dos2unix.c
+++ scripts/win/dos2unix.c
@@ -1,12 +1,6 @@
 /*
-This is a wrapper for \cygwin\bin\cvs.exe that doesn't require changing %PATH%.
+This is a wrapper for \cygwin\bin\dos2unix.exe that doesn't require changing %PATH%.
 Put it in some directory already in %PATH%, such as %WinDir% or %WinDir%\system32.
-As well, on the Elego Windows VM, \Program Files\CVSNT has been renamed to CVSNT.1
-to get it out of %PATH%. It ruins line endings.
-As well \SFU was mostly renamed to \SFU.1, to remove it as a possible factor, though
-it turns out it was probably completely innocent.
-As well, CVS_RSH changed to /bin/ssh. Plain "ssh" as all other Hudson jobs use would
-probably work if in this wrapper we prepended c:\cygwin\bin to %PATH%.
 
 Build it with:
     cl -Ox dos2unix.c -link -entry:Entry -subsystem:console kernel32.lib -merge:.rdata=.text -opt:ref
