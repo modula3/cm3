38c821571f700a213e96bc708d0fc4074bbe6474
Jay Michael Krell <jay.krell@cornell.edu>
2010-11-06 11:55:01 +0000

    try a little harder, but not much more on this..

--- m3-sys/m3cc/gcc/include/ansidecl.h
+++ m3-sys/m3cc/gcc/include/ansidecl.h
@@ -265,17 +265,23 @@ So instead we use the macro below and test it against specific values.  */
 # endif /* !__cplusplus && GNUC >= 2.93 */
 #endif /* ATTRIBUTE_UNUSED_LABEL */
 
-#ifndef ATTRIBUTE_UNUSED
-#define ATTRIBUTE_UNUSED __attribute__ ((__unused__))
-#endif /* ATTRIBUTE_UNUSED */
-
 /* Before GCC 3.4, the C++ frontend couldn't parse attributes placed after the
-   identifier name.  */
+   identifier name. ATTRIBUTE_UNUSED is frequently used instead of
+   ARG_UNUSED. */
+
+#ifndef ATTRIBUTE_UNUSED
 #if ! defined(__cplusplus) || (GCC_VERSION >= 3004)
+#define ATTRIBUTE_UNUSED __attribute__ ((__unused__))
+#else
+#define ATTRIBUTE_UNUSED /* nothing */
+#endif
+#endif
+
+#ifdef __cplusplus
+# define ARG_UNUSED(NAME) /* nothing */
+#else
 # define ARG_UNUSED(NAME) NAME ATTRIBUTE_UNUSED
-#else /* !__cplusplus || GNUC >= 3.4 */
-# define ARG_UNUSED(NAME) NAME
-#endif /* !__cplusplus || GNUC >= 3.4 */
+#endif
 
 #ifndef ATTRIBUTE_NORETURN
 #define ATTRIBUTE_NORETURN __attribute__ ((__noreturn__))
