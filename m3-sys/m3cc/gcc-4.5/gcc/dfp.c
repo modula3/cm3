620b9d82a0ea606e4d6791b728bd7f7901f7077f
jkrell <>
2010-11-06 17:18:26 +0000

    "../../gcc-4.5/gcc/dfp.c", line 76: Error: "decimal_do_compare(const real_value*, const real_value*, int)" is expected to return a value. "../../gcc-4.5/gcc/dfp.c", line 100: Error: "decimal_real_to_integer(const real_value*)" is expected to return a value. "../../gcc-4.5/gcc/dfp.c", line 111: Error: "decimal_real_arithmetic(real_value*, tree_code, const real_value*, const real_value
    and quash warnings

--- m3-sys/m3cc/gcc-4.5/gcc/dfp.c
+++ m3-sys/m3cc/gcc-4.5/gcc/dfp.c
@@ -37,79 +37,82 @@ along with GCC; see the file COPYING3.  If not see
 #include "decNumber.h"
 
 void
-decimal_real_from_string (REAL_VALUE_TYPE *r, const char *s)
+ decimal_real_from_string (REAL_VALUE_TYPE *r ATTRIBUTE_UNUSED, const char *s ATTRIBUTE_UNUSED)
 { gcc_unreachable (); }
 
 void
 encode_decimal32 (const struct real_format *fmt ATTRIBUTE_UNUSED,
-		  long *buf, const REAL_VALUE_TYPE *r)
+		  long *buf ATTRIBUTE_UNUSED, const REAL_VALUE_TYPE *r ATTRIBUTE_UNUSED)
 { gcc_unreachable (); }
 
 void
 decode_decimal32 (const struct real_format *fmt ATTRIBUTE_UNUSED,
-		  REAL_VALUE_TYPE *r, const long *buf)
+		  REAL_VALUE_TYPE *r ATTRIBUTE_UNUSED, const long *buf ATTRIBUTE_UNUSED)
 { gcc_unreachable (); }
 
 void
 encode_decimal64 (const struct real_format *fmt ATTRIBUTE_UNUSED,
-		  long *buf, const REAL_VALUE_TYPE *r)
+		  long *buf ATTRIBUTE_UNUSED, const REAL_VALUE_TYPE *r ATTRIBUTE_UNUSED)
 { gcc_unreachable (); }
 
 void
 decode_decimal64 (const struct real_format *fmt ATTRIBUTE_UNUSED,
-		  REAL_VALUE_TYPE *r, const long *buf)
+		  REAL_VALUE_TYPE *r ATTRIBUTE_UNUSED, const long *buf ATTRIBUTE_UNUSED)
 { gcc_unreachable (); }
 
 void
 encode_decimal128 (const struct real_format *fmt ATTRIBUTE_UNUSED,
-		   long *buf, const REAL_VALUE_TYPE *r)
+		   long *buf ATTRIBUTE_UNUSED, const REAL_VALUE_TYPE *r ATTRIBUTE_UNUSED)
 { gcc_unreachable (); }
 
 void
 decode_decimal128 (const struct real_format *fmt ATTRIBUTE_UNUSED,
-		   REAL_VALUE_TYPE *r, const long *buf)
+ 		   REAL_VALUE_TYPE *r ATTRIBUTE_UNUSED, const long *buf ATTRIBUTE_UNUSED)
 { gcc_unreachable (); }
 
 int
-decimal_do_compare (const REAL_VALUE_TYPE *a, const REAL_VALUE_TYPE *b,
-		    int nan_result)
+decimal_do_compare (const REAL_VALUE_TYPE *a ATTRIBUTE_UNUSED, const REAL_VALUE_TYPE *b ATTRIBUTE_UNUSED,
+		    int nan_result ATTRIBUTE_UNUSED)
+{ gcc_unreachable ();
+  return 0; }
+
+void
+decimal_round_for_format (const struct real_format *fmt ATTRIBUTE_UNUSED, REAL_VALUE_TYPE *r ATTRIBUTE_UNUSED)
 { gcc_unreachable (); }
 
 void
-decimal_round_for_format (const struct real_format *fmt, REAL_VALUE_TYPE *r)
+decimal_real_convert (REAL_VALUE_TYPE *r ATTRIBUTE_UNUSED, enum machine_mode mode ATTRIBUTE_UNUSED,
+		      const REAL_VALUE_TYPE *a ATTRIBUTE_UNUSED)
 { gcc_unreachable (); }
 
 void
-decimal_real_convert (REAL_VALUE_TYPE *r, enum machine_mode mode,
-		      const REAL_VALUE_TYPE *a)
-{ gcc_unreachable (); }
-
-void
-decimal_real_to_decimal (char *str, const REAL_VALUE_TYPE *r_orig,
-			 size_t buf_size,
+decimal_real_to_decimal (char *str ATTRIBUTE_UNUSED, const REAL_VALUE_TYPE *r_orig ATTRIBUTE_UNUSED,
+			 size_t buf_size ATTRIBUTE_UNUSED,
 			 size_t digits ATTRIBUTE_UNUSED,
 			 int crop_trailing_zeros ATTRIBUTE_UNUSED)
 { gcc_unreachable (); }
 
 void
-decimal_do_fix_trunc (REAL_VALUE_TYPE *r, const REAL_VALUE_TYPE *a)
+decimal_do_fix_trunc (REAL_VALUE_TYPE *r ATTRIBUTE_UNUSED, const REAL_VALUE_TYPE *a ATTRIBUTE_UNUSED)
 { gcc_unreachable (); }
 
 HOST_WIDE_INT
-decimal_real_to_integer (const REAL_VALUE_TYPE *r)
-{ gcc_unreachable (); }
+decimal_real_to_integer (const REAL_VALUE_TYPE *r ATTRIBUTE_UNUSED)
+{ gcc_unreachable ();
+  return 0; }
 
 void
-decimal_real_to_integer2 (HOST_WIDE_INT *plow, HOST_WIDE_INT *phigh,
-			  const REAL_VALUE_TYPE *r)
+decimal_real_to_integer2 (HOST_WIDE_INT *plow ATTRIBUTE_UNUSED, HOST_WIDE_INT *phigh ATTRIBUTE_UNUSED,
+			  const REAL_VALUE_TYPE *r ATTRIBUTE_UNUSED)
 { gcc_unreachable (); }
 
 bool
-decimal_real_arithmetic (REAL_VALUE_TYPE *r, enum tree_code code,
-			 const REAL_VALUE_TYPE *op0,
-			 const REAL_VALUE_TYPE *op1)
-{ gcc_unreachable (); }
+decimal_real_arithmetic (REAL_VALUE_TYPE *r ATTRIBUTE_UNUSED, enum tree_code code ATTRIBUTE_UNUSED,
+			 const REAL_VALUE_TYPE *op0 ATTRIBUTE_UNUSED,
+			 const REAL_VALUE_TYPE *op1 ATTRIBUTE_UNUSED)
+{ gcc_unreachable ();
+  return 0; }
 
 void
-decimal_real_maxval (REAL_VALUE_TYPE *r, int sign, enum machine_mode mode)
+decimal_real_maxval (REAL_VALUE_TYPE *r ATTRIBUTE_UNUSED, int sign ATTRIBUTE_UNUSED, enum machine_mode mode ATTRIBUTE_UNUSED)
 { gcc_unreachable (); }
