fe371b35a6ad83a5da607907d656e13de8469cbc
Jay Michael Krell <jay.krell@cornell.edu>
2010-11-07 01:38:13 +0000

    huh, seems to work: enum bitfields; can then make it back to one parameter and unchange gengtype, later

--- m3-sys/m3cc/gcc-4.5/libcpp/system.h
+++ m3-sys/m3cc/gcc-4.5/libcpp/system.h
@@ -348,10 +348,10 @@ extern void abort (void);
 /* Be conservative and only use enum bitfields with GCC.
    FIXME: provide a complete autoconf test for buggy enum bitfields.  */
 
-#if (GCC_VERSION > 2000)
+#ifdef __cplusplus
+#define ENUM_BITFIELD(TYPE, NAME, SIZE) enum TYPE NAME : SIZE
+#elif (GCC_VERSION > 2000)
 #define ENUM_BITFIELD(TYPE, NAME, SIZE) __extension__ enum TYPE NAME : SIZE
-#elif defined(__cplusplus)
-#define ENUM_BITFIELD(TYPE, NAME, SIZE) enum TYPE NAME
 #else
 #define ENUM_BITFIELD(TYPE, NAME, SIZE) unsigned int NAME
 #endif
