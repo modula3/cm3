5bacbf95c34d653da54ba46167d8a685c8cd8659
Jay Michael Krell <jay.krell@cornell.edu>
2010-11-10 13:12:39 +0000

    don't inline nested functions inside volatized functions, to fix: elego/m3msh/src/M3MiniShell.m3: In function 'M3MiniShell__ProcessParameters': elego/m3msh/src/M3MiniShell.m3:608:0: error: variable '_nonlocal_var_rec.188' might be clobbered by 'longjmp' or 'vfork'

--- m3-sys/m3cc/gcc/gcc/m3cg/parse.c
+++ m3-sys/m3cc/gcc/gcc/m3cg/parse.c
@@ -4402,6 +4402,8 @@ M3CG_HANDLER (BEGIN_PROCEDURE)
 
 M3CG_HANDLER (END_PROCEDURE)
 {
+  bool volatize = get_volatize ();
+
   gcc_assert (current_function_decl == p);
 
   /* Attach block to the function */
@@ -4429,6 +4431,16 @@ M3CG_HANDLER (END_PROCEDURE)
   else
     /* We are not inside of any scope now. */
     {
+/* don't inline nested functions inside volatized functions, to fix:
+elego/m3msh/src/M3MiniShell.m3: In function 'M3MiniShell__ProcessParameters':
+elego/m3msh/src/M3MiniShell.m3:608:0: error: variable '_nonlocal_var_rec.188' might be clobbered by 'longjmp' or 'vfork'
+*/
+      if (volatize)
+      {
+        struct cgraph_node* node = cgraph_node (p);
+        for (node = node->nested; node; node = node->next_nested)
+          DECL_UNINLINABLE (node->decl) = true;
+      }
       m3_gimplify_function (p);
       cgraph_finalize_function (p, false);
     }
