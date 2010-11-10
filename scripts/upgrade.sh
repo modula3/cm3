21735481d1d4368c7e85219f869b173e92d8df9c
Jay Michael Krell <jay.krell@cornell.edu>
2010-11-10 11:14:49 +0000

    reexpand boilerplate, so scripts can run with cd=root

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -5,8 +5,18 @@ set -x
 
 if [ -n "$ROOT" -a -d "$ROOT" ] ; then
   sysinfo="$ROOT/scripts/sysinfo.sh"
+  root="${ROOT}"; export root
 else
-  sysinfo="`pwd`/sysinfo.sh"
+  root=`pwd`
+  while [ -n "$root" -a ! -f "$root/scripts/sysinfo.sh" ] ; do
+    root=`dirname $root`
+  done
+  sysinfo="$root/scripts/sysinfo.sh"
+  if [ ! -f "$sysinfo" ] ; then
+    echo "scripts/sysinfo.sh not found" 1>&2
+    exit 1
+  fi
+  export root
 fi
 
 . "$sysinfo"
