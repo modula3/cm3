aef9d890bd40e0d78424512bfac0ea0f9f70e9a5
jkrell <>
2010-11-11 10:22:35 +0000

    make "all" and "std" both be simply every line in pkginfo.txt don't have to put "std" tag on every line to make it so

--- scripts/win/do-cm3-std.cmd
+++ scripts/win/do-cm3-std.cmd
@@ -9,7 +9,7 @@ call %~dp0pkgcmds || exit /b 1
 call %~dp0pkgcmds map_action %* || exit /b 1
 
 set P=
-for /f "tokens=1" %%a in ('findstr /c:" std" %~dp0..\pkginfo.txt') do set P=!P! %%a
+for /f "tokens=1" %%a in (%~dp0..\pkginfo.txt) do set P=!P! %%a
 
 call %~dp0do-pkg %* %P% || exit /b 1
 
