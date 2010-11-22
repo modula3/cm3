636e7094ed5f976574397327eefaef397e00ca25
Jay Michael Krell <jay.krell@cornell.edu>
2010-11-22 07:45:37 +0000

    see http://hudson.modula3.com:8080/job/cm3-current-build-PPC_DARWIN/177/consoleFull
    /Users/hudson/workspace/cm3-current-build-PPC_DARWIN/cm3/scripts/regression/hudson_build_system.sh: line 40: [: missing `]'

--- scripts/regression/hudson_pkg_tests.sh
+++ scripts/regression/hudson_pkg_tests.sh
@@ -36,7 +36,7 @@ if [ -z "${CM3CG}" ]; then
     ;;
   esac
 fi
-if [ "$CLEAN" = "false" ]; then
+if [ "x$CLEAN" = "xfalse" ]; then
   if [ -x "${CM3CG}" ]; then
     echo "checking for working pre-built cm3cg in ${CM3CG}"
     cp -p "${CM3CG}" ${WS}/cm3cg
