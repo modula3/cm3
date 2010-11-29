fcfca9ef1c2ecea6879d47ead5fdef20ad3d5e4a
Jay Michael Krell <jay.krell@cornell.edu>
2010-11-29 07:47:54 +0000

    fix wrt DebugBreak

--- m3-libs/m3core/src/runtime/common/RTDebugC.c
+++ m3-libs/m3core/src/runtime/common/RTDebugC.c
@@ -1,6 +1,7 @@
 #ifdef _WIN32
 
 __declspec(dllimport) int __stdcall IsDebuggerPresent(void);
+__declspec(dllimport) void __stdcall DebugBreak(void);
 
 int __cdecl RTDebug__IsDebuggerPresent(void)
 {
