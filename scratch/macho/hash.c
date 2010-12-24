fe848b7c4c6c7696559420438180e45194fbb2b8
Jay Michael Krell <jay.krell@cornell.edu>
2010-12-24 04:28:46 +0000

    scratch/macho (the most work needed to port the x86 backend is actually supporting other object files)

---
+++ scratch/macho/hash.c
@@ -0,0 +1,235 @@
+#include "hash.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct _hashtable_t
+{
+    hashtable_entry_t** buckets;
+    size_t bucket_count;
+    size_t entry_count;
+    hashtable_traits_t* traits;
+    size_t key_size;
+    size_t value_size;
+    hashtable_entry_t* buckets0[97];
+};
+
+struct _hashtable_entry_t
+{
+    uint64_t hashcode;
+    hashtable_entry_t* next;
+    /* key and value follow */
+};
+
+struct _hashtable_lookup_t
+{
+    hashtable_t* table;
+    void* key;
+    hashtable_entry_t* entry;
+    hashtable_entry_t* bucket;
+    hashtable_entry_t* previous;
+    size_t bucket_index;
+    uint64_t hashcode;
+};
+
+void* hashtable_entry_getkey(hashtable_t* t, hashtable_entry_t* e)
+{
+    return (e && t->key_size) ? (1 + e) : 0;
+}
+
+void* hashtable_entry_getvalue(hashtable_t* t, hashtable_entry_t* e)
+{
+    return (e && t->value_size) ? (t->key_size + (char*)e) : 0;
+}
+
+struct hashtable_t
+{
+    hashtable_traits_t* traits;
+};
+
+int hashtable_new(hashtable_t** out_table,
+                  hashtable_traits_t* traits)
+{
+  hashtable_t* table = 0;
+  
+  *out_table = table;  
+  if (!(table = (hashtable_t*)calloc(1, sizeof(*table))))
+    return -1;
+  *out_table = table;  
+  table->bucket_count = sizeof(table->buckets0) / sizeof(table->buckets0[0]);
+  table->buckets = table->buckets0;   
+  table->traits = traits;
+  table->key_size = traits->key_size;
+  table->value_size = traits->value_size;
+  return 0;
+}
+
+void hashtable_lookup(hashtable_lookup_t* lookup)
+{
+  void* key = lookup->key;
+  hashtable_t* table = lookup->table;
+  size_t bucket_count = table->bucket_count;
+  hashtable_traits_t* traits = table->traits;
+  uint64_t hashcode = lookup->hashcode ? lookup->hashcode : traits->hash(traits, lookup->key);
+  size_t bucket_index = (hashcode % table->bucket_count);
+  hashtable_entry_t* bucket = table->buckets[bucket_index];
+  hashtable_entry_t* entry = bucket;
+  hashtable_entry_t* previous = { 0 };
+  lookup->bucket = bucket;
+  lookup->entry = 0;
+  lookup->previous = 0;
+  lookup->bucket_index = bucket_index;
+  lookup->hashcode = hashcode;
+  while (entry && (entry->hashcode != hashcode
+        || !traits->equal(traits, key, hashtable_entry_getkey(table, entry))))
+  {
+    previous = entry;
+    entry = entry->next;
+  }
+  if (entry)
+  {
+    lookup->entry = entry;
+    lookup->previous = previous;
+  }
+}
+
+void hashtable_rebucket(hashtable_t* table)
+{
+}
+
+void* hashtable_find(hashtable_t* table, void* key)
+{
+  hashtable_entry_t* entry = { 0 };
+  hashtable_lookup_t lookup = { 0 };
+  lookup.table = table;
+  lookup.key = key;
+  hashtable_lookup(&lookup);
+  return lookup.entry ? hashtable_entry_getvalue(table, lookup.entry) : 0;
+}
+
+int hashtable_add(hashtable_t* table, void* key, void* value)
+{
+  hashtable_entry_t* entry = { 0 };
+  hashtable_lookup_t lookup = { 0 };
+  lookup.table = table;
+  lookup.key = key;
+  hashtable_lookup(&lookup);
+  if (entry = lookup.entry)
+  {
+    memcpy(hashtable_entry_getvalue(table, entry), value, table->value_size);
+    return 0;
+  }
+  entry = (hashtable_entry_t*)calloc(1, table->key_size + table->value_size + sizeof(hashtable_entry_t));
+  if (!entry)
+    return -1;
+  memcpy(hashtable_entry_getkey(table, entry), key, table->key_size);
+  memcpy(hashtable_entry_getvalue(table, entry), value, table->value_size);
+  entry->next = lookup.bucket;
+  entry->hashcode = lookup.hashcode;
+  table->buckets[lookup.bucket_index] = entry;
+  table->entry_count += 1;
+  hashtable_rebucket(table);
+  return 0;
+}
+
+void hashtable_remove(hashtable_t* table, void* key)
+{
+  hashtable_entry_t* entry = { 0 };
+  hashtable_lookup_t lookup = { 0 };
+  size_t bucket_index = { 0 };
+  lookup.table = table;
+  lookup.key = key;
+  hashtable_lookup(&lookup);
+  entry = lookup.entry;
+  if (!entry)
+    return;
+  if (lookup.previous)
+    lookup.previous->next = entry->next;
+  else
+    table->buckets[lookup.bucket_index] = 0;
+  table->entry_count -= 1;
+  free(entry);
+}
+
+void hashtable_first(hashtable_t* table, hashtable_entry_t** entry)
+{
+  size_t a;
+  hashtable_entry_t* e = { 0 };
+  hashtable_entry_t** buckets = table->buckets;
+  size_t bucket_count = table->bucket_count;
+  for (a = 0; a < bucket_count && !(e = buckets[a]); ++a)
+  {
+    /* nothing */
+  }
+  *entry = e;
+}
+
+void hashtable_next(hashtable_t* table, hashtable_entry_t** entry)
+{
+  hashtable_entry_t* e = *entry;
+  hashtable_entry_t* next = e->next;
+  size_t a = { 0 };
+  hashtable_entry_t** buckets = { 0 };
+  size_t bucket_count = { 0 };
+  if (next)
+  {
+    *entry = next;
+    return;
+  }
+  buckets = table->buckets;
+  bucket_count = table->bucket_count;
+  a = (e->hashcode % bucket_count);
+  for (; a < bucket_count && !(e = buckets[a]); ++a)
+  {
+    /* nothing */
+  }
+  *entry = e;
+}
+
+void hashtable_clear(hashtable_t* table)
+{
+  /* leaky */
+  table->entry_count = 0;
+  memset(table->buckets, 0, table->bucket_count * sizeof(table->buckets[0]));
+}
+
+#if 1
+
+uint64_t string_hash(hashtable_traits_t* c, void* x)
+{
+  const unsigned char* a = (const unsigned char*)x;
+  uint64_t b = { 0 };
+  while (*a)
+    b = (b << 6) ^ *a++;
+  return b;
+}
+
+BOOL string_equal(hashtable_traits_t* x, void* a, void* b)
+{
+  return !strcmp(a, b);
+}
+
+int main()
+{
+  int value = { 0 };
+  hashtable_t* table = { 0 };
+  hashtable_traits_t
+  traits = { string_equal,
+             string_hash,
+             sizeof(char*),
+             sizeof(int) };
+             
+  hashtable_new(&table, &traits);
+  
+  hashtable_add(table, "one", (value = 1, &value));
+  hashtable_add(table, "two", (value = 2, &value));
+  hashtable_add(table, "three", (value = 3, &value));
+  
+  printf("%d\n", *(int*)hashtable_find(table, "one"));
+  printf("%d\n", *(int*)hashtable_find(table, "two"));
+  printf("%d\n", *(int*)hashtable_find(table, "three"));
+  
+  return 0;
+}
+
+#endif
---
+++ scratch/macho/macho.c
@@ -0,0 +1,567 @@
+#include <stdlib.h>
+#include <string.h>
+#include "macho.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <limits.h>
+#ifdef _WIN32
+#define PRINT64 "I64"
+#else
+#define PRINT64 "ll"
+#endif
+#define NUMBER_OF(a) (sizeof(a)/sizeof((a)[0]))
+#define STRINGIZEx(a) #a
+#define STRINGIZE(a) STRINGIZEx(a)
+#define PASTEx(a, b) a##b
+#define PASTE(a, b) PASTEx(a, b)
+
+/* const means static const in C++, and extern const in C,
+ * "extern const" portable means the same thing in either, but gcc warns.
+ */
+#if defined(__cplusplus) || !defined(__GNUC__)
+#define extern_const extern const
+#else
+#define extern_const const
+#endif
+
+typedef struct _field_t {
+    char name[14];
+    uchar offset;
+    uchar size;
+    uchar element_size;
+    uchar str;
+} field_t;
+
+#define FIELD(t, f) {STRINGIZE(f), offsetof(t, f), sizeof((((t*)0)->f))}
+#define FIELD_ARRAY(t, f) {STRINGIZE(f), offsetof(t, f), sizeof((((t*)0)->f)), sizeof((((t*)0)->f)[0]) }
+#define FIELD_STRING(t, f) {STRINGIZE(f), offsetof(t, f), sizeof((((t*)0)->f)), sizeof((((t*)0)->f)[0]), 1 }
+
+typedef struct _struct_t {
+    const char* name;
+    uint    size;
+    uint    nfields;
+    const field_t* fields;
+    int widest_field;
+} struct_t;
+
+#define STRUCT(a) {STRINGIZE(a), sizeof(a), NUMBER_OF(PASTE(a,_fields)), PASTE(a,_fields)}
+
+extern_const field_t macho_header32_t_fields[] = {
+    FIELD(macho_header32_t, magic),
+    FIELD(macho_header32_t, cputype),
+    FIELD(macho_header32_t, cpusubtype),
+    FIELD(macho_header32_t, filetype),
+    FIELD(macho_header32_t, ncmds),
+    FIELD(macho_header32_t, sizeofcmds),
+    FIELD(macho_header32_t, flags)
+};
+
+extern_const field_t macho_header64_t_fields[] = {
+    FIELD(macho_header64_t, magic),
+    FIELD(macho_header64_t, cputype),
+    FIELD(macho_header64_t, cpusubtype),
+    FIELD(macho_header64_t, filetype),
+    FIELD(macho_header64_t, ncmds),
+    FIELD(macho_header64_t, sizeofcmds),
+    FIELD(macho_header64_t, flags),
+    FIELD(macho_header64_t, reserved)
+};
+
+struct_t struct_macho_header32 = STRUCT(macho_header32_t);
+struct_t struct_macho_header64 = STRUCT(macho_header64_t);
+
+typedef union _macho_load_command_u {
+    macho_loadcommand_t base;
+    macho_segment32_t segment32;
+    macho_segment64_t segment64;
+} macho_load_command_u;
+
+typedef struct _macho_file_t {
+    const char* path;
+    uchar* contents;
+    size_t size;
+    macho_header32_t* mh32;
+    macho_header64_t* mh64;
+    uchar m64;
+    uchar swapped;
+} macho_file_t;
+
+uint32 swap4(uint32 a)
+{
+    return (a >> 24) | ((a >> 8) & 0xFF00) | ((a << 8) & 0xFF0000) | (a << 24);
+}
+
+uint64 swap8(uint64 a)
+{
+    const uint64 FF = 0xFF;
+    return (a >> 56)
+        | ((a >> 40) & (FF << 8))
+        | ((a >> 24) & (FF << 16))
+        | ((a >>  8) & (FF << 24))
+        | ((a <<  8) & (FF << 32))
+        | ((a << 24) & (FF << 40))
+        | ((a << 40) & (FF << 48))
+        | (a << 56);
+}
+
+uint32 macho_swap4(macho_file_t* m, uint32 a)
+{
+    return m->swapped ? swap4(a) : a;
+}
+
+void swap4p(uint32* a)
+{
+    *a = swap4(*a);
+}
+
+int char_is_printable(char ch)
+{
+    return (ch >= 0x20 && ch <= 0x7E);
+}
+
+char char_to_printable(char ch)
+{
+    return char_is_printable(ch) ? ch : '.';
+}
+
+void adjust_hex_case(char* a)
+{
+    a = strchr(a, 'X');
+    if (a) *a = 'x';
+    else return;
+    a = strchr(a, 'X');
+    if (a) *a = 'x';
+}
+
+void field_print(struct_t* s, const field_t* f, void* p)
+{
+    char buffer[256];
+    char* cursor = buffer;
+    uchar* q = (f->offset + (uchar*)p);
+    uint32* q32 = (uint32*)q;
+    uint64* q64 = (uint64*)q;
+    uint i = {0};
+    uchar size = f->size;
+    uchar str = f->str;
+    cursor += sprintf(cursor, "%s.%-*s ", s->name, s->widest_field, f->name);
+    if (str)
+    {
+        uint length = 0;
+        for (i = 0; i < size && q[i]; ++i)
+            length += 1;
+        while (length < 16)
+        {
+            length += 1;
+            *cursor++ = ' ';
+        }
+        for (i = 0; i < size && q[i]; ++i)
+            *cursor++ = q[i];
+        *cursor++ = 0;
+    }
+    else
+    {
+        if (size == 4)
+            i = sprintf(cursor, "%#16lX %#lX", (ulong)*q32, (ulong)swap4(*q32));
+        else if (size == 8)
+            i = sprintf(cursor, "%#16" PRINT64 "X %#" PRINT64 "X", *q64, swap8(*q64));
+        adjust_hex_case(cursor);
+        cursor += i;
+    }
+    printf("%s\n", buffer);
+}
+
+void struct_print(struct_t* t, void* p)
+{
+    uint i;
+    int widest_field = t->widest_field;
+    if (widest_field == 0)
+    {
+        for (i = 0; i < t->nfields; ++i)
+        {
+            size_t j = strlen(t->fields[i].name);
+            if (j > widest_field && j <= INT_MAX)
+                widest_field = (int)j;
+        }
+        t->widest_field = widest_field;
+     }
+    for (i = 0; i < t->nfields; ++i)
+        field_print(t, &t->fields[i], p);
+}
+
+const char* macho_loadcommand_name(uint32_t cmd)
+{
+    static char a[99];
+    switch (cmd & ~macho_loadcommand_require_dyld)
+    {
+#define X(a) case macho_loadcommand_##a & ~macho_loadcommand_require_dyld: return #a;
+    X(segment32)
+    X(symtab)
+    X(symseg)
+    X(thread)
+    X(unixthread)
+    X(fixed_vm_lib)
+    X(id_fixed_vm_lib)
+    X(ident)
+    X(fixed_vm_file)
+    X(prepage)
+    X(dysymtab)
+    X(load_dylib)
+    X(id_dylib)
+    X(load_dylinker)
+    X(id_dylinker)
+    X(prebound_dylib)
+    X(routines32)
+    X(sub_framework)
+    X(sub_umbrella)
+    X(sub_client)
+    X(sub_library)
+    X(twolevel_hints)
+    X(prebind_checksum)
+    X(load_weak_dylib)
+    X(segment64)
+    X(routines64)
+    X(uuid)
+    X(rpath)
+    X(code_signature)
+    X(segment_split_info)
+    X(reexport_dylib)
+    X(lazy_load_dylib)
+    X(encryption_info)
+#undef X
+    }
+    sprintf(a, "unknown (%u)", cmd);
+    return a;
+}
+
+macho_loadcommand_t* macho_first_load_command(macho_file_t* m)
+{
+    return (m->m64 ? (macho_loadcommand_t*)(m->mh64 + 1) : (macho_loadcommand_t*)(m->mh32 + 1));
+}
+
+macho_loadcommand_t* macho_next_load_command(macho_file_t* m, macho_loadcommand_t* L)
+{
+    return (macho_loadcommand_t*)(macho_swap4(m, L->cmdsize) + (uchar*)L);
+}
+
+uint macho_ncmds(macho_file_t* m)
+{
+    return macho_swap4(m, m->m64 ? m->mh64->ncmds : m->mh32->ncmds);
+}
+
+extern_const field_t macho_segment32_t_fields[] = {
+    FIELD(macho_segment32_t, cmd),
+    FIELD(macho_segment32_t, cmdsize),
+    FIELD_STRING(macho_segment32_t, segname),
+    FIELD(macho_segment32_t, vmaddr),
+    FIELD(macho_segment32_t, vmsize),
+    FIELD(macho_segment32_t, fileoff),
+    FIELD(macho_segment32_t, filesize),
+    FIELD(macho_segment32_t, maxprot),
+    FIELD(macho_segment32_t, initprot),
+    FIELD(macho_segment32_t, nsects),
+    FIELD(macho_segment32_t, flags)
+};
+
+struct_t struct_macho_segment32 = STRUCT(macho_segment32_t);
+
+extern_const field_t macho_segment64_t_fields[] = {
+    FIELD(macho_segment64_t, cmd),
+    FIELD(macho_segment64_t, cmdsize),
+    FIELD_STRING(macho_segment64_t, segname),
+    FIELD(macho_segment64_t, vmaddr),
+    FIELD(macho_segment64_t, vmsize),
+    FIELD(macho_segment64_t, fileoff),
+    FIELD(macho_segment64_t, filesize),
+    FIELD(macho_segment64_t, maxprot),
+    FIELD(macho_segment64_t, initprot),
+    FIELD(macho_segment64_t, nsects),
+    FIELD(macho_segment64_t, flags)
+};
+
+struct_t struct_macho_segment64 = STRUCT(macho_segment64_t);
+
+extern_const field_t macho_section32_t_fields[] = {
+    FIELD_STRING(macho_section32_t, sectname),
+    FIELD_STRING(macho_section32_t, segname),
+    FIELD(macho_section32_t, addr),
+    FIELD(macho_section32_t, size),
+    FIELD(macho_section32_t, offset),
+    FIELD(macho_section32_t, align),
+    FIELD(macho_section32_t, reloff),
+    FIELD(macho_section32_t, nreloc),
+    FIELD(macho_section32_t, flags),
+    FIELD(macho_section32_t, reserved1),
+    FIELD(macho_section32_t, reserved2)
+};
+
+struct_t struct_macho_section32 = STRUCT(macho_section32_t);
+
+extern_const field_t macho_section64_t_fields[] = {
+    FIELD_STRING(macho_section64_t, sectname),
+    FIELD_STRING(macho_section64_t, segname),
+    FIELD(macho_section64_t, addr),
+    FIELD(macho_section64_t, size),
+    FIELD(macho_section64_t, offset),
+    FIELD(macho_section64_t, align),
+    FIELD(macho_section64_t, reloff),
+    FIELD(macho_section64_t, nreloc),
+    FIELD(macho_section64_t, flags),
+    FIELD(macho_section64_t, reserved1),
+    FIELD(macho_section64_t, reserved2),
+    FIELD(macho_section64_t, reserved3)
+};
+
+struct_t struct_macho_section64 = STRUCT(macho_section64_t);
+
+void macho_dump_load_command_segment32(macho_file_t* m, macho_loadcommand_t* L)
+{
+    struct_print(&struct_macho_segment32, L);
+}
+
+void macho_dump_load_command_segment64(macho_file_t* m, macho_loadcommand_t* L)
+{
+    struct_print(&struct_macho_segment64, L);
+}
+
+void macho_dump_load_command_symtab(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_symseg(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_thread(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_unixthread(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_fixed_vm_lib(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_id_fixed_vm_lib(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_ident(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_fixed_vm_file(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_prepage(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_dysymtab(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_load_dylib(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_id_dylib(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_load_dylinker(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_id_dylinker(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_prebound_dylib(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_routines32(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_sub_framework(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_sub_umbrella(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_sub_client(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_sub_library(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_twolevel_hints(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_prebind_checksum(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_load_weak_dylib(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_routines64(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_uuid(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_rpath(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_code_signature(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_segment_split_info(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_reexport_dylib(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_lazy_load_dylib(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command_encryption_info(macho_file_t* m, macho_loadcommand_t* L)
+{
+}
+
+void macho_dump_load_command(macho_file_t* m, macho_loadcommand_t* L, uint i)
+{
+    uint cmd = macho_swap4(m, L->cmd);
+    printf("cmd %u %s\n", i, macho_loadcommand_name(cmd));
+    switch (cmd)
+    {
+#define X(a) case macho_loadcommand_##a & ~macho_loadcommand_require_dyld: macho_dump_load_command_##a(m, L); break;
+    X(segment32)
+    X(symtab)
+    X(symseg)
+    X(thread)
+    X(unixthread)
+    X(fixed_vm_lib)
+    X(id_fixed_vm_lib)
+    X(ident)
+    X(fixed_vm_file)
+    X(prepage)
+    X(dysymtab)
+    X(load_dylib)
+    X(id_dylib)
+    X(load_dylinker)
+    X(id_dylinker)
+    X(prebound_dylib)
+    X(routines32)
+    X(sub_framework)
+    X(sub_umbrella)
+    X(sub_client)
+    X(sub_library)
+    X(twolevel_hints)
+    X(prebind_checksum)
+    X(load_weak_dylib)
+    X(segment64)
+    X(routines64)
+    X(uuid)
+    X(rpath)
+    X(code_signature)
+    X(segment_split_info)
+    X(reexport_dylib)
+    X(lazy_load_dylib)
+    X(encryption_info)
+#undef X
+    }
+}
+
+void macho_dump_load_commands(macho_file_t* m)
+{
+    uint ncmds = macho_ncmds(m);
+    uint i;
+    macho_loadcommand_t* L = macho_first_load_command(m);
+    for (i = 0; i < ncmds; ++i)
+    {
+        macho_dump_load_command(m, L, i);
+        L = macho_next_load_command(m, L);
+    }
+}
+
+void open_and_read_entire_file(const char* path, uchar** contents, size_t* size)
+{
+    uchar* buffer = { 0 };
+    uchar* prev_buffer = { 0 };
+    size_t buffer_size = { 0x10000 / 2 };
+    size_t prev_buffer_size = { 0 };
+    FILE* file = { 0 };
+    size_t bytes_read = { 0 };
+    
+    *size = 0;
+    *contents = 0;
+    file = fopen(path, "rb");
+    if (!file)
+        goto Exit;
+    while (1)
+    {
+        buffer_size *= 2;
+        prev_buffer = buffer;
+        buffer = malloc(buffer_size);
+        if (!buffer)
+            goto Exit;
+        if (prev_buffer)
+        {
+            memcpy(buffer, prev_buffer, prev_buffer_size);
+            free(prev_buffer);
+            prev_buffer = 0;
+        }
+        bytes_read = fread(buffer + prev_buffer_size, 1, buffer_size - prev_buffer_size, file);
+        if (bytes_read < (buffer_size - prev_buffer_size))
+            break;
+        prev_buffer_size = buffer_size;
+    }
+Exit:
+    if (prev_buffer) free(prev_buffer);
+    *size = prev_buffer_size + bytes_read;
+    *contents = buffer;
+    if (file) fclose(file);
+}
+
+int main(int argc, char** argv)
+{
+    macho_file_t m = { 0 };
+
+    m.path = argv[1];
+    open_and_read_entire_file(m.path, &m.contents, &m.size);
+    /*printf("%p %lu\n", mh32, (ulong)size);*/
+    
+    m.mh32 = (macho_header32_t*)m.contents;
+    
+    m.m64 =     (m.mh32->magic == macho_magic64          || m.mh32->magic == macho_magic64_reversed);
+    m.swapped = (m.mh32->magic == macho_magic32_reversed || m.mh32->magic == macho_magic64_reversed);
+    if (m.m64)
+        m.mh64 = (macho_header64_t*)m.contents;
+
+    if (m.m64)
+        struct_print(&struct_macho_header64, m.mh64);
+    else
+        struct_print(&struct_macho_header32, m.mh32);
+
+    macho_dump_load_commands(&m);
+    return 0;
+}
