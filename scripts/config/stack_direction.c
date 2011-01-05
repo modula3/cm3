a016418a24bd9aebdb4e9177d39ef95f7716bd02
Jay Michael Krell <jay.krell@cornell.edu>
2011-01-05 14:17:51 +0000

    more like m3core

--- scripts/config/stack_direction.c
+++ scripts/config/stack_direction.c
@@ -2,17 +2,27 @@
 #include <assert.h>
 #include <stdlib.h>
 
-static int xStackGrowsDown (volatile char* a)
+int
+ThreadInternal__StackGrowsDownHelper (volatile char* a)
+/* Inlining could potentially reverse the relative placements,
+ * leading to an incorrect result! Recursion is used to
+ * help defeat inlining optimizer, though a smart compiler
+ * could still inline. */
 {
   volatile char b;
-  return (&b < a);
+  return a ? (&b < a) : ThreadInternal__StackGrowsDownHelper (&b);
 }
 
-int
+int __cdecl
 ThreadInternal__StackGrowsDown (void)
 {
-  volatile char a;
-  return xStackGrowsDown (&a);
+  int a = ThreadInternal__StackGrowsDownHelper (0);
+#ifdef __hppa__
+  assert(!a);
+#else
+  assert(a);
+#endif
+  return a;
 }
 
 int main()
