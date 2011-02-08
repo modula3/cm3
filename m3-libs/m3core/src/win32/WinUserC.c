c289253339f04fcf3fd72c80be4e8a417996faed
jkrell <>
2011-02-08 14:21:14 +0000

    comment only

--- m3-libs/m3core/src/win32/WinUserC.c
+++ m3-libs/m3core/src/win32/WinUserC.c
@@ -10,7 +10,8 @@ extern "C" {
 
 HWND __stdcall WinUser__WindowFromPointWorkaround (POINT* Point)
 /* The Modula-3 gcc backend is not naming WindowFromPoint correctly.
-   It is calling it WindowFromPoint@4 instead of WindowFromPoint@8
+   It is calling it WindowFromPoint@4 instead of WindowFromPoint@8.
+   We workaround that by passing by VAR instead of by value.
 */
 {
 	return WindowFromPoint(*Point);
