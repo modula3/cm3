0d9f18de75f497951f511238b7d11796fd029889
Jay Michael Krell <jay.krell@cornell.edu>
2011-04-22 07:59:42 +0000

    initial copies from runtime/common

---
+++ m3-libs/m3core/src/thread/POSIX/RTProcessC.c
@@ -0,0 +1,46 @@
+typedef void (*ForkHandler)(void);
+ 
+#include "m3core.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* NOTE: Even userthreads now depends
+ * on availability of pthreads.
+ * This can be fixed if need be.
+ */
+
+INTEGER
+__cdecl
+RTProcess__RegisterForkHandlers(
+    ForkHandler prepare,
+    ForkHandler parent,
+    ForkHandler child)
+{
+/* FreeBSD < 6 lacks pthread_atfork. Would be good to use autoconf.
+ * VMS lacks pthread_atfork? Would be good to use autoconf.
+ * Win32 lacks pthread_atfork and fork. OK.
+ *
+ * As well, for all Posix systems, we could implement
+ * atfork ourselves, as long as we provide a fork()
+ * wrapper that code uses.
+ */
+#if defined(_WIN32) \
+        || defined(__vms) \
+        || (defined(__FreeBSD__) && (__FreeBSD__ < 6))
+    return 0;
+#else
+    while (1)
+    {
+      int i = pthread_atfork(prepare, parent, child);
+      if (i != EAGAIN)
+        return i;
+      sleep(0);
+    }
+#endif
+}
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
