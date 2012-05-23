ca8f18804061e73cd215a0c4e02ab54037f472dc
Jay Michael Krell <jay.krell@cornell.edu>
2012-05-23 16:49:39 +0000

    fix

--- m3-sys/m3cc/gcc-4.6/gcc/1.pl
+++ m3-sys/m3cc/gcc-4.6/gcc/1.pl
@@ -6,9 +6,10 @@ my $c = join("", <$b>);
 
 my $d = $c;
 $d =~ s/#ifdef __cplusplus\nextern "C" {\n#endif\n/EXTERN_C_START\n/;
+$d =~ s/#ifdef __cplusplus\n} \/\* extern "C" \*\/\n#endif\n/EXTERN_C_END\n/;
 
 if ($c ne $d)
 {
   open(my $b, ">", $a);
-  print $b $c;
+  print $b $d;
 }
