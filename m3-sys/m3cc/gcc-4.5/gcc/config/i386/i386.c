5889144cb020d7ea2e5d05977de0f0d92b767d1f
Jay Michael Krell <jay.krell@cornell.edu>
2012-06-11 01:32:15 +0000

    add comment that we modified it

--- m3-sys/m3cc/gcc-4.5/gcc/config/i386/i386.c
+++ m3-sys/m3cc/gcc-4.5/gcc/config/i386/i386.c
@@ -1,3 +1,5 @@
+/* Modula-3: modified */
+
 /* Subroutines used for code generation on IA-32.
    Copyright (C) 1988, 1992, 1994, 1995, 1996, 1997, 1998, 1999, 2000,
    2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010
