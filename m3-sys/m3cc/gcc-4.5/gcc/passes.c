1eafd5a00ac8ee62e4b54a6dc2e709ece46449a1
Jay Michael Krell <jay.krell@cornell.edu>
2012-11-29 12:50:10 +0000

    http://www.gnu.org/software/gcc/gcc-4.8/changes.html
    The struct reorg and matrix reorg optimizations
    (command-line options -fipa-struct-reorg and
    -fipa-matrix-reorg) have been removed. They
    did not work correctly nor with link-time
    optimization (LTO), hence were only applicable
    to programs consisting of a single translation unit.
    
    
    so remove them in our copies of older versions too.
    Always looking for an excuse to remove gnarly code that
    probably isn't used..that the owners removed it in newer
    versions is a good reason..

--- m3-sys/m3cc/gcc-4.5/gcc/passes.c
+++ m3-sys/m3cc/gcc-4.5/gcc/passes.c
@@ -801,7 +801,6 @@ init_optimization_passes (void)
       NEXT_PASS (pass_inline_parameters);
     }
   NEXT_PASS (pass_ipa_increase_alignment);
-  NEXT_PASS (pass_ipa_matrix_reorg);
   *p = NULL;
 
   p = &all_regular_ipa_passes;
@@ -812,7 +811,6 @@ init_optimization_passes (void)
   NEXT_PASS (pass_ipa_pure_const);
   NEXT_PASS (pass_ipa_type_escape);
   NEXT_PASS (pass_ipa_pta);
-  NEXT_PASS (pass_ipa_struct_reorg);
   *p = NULL;
 
   /* These passes are run after IPA passes on every function that is being
