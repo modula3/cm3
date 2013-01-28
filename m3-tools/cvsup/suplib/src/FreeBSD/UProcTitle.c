a72ba79226952a3b5b3a93b1e0a0ff3c142b4582
Jay Michael Krell <jay.krell@cornell.edu>
2013-01-28 11:03:12 +0000

    C++ calling conventions let it compile for all platforms, albeit the function will silently do nothing except on FreeBSD

--- m3-tools/cvsup/suplib/src/FreeBSD/UProcTitle.c
+++ m3-tools/cvsup/suplib/src/FreeBSD/UProcTitle.c
@@ -1,11 +1,23 @@
+#include "m3core.h"
+
 #ifdef __FreeBSD__
 #include <sys/types.h>
 #include <libutil.h>
 #endif
 
-void m3_setproctitle(const char *title)
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void
+__cdecl
+m3_setproctitle(const char *title)
 {
 #ifdef __FreeBSD__
     setproctitle("%s", title);
 #endif
 }
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
