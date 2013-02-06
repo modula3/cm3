61ce8424b14d1f1c85bb24acca4f4610f6b00ee1
Olaf Wagner <wagner@elegosoft.com>
2013-02-06 07:18:12 +0000

    that was not what this script is supposed to do...

--- scripts/regression/hudson_build_system.sh
+++ scripts/regression/hudson_build_system.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
-set -e
+#set -e
 #set -x
 
 ulimit -d unlimited || true
@@ -29,42 +29,6 @@ cd ${WS}/cm3/scripts/regression || {
   exit 1
 }
 
-for a in cm3cg gcc/m3cgc1; do
-    for b in "" .exe; do
-        rm "${WS}/cm3/m3-sys/m3cc/${TARGET}/$a$b" || true
-    done
-done
-
-# echo 1 CM3CG is ${CM3CG}
-# if [ -z "${CM3CG}" ]; then
-#   case "${WS}" in
-#     *workspace)
-#       CM3CG=${WS}/../../cm3-current-m3cc-${TARGET}/workspace/cm3/m3-sys/m3cc/${TARGET}/cm3cg
-#     ;;
-#     *)
-#       CM3CG=${WS}/../cm3-current-m3cc-${TARGET}/cm3/m3-sys/m3cc/${TARGET}/cm3cg
-#     ;;
-#   esac
-# fi
-# echo 2 CM3CG is ${CM3CG}
-
-# if [ "x$CLEAN" = "xfalse" ]; then
-#   if [ "x$USE_PREBUILT_CM3CG" = "xtrue" ]; then
-#     if [ -x "${CM3CG}" ]; then
-#       echo "checking for working pre-built cm3cg in ${CM3CG}"
-#       cp -p "${CM3CG}" ${WS}/cm3cg
-#       if ${WS}/cm3cg --version; then
-#         echo "using PREBUILT_CM3CG=${WS}/cm3cg"
-#         PREBUILT_CM3CG=${WS}/cm3cg
-#         export PREBUILT_CM3CG
-#       else
-#         echo "NOT using ${WS}/cm3cg"
-#       fi
-#     else
-#       echo "no executable ${CM3CG}"
-#     fi
-#   fi
-# fi
 . ./defs.sh
 
 [ "${WS}/cm3/scripts/pkginfo.txt" -nt "${WS}/cm3/scripts/PKGS" ] && {
@@ -74,12 +38,9 @@ done
 
 rm -rf "${INSTROOT_CUR}"
 mkdir -p "${INSTROOT_CUR}"
-# echo setup lastok version as a start
-# echo cp -pR ${INSTROOT_LOK}/* ${INSTROOT_CUR}
-# cp -pR ${INSTROOT_LOK}/* ${INSTROOT_CUR}
-echo setup lastrel version as a start
-echo cp -pR ${INSTROOT_REL}/* ${INSTROOT_CUR}
-cp -pR ${INSTROOT_REL}/* ${INSTROOT_CUR}
+echo setup lastok version as a start
+echo cp -pR ${INSTROOT_LOK}/* ${INSTROOT_CUR}
+cp -pR ${INSTROOT_LOK}/* ${INSTROOT_CUR}
 
 export TMPDIR=${WS}
 
