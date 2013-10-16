3b357a7965658f20c1decd9a71cc3e40c36bf534
Jay Michael Krell <jay.krell@cornell.edu>
2013-10-16 08:41:24 +0000

    OpenMP removal

--- m3-sys/m3cc/gcc-4.7/gcc/passes.c
+++ m3-sys/m3cc/gcc-4.7/gcc/passes.c
@@ -1193,7 +1193,6 @@ init_optimization_passes (void)
       struct opt_pass **p = &pass_early_local_passes.pass.sub;
       NEXT_PASS (pass_fixup_cfg);
       NEXT_PASS (pass_init_datastructures);
-      NEXT_PASS (pass_expand_omp);
 
       NEXT_PASS (pass_referenced_vars);
       NEXT_PASS (pass_build_ssa);
