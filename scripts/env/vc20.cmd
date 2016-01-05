d2d199f6409757b20acdedeeb330b083f9bea068
Jay Krell <jay.krell@cornell.edu>
2016-01-05 02:25:20 -0800

    More scripts to set compilation environment.

---
+++ scripts/env/vc20.cmd
@@ -0,0 +1,5 @@
+call %~dp0clean.cmd
+set CM3_VS2015_OR_NEWER=
+set INCLUDE=%~d0\msdev\40\include
+set LIB=%~d0\msdev\20\lib
+set PATH=%~d0\msdev\20\bin;%PATH%
---
+++ scripts/env/vc40/x86.cmd
@@ -0,0 +1,5 @@
+call %~dp0..\clean.cmd
+set CM3_VS2015_OR_NEWER=
+set INCLUDE=%~d0\msdev\40\include
+set LIB=%~d0\msdev\40\lib
+set PATH=%~d0\msdev\40\bin;%PATH%
