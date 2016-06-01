c6542aa23fe0937ed81a0cbbcd39333df5201711
jaykrell <jay.krell@cornell.edu>
2016-06-01 01:35:50 -0700

    copy boot2.sh to boot2.py so I can parse the command line in a reasonable language

---
+++ scripts/python/boot2.py
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+set -x
+
+./make-dist-cfg.py $*
+./do-pkg.py m3cc buildship $*
+./do-cm3-all.py realclean skipgcc $*
+./do-pkg.py m3cc m3core libm3 buildship $*
+./upgrade.py skipgcc $*
+./do-cm3-all.py realclean skipgcc $*
+./do-cm3-all.py buildship $*
