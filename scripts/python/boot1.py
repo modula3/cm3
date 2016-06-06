4b67cf429141b50e089db84df5649a2fef77b0b2
jaykrell <jay.krell@cornell.edu>
2016-06-05 22:49:58 -0700

    Update comments, but more to do.

--- scripts/python/boot1.py
+++ scripts/python/boot1.py
@@ -6,13 +6,18 @@ import pylib
 from pylib import *
 
 #
-# Having used ".\do-cm3-front.py boot <platform>"
-# this script archives up the files needed on the target,
-# along with a makefile and script.
+# usage: ./boot1.py target
 #
-# It is assumed the target has a full source tree.
-# The goal of bootstrapping is to produce a cm3 on the new target.
-# From there, the tree can be rebuilt normally from cm3cg, m3core and on up.
+# This produces an archive that can be copied to the target,
+# extract, run make, producing a cm3.
+#
+# Copy that into place and proceed with boot2.py.
+#
+# The source trees on the two systems must be "compatible",
+# essentially cm3/m3core/m3middle/etc. have to match.
+#
+# TODO: This should really produce the entire system
+# in either assembly or C.
 #
 
 CopyConfigForDevelopment() or sys.exit(1)
