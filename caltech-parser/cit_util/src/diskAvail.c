02e2e55503ac31e6bbaf1b7adc1d2a5daa3b2f56
Jay Krell <jay.krell@cornell.edu>
2019-05-31 11:35:26 -0700

    Start restoring caltech stuff for Win32. (#55)
    * Revert "Skip all the calltech-parser, caltech-other, scheme stuff on windows (#50)"
    
    This reverts commit 658e25c6a29b646370a4b2db0649fbc849f3ed65.
    
    * Start repairing caltech stuff for Win32.

--- caltech-parser/cit_util/src/diskAvail.c
+++ caltech-parser/cit_util/src/diskAvail.c
@@ -1,3 +1,5 @@
+#if !_WIN32
+
 #ifdef linux
 # include <sys/statfs.h>
 #endif
@@ -5,13 +7,22 @@
 #include <sys/param.h>
 #include <sys/mount.h>
 
+#endif
+
+#include <assert.h>
+
 /* $Id$ */
+
+#if __cplusplus
+extern "C" {
+#endif
 
 int
 diskAvail(const char * path,
 	  int *bsize,
 	  double *total, double *avail, double *availNonSuperUser) 
 {
+#if !_WIN32
   struct statfs buf;
 
   if (statfs(path,&buf)<0) return -1;
@@ -20,7 +31,20 @@ diskAvail(const char * path,
   *total = buf.f_blocks;
   *avail = buf.f_bfree;
   *availNonSuperUser = buf.f_bavail;
-
   return 0;
+
+#else
+  /* TODO GetDiskFreeSpaceEx and/or refactor */
+
+  assert (0);
+  *bsize = 0;
+  *total = 0;
+  *avail = 0;
+  return -1;
+
+#endif
 }
 
+#if __cplusplus
+} /* extern "C" */
+#endif
