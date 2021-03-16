0a3e9a7bc5aa4c1a7c55963897ca71b485ef33c4
JayKrell <jay.krell@cornell.edu>
2021-03-15 22:20:22 -0700

    m3core: Consolidate RTOSbrk.c and RTOSmmap.c into RTOSc.c, and use ifdef to chose between sbrk and mmap.
    The only current system using sbrk is Linux/amd64.
    This is apparently to keep address range seen denser
    and garbage collector data structures smaller.
    This should be revisited and all Unix systems use mmap.
    
    Historical systems also used sbrk.
    Autoconf/cmake might be nice to drive this.

---
+++ m3-libs/m3core/src/runtime/POSIX/RTOSc.c
@@ -0,0 +1,48 @@
+#include "m3core.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if !defined(MAP_ANON) && defined(MAP_ANONYMOUS)
+#define MAP_ANON MAP_ANONYMOUS
+#endif
+
+void*
+__cdecl
+RTOS__GetMemory(WORD_T size)
+{
+    // TODO autoconf/make HAVE_MMAP
+#if defined(ULTRIX)                           || \
+    defined(ultrix)                           || \
+    defined(__ultrix)                         || \
+    defined(__ultrix__)                       || \
+    /* AIX386    */                              \
+    /* IBMR2     */                              \
+    /* IBMRT     */                              \
+    /* IRIX5     */                              \
+    /* SPARC SunOS 4.x */                        \
+    (defined(__x86_64__) && defined(__linux))
+    // This is the historic and more portable implementation.
+    // http://modula3.elegosoft.com/cgi-bin/cvsweb.cgi/cm3/m3-libs/m3core/src/runtime/POSIX/RTOS.m3?rev=1.1;content-type=text%2Fplain
+    //
+    // TODO change Linux/amd64 and all systems with mmap to mmap, once
+    // the garbage collector is changed to a multi-level array.
+    //
+    // sbrk is considered out of date but has the advantage of providing denser results.
+    //
+    // Anecdotal evidence:
+    // https://github.com/modula3/cm3/commit/518f93e67ed8f3291a5cd5cf0a39d1fefbc79969
+    // https://github.com/modula3/cm3/commit/384b3cc05fcedf4307f077f49cde3d6675b039c6
+    return sbrk(size);
+
+#else
+
+    return mmap(0, size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+
+#endif
+}
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
