5db5e044a0c1c8ac5861a94cdf85aff96b628de3
JayKrell <jay.krell@cornell.edu>
2021-03-26 14:48:02 -0700

    udp: Fix compilation as C.

--- m3-comm/udp/src/Common/UDP_c.c
+++ m3-comm/udp/src/Common/UDP_c.c
@@ -24,6 +24,9 @@
 #include <sys/un.h>
 #endif
 
+typedef struct sockaddr sockaddr;
+typedef struct sockaddr_in sockaddr_in;
+
 #if __cplusplus
 extern "C" {
 #endif
@@ -32,8 +35,8 @@ void
 __cdecl
 UDPInternal__Assert(void)
 {
-    sockaddr_in sockaddr = {0};
-    M3_STATIC_ASSERT(sizeof(sockaddr.sin_addr.s_addr) == 4);
+    sockaddr_in sa = {0};
+    M3_STATIC_ASSERT(sizeof(sa.sin_addr.s_addr) == 4);
 }
 
 #ifndef _WIN32
@@ -49,18 +52,18 @@ UDPInternal__Send(
 // addr is array of char so we cannot assume alignment.
 // data is volatile pointer to pointer on stack to try to cooperate with garbage collector
 {
-    sockaddr_in sockaddr = {0};
+    sockaddr_in sa = {0};
 
     Scheduler__DisableSwitching();
 
-    ZERO_MEMORY(sockaddr);
-    sockaddr.sin_family = AF_INET;
-    sockaddr.sin_port = htons(port);
+    ZERO_MEMORY(sa);
+    sa.sin_family = AF_INET;
+    sa.sin_port = htons(port);
 
     // Copy possibly unaligned 4 character array to 4 byte integer.
-    memcpy(&sockaddr.sin_addr.s_addr, addr, 4);
+    memcpy(&sa.sin_addr.s_addr, addr, 4);
 
-    len = sendto(fd, *data, len, 0 /* flags */, (sockaddr*)&sockaddr, sizeof(sockaddr));
+    len = sendto(fd, *data, len, 0 /* flags */, (sockaddr*)&sa, sizeof(sa));
 
     Scheduler__EnableSwitching();
 
@@ -78,24 +81,24 @@ UDPInternal__Receive(
 // data is volatile pointer to pointer on stack to try to cooperate with garbage collector
 // addr is array of char so we cannot assume alignment.
 {
-    sockaddr_in sockaddr = {0};
-    socklen_t addr_len = sizeof(sockaddr);
+    sockaddr_in sa = {0};
+    socklen_t addr_len = sizeof(sa);
 
-    ZERO_MEMORY(sockaddr);
+    ZERO_MEMORY(sa);
 
     Scheduler__DisableSwitching();
 
-    len = recvfrom(fd, *data, len, 0 /* flags */, (sockaddr*)&sockaddr, &addr_len);
+    len = recvfrom(fd, *data, len, 0 /* flags */, (sockaddr*)&sa, &addr_len);
 
     Scheduler__EnableSwitching();
 
     if (len >= 0)
     {
         assert(addr_len == sizeof(sockaddr_in));
-        *port = ntohs(sockaddr.sin_port);
+        *port = ntohs(sa.sin_port);
 
         // Copy 4 byte integer to possibly unaligned 4 character array.
-        memcpy(addr, &sockaddr.sin_addr.s_addr, 4);
+        memcpy(addr, &sa.sin_addr.s_addr, 4);
     }
 
     return len;
