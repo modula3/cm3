1ace02b36f806bb98b1fc0eba8d4cfbf6852b91d
Jay Krell <jay.krell@cornell.edu>
2021-03-30 20:00:13 -0700

    Remove OSF/1 and Ultrix socket hacks. (#281)
    Specifically for certain socket errors, calls are made in an attempt to get the correct error.
    Ultrix is out of date 20+ years.
    OSF/1 is out of date 10+ years.
    This does not remove support for them per se, but just a bit of socket code specifically for them.
    If someone brings up these machines (even emulated) and gets everything else working (should not be difficult esp. for OSF/1) and provides access to the machine and preferably can demonstrate these bugs (or not), we will gladly bring this code back.

--- m3-libs/libm3/src/os/POSIX/SocketPosixC.c
+++ m3-libs/libm3/src/os/POSIX/SocketPosixC.c
@@ -1,9 +1,6 @@
 /* Copyright 1996-2000, Critical Mass, Inc.  All rights reserved. */
 /* See file COPYRIGHT-CMASS for details. */
 
-// TODO Just delete this old code? All the copies?
-// git grep RefetchError
-
 #include "m3core.h"
 #ifndef _WIN32
 #include <sys/types.h>
@@ -20,24 +17,6 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#define TRUE 1
-#define FALSE 0
-
-/* https://github.com/natefoo/predef/wiki/OperatingSystems */
-#if defined(ULTRIX)     || \
-    defined(ultrix)     || \
-    defined(__ultrix)   || \
-    defined(__ultrix__) || \
-    defined(__osf__)    || \
-    defined(__osf)
-static const char refetchError = TRUE;
-#else
-static const char refetchError = FALSE;
-#endif
-
-#define RefetchError SocketPosix__RefetchError
-void RefetchError(INTEGER fd);
 
 #if 0
 
@@ -615,21 +594,6 @@ static void MakeNonBlocking(int fd)
 }
 
 #endif
-
-void
-__cdecl
-RefetchError(INTEGER fd)
-{
-  if (refetchError)
-  {
-    /* Awful hack to retrieve a meaningful error from a TCP accept
-       socket.  Only works on Ultrix and OSF.  Leaves result
-       in errno.  */
-    int optbuf = 0;
-    socklen_t optlen = sizeof(optbuf);
-    getsockopt((int)fd, IPPROTO_TCP, TCP_NODELAY, &optbuf, &optlen);
-  }
-}
 
 #ifdef __cplusplus
 } /* extern C */
