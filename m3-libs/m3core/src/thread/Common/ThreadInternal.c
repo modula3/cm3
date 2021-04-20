fe1c78ac4ccb185df7e904e8be4bd5be8e8e1336
JayKrell <jay.krell@cornell.edu>
2021-04-19 23:41:32 -0700

    m3core: undef THOUSAND to support concat This is maybe not best solution -- try to find a shared definition or unshare the name.

--- m3-libs/m3core/src/thread/Common/ThreadInternal.c
+++ m3-libs/m3core/src/thread/Common/ThreadInternal.c
@@ -43,6 +43,7 @@ ThreadInternal__StackGrowsDown (void)
 enum {WaitResult_Ready, WaitResult_Error, WaitResult_FDError, WaitResult_Timeout};
 /* ^Must match TYPE WaitResult declared in SchedularPosix.i3. */ 
 
+#undef THOUSAND /* Support concatenating multiple .c files. */
 #define THOUSAND (1000)
 #define MILLION (1000 * 1000)
 
