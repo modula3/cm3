modula3/cm3

commit 49ce5350b6e7c135b7cba2da7871d1b4c984ce7e

m3front: Pass typeids for import_global. (#574)
This is needed so C globals can be const.
Previously the code avoided this, with an unclear comment. Why?

m3-sys/m3front/src/values/Variable.m3
@@ -598,7 +598,7 @@ PROCEDURE Declare (t: T): BOOLEAN =
         RTIO.Flush ();
       END;
 
-      t.cg_var := CG.Import_global (externM3ID, size, align, mtype, 0(*no mangling*), typename);
+      t.cg_var := CG.Import_global (externM3ID, size, align, mtype, typeUID, typename);
       t.cg_align := align;
 
     ELSIF (t.imported) THEN

commit 37adc1ab4559cd354e977ec034a7eadebf8a4d62

m3front: Restore globals to have uid=0 when not targeting C. (#585)
The identifiers do get changed and link fails otherwise.
Broken in https://github.com/modula3/cm3/commit/49ce5350b6e7c135b7cba2da7871d1b4c984ce7e

m3-sys/m3front/src/values/Variable.m3
@@ -598,6 +598,9 @@ PROCEDURE Declare (t: T): BOOLEAN =
         RTIO.Flush ();
       END;
 
+      IF Target.BackendMode # Target.M3BackendMode_t.C THEN
+        typeUID := 0(*no mangling*);
+      END;
       t.cg_var := CG.Import_global (externM3ID, size, align, mtype, typeUID, typename);
       t.cg_align := align;
 