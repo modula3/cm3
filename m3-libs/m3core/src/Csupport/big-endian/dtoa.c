f4ae8b3d40f1714f79c01a120928e6a448d185a6
JayKrell <jay.krell@cornell.edu>
2021-06-07 23:39:26 -0700

    m3core: Consolidate dtoa.
     - Move what is repeated in the dtoa.c files into dtoa.h.
     - This should almost enable:
    
     cat m3core.h *.c dtoa.h > cm3.c
    
     except for the pesky include. Think about it more later.

--- m3-libs/m3core/src/Csupport/big-endian/dtoa.c
+++ m3-libs/m3core/src/Csupport/big-endian/dtoa.c
@@ -2,35 +2,7 @@
 /* All rights reserved.                                                      */
 /* See the file COPYRIGHT for a full description.                            */
 
-#ifndef INCLUDED_M3CORE_H
-#include "m3core.h"
-#endif
-
-#ifndef __cplusplus
-#define KR_headers
-#endif
 #define IEEE_MC68k
 #undef IEEE_8087
 
-#define MULTIPLE_THREADS
-#define ACQUIRE_DTOA_LOCK(n) CConvert__Acquire(n)
-#define FREE_DTOA_LOCK(n) CConvert__Release(n)
-
-#if defined(__STDC__) || defined(__cplusplus)
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-void __cdecl CConvert__Acquire(INTEGER);
-void __cdecl CConvert__Release(INTEGER);
-
-#ifdef __cplusplus
-} /* extern C */
-#endif
-
-#endif /* STDC || C++ */
-
 #include "dtoa.h"
-
-#undef Bias
--- m3-libs/m3core/src/Csupport/little-endian/dtoa.c
+++ m3-libs/m3core/src/Csupport/little-endian/dtoa.c
@@ -2,31 +2,7 @@
 /* All rights reserved.                                                      */
 /* See the file COPYRIGHT for a full description.                            */
 
-#ifndef INCLUDED_M3CORE_H
-#include "m3core.h"
-#endif
-
-#ifndef __cplusplus
-#define KR_headers
-#endif
 #define IEEE_8087
 #undef IEEE_MC68k
 
-#define MULTIPLE_THREADS
-#define ACQUIRE_DTOA_LOCK(n) CConvert__Acquire(n)
-#define FREE_DTOA_LOCK(n) CConvert__Release(n)
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-void __cdecl CConvert__Acquire(INTEGER);
-void __cdecl CConvert__Release(INTEGER);
-
-#ifdef __cplusplus
-} /* extern C */
-#endif
-
 #include "dtoa.h"
-
-#undef Bias
