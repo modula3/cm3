a3b4f9198021bb82f4bb096798868be8858776ac
Eric Sessoms <coder5506@pobox.com>
2021-12-21 21:36:47 +0000

    add Vagrant definitions for BSD platforms
    There doesn't seem to be any real CI/CD support available for the BSDs, so
    we're slumming it with Vagrant boxes.  Right now Vagrant only builds and
    installs the latest release, verifying that it works on FreeBSD and NetBSD.

---
+++ vagrant/openbsd7/check-release.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+VERSION=d5.11.4
+DISTRIBUTION="cm3-dist-AMD64_LINUX-${VERSION}"
+
+mkdir work
+cd work
+wget "https://github.com/modula3/cm3/releases/download/${VERSION}/${DISTRIBUTION}.tar.xz"
+gtar Jxf "${DISTRIBUTION}.tar.xz"
+sed -i -e 's/^SYSTEM_CC.*/SYSTEM_CC = "c++ -fPIC"/' "${DISTRIBUTION}/m3-sys/cminstall/src/config-no-install/AMD64_OPENBSD"
+mkdir build
+cd build
+"../${DISTRIBUTION}/scripts/concierge.py" install --prefix "${HOME}/cm3" --target AMD64_OPENBSD
+PATH="${HOME}/cm3/bin:${PATH}"
+cd "../${DISTRIBUTION}/m3-sys/m3tests"
+cm3 -DHTML
+head -1 m3tests-results.xml
