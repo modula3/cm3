bec09f8fc7ddb78060d42cbc8bde6b87f7389c66
Jay Krell <jay.krell@cornell.edu>
2022-01-06 09:05:52 -0800

    scripts: Prefix .py with python on Windows.

--- scripts/python/upgrade-full.cmd
+++ scripts/python/upgrade-full.cmd
@@ -1,6 +1,6 @@
 SetLocal
 
 pushd %~dp0
-.\upgrade.py %* || goto :eof
-.\do-cm3-all.py realclean skipgcc %* || goto :eof
-.\do-cm3-all.py buildship %* || goto :eof
+python .\upgrade.py %* || goto :eof
+python .\do-cm3-all.py realclean skipgcc %* || goto :eof
+python .\do-cm3-all.py buildship %* || goto :eof
