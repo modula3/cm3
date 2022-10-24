65ca3e1248adb91e9151dc198dd87e2dad64205a
Jay Krell <jay.krell@cornell.edu>
2022-10-24 00:11:42 -0700

    caltech-parser: NetBSD does not have statfs, only Posix statvfs. (#1110)
    Old NetBSD had it. i.e. statvfs added circa NetBSD 2.0, statfs
    existed in 1.x and was preserved for compatibility.
    
    We probably could call the old one for compatibility, on x86,
    i.e. to run on 1.0.
    
    Or the other way around, we could probably use statvfs on all systems.
    Still, it'd be nice to investigate and test the long list of usual suspects:
    OpenBSD, FreeBSD, Mac, HP-UX, AIX, Irix, OpenVMS, Solaris, OSF/1, Haiku, Cygwin, DJGPP.
    Or some subset.

--- caltech-parser/cit_util/src/diskAvail.c
+++ caltech-parser/cit_util/src/diskAvail.c
@@ -1,7 +1,16 @@
+// $Id$
+
 #if !_WIN32
 
-#ifdef linux
-# include <sys/statfs.h>
+// statvfs is Posix
+// statfs is the historical code here, since 2019
+// and does work on some systems.
+// It does not always work where documented, e.g. NetBSD.
+// More testing is needed here.
+#if defined(__NetBSD__)
+#include <sys/statvfs.h>
+#elif defined(linux)
+#include <sys/statfs.h>
 #endif
 
 #include <sys/param.h>
@@ -10,8 +19,6 @@
 #endif
 
 #include <assert.h>
-
-/* $Id$ */
 
 #if __cplusplus
 extern "C" {
@@ -23,10 +30,18 @@ diskAvail(const char * path,
 	  double *total, double *avail, double *availNonSuperUser) 
 {
 #if !_WIN32
+  // TODO: This is Posix.
+  // Check other systems (FreeBSD, OpenBSD, Cygwin, Linux, Mac, Solaris, AIX, Irix, HP-UX, OSF/1, OpenVMS, etc.)
+  // Autoconf would be good here.
+#if defined(__NetBSD__)
+  struct statvfs buf;
+
+  if (statvfs(path,&buf)<0) return -1;
+#else
   struct statfs buf;
 
   if (statfs(path,&buf)<0) return -1;
-
+#endif
   *bsize = buf.f_bsize;
   *total = buf.f_blocks;
   *avail = buf.f_bfree;
@@ -34,7 +49,7 @@ diskAvail(const char * path,
   return 0;
 
 #else
-  /* TODO GetDiskFreeSpaceEx and/or refactor */
+  // TODO GetDiskFreeSpaceEx and/or refactor
 
   assert (0);
   *bsize = 0;
@@ -46,5 +61,5 @@ diskAvail(const char * path,
 }
 
 #if __cplusplus
-} /* extern "C" */
+} // extern "C"
 #endif
